d94c5758a6032b2d00fd43d48d9bcf2c93c9cd3c
hrumhurum <alex.s.gap@gmail.com>
2019-03-02 18:45:52 +0200

    Added Gapotchenko.FX.Linq.Test project.

---
+++ Source/Test/Gapotchenko.FX.Linq.Test/README.md
@@ -0,0 +1,3 @@
+﻿# Gapotchenko.FX.Linq.Test
+
+This is a test project for `Gapotchenko.FX.Linq` module.
---
+++ Source/Test/Gapotchenko.FX.Linq.Test/TracedEnumerable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gapotchenko.FX.Linq.Testing
+{
+    sealed class TracedEnumerable<T> : IEnumerable<T>
+    {
+        public TracedEnumerable(IEnumerable<T> underlyingEnumerable)
+        {
+            _UnderlyingEnumerable = underlyingEnumerable;
+        }
+
+        readonly IEnumerable<T> _UnderlyingEnumerable;
+
+        public bool EnumeratorRetrieved
+        {
+            get;
+            private set;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            EnumeratorRetrieved = true;
+            return _UnderlyingEnumerable.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            EnumeratorRetrieved = true;
+            return ((IEnumerable)_UnderlyingEnumerable).GetEnumerator();
+        }
+    }
+}
