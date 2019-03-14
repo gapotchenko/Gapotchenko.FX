2c4171eafc07c239d0e189cecba5326c72551007
hrumhurum <alex.s.gap@gmail.com>
2019-03-14 18:22:32 +0200

    Gapotchenko.FX.Linq module in progress.

--- Source/Gapotchenko.FX.Linq/EnumerableExtensions.cs
+++ Source/Gapotchenko.FX.Linq/EnumerableExtensions.cs
@@ -33,7 +33,7 @@ namespace Gapotchenko.FX.Linq
 
             foreach (var i in source)
                 yield return i;
-            
+
             yield return element;
 #endif
         }
@@ -59,7 +59,7 @@ namespace Gapotchenko.FX.Linq
                 throw new ArgumentNullException(nameof(source));
 
             yield return element;
-            
+
             foreach (var i in source)
                 yield return i;
 #endif
@@ -136,6 +136,34 @@ namespace Gapotchenko.FX.Linq
         }
 
         /// <summary>
+        /// Returns distinct elements from a sequence by using the default equality comparer on the keys extracted by a specified selector function.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of source.</typeparam>
+        /// <typeparam name="TKey">The type of the key returned by <paramref name="keySelector"/>.</typeparam>
+        /// <param name="source">The sequence to remove duplicate elements from.</param>
+        /// <param name="keySelector">A function to extract the key for each element.</param>
+        /// <returns>An <see cref="IEnumerable{T}"/> that contains distinct elements from the source sequence.</returns>
+        public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector) => source.DistinctBy(keySelector, null);
+
+        /// <summary>
+        /// Returns distinct elements from a sequence by using a specified <see cref="IEqualityComparer{T}"/> comparer on the keys extracted by a specified selector function.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of source.</typeparam>
+        /// <typeparam name="TKey">The type of the key returned by <paramref name="keySelector"/>.</typeparam>
+        /// <param name="source">The sequence to remove duplicate elements from.</param>
+        /// <param name="keySelector">A function to extract the key for each element.</param>
+        /// <param name="comparer">An <see cref="IEqualityComparer{T}"/> to compare values.</param>
+        /// <returns>An <see cref="IEnumerable{T}"/> that contains distinct elements from the source sequence.</returns>
+        public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector, IEqualityComparer<TKey> comparer)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            if (keySelector == null)
+                throw new ArgumentNullException(nameof(keySelector));
+            return source.Distinct(new SelectedKeyEqualityComparer<TSource, TKey>(keySelector, comparer));
+        }
+
+        /// <summary>
         /// Checks whether the number of elements in a sequence is greater or equal to a specified <paramref name="value"/>.
         /// </summary>
         /// <typeparam name="TSource">The type of the elements of source.</typeparam>
@@ -162,6 +190,148 @@ namespace Gapotchenko.FX.Linq
             }
 
             return false;
+        }
+
+        /// <summary>
+        /// Determines whether a sequence contains any elements and all of them satisfy a condition.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of source.</typeparam>
+        /// <param name="source">An <see cref="IEnumerable{T}"/> that contains the elements to apply the predicate to.</param>
+        /// <param name="predicate">A function to test each element for a condition.</param>
+        /// <returns>
+        /// <c>true</c> if the source sequence is not empty and every element passes the test in the specified predicate;
+        /// otherwise, <c>false</c>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">Source or predicate is null.</exception>
+        public static bool AnyAndAll<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            if (predicate == null)
+                throw new ArgumentNullException(nameof(predicate));
+
+            using (var enumerator = source.GetEnumerator())
+            {
+                if (!enumerator.MoveNext())
+                {
+                    // Sequence is empty.
+                    return false;
+                }
+
+                do
+                {
+                    if (!predicate(enumerator.Current))
+                        return false;
+                }
+                while (enumerator.MoveNext());
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Determines whether the <paramref name="first"/> sequence contains the <paramref name="second"/> sequence
+        /// by using a specified equality comparer.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of the input sequences.</typeparam>
+        /// <param name="first">An <see cref="IEnumerable{T}"/> to compare to second.</param>
+        /// <param name="second">An <see cref="IEnumerable{T}"/> to compare to the first sequence.</param>
+        /// <param name="comparer">An <see cref="IEqualityComparer{T}"/> to use to compare elements.</param>
+        /// <returns>
+        /// <c>true</c> if the <paramref name="first"/> sequence contains the <paramref name="second"/> sequence; otherwise, <c>false</c>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException"><paramref name="first"/> or <paramref name="second"/> is null.</exception>
+        public static bool Contains<TSource>(this IEnumerable<TSource> first, IEnumerable<TSource> second, IEqualityComparer<TSource> comparer)
+        {
+            if (first == null)
+                throw new ArgumentNullException(nameof(first));
+            if (second == null)
+                throw new ArgumentNullException(nameof(second));
+
+            if (ReferenceEquals(first, second))
+                return true;
+
+            if (comparer == null)
+                comparer = EqualityComparer<TSource>.Default;
+
+            second = second.Memoize();
+            bool eqRegion = true;
+
+            using (var e2 = second.GetEnumerator())
+            {
+                foreach (var e1Current in first)
+                {
+                    if (!e2.MoveNext())
+                        return eqRegion;
+
+                    if (comparer.Equals(e1Current, e2.Current))
+                    {
+                        eqRegion = true;
+                    }
+                    else
+                    {
+                        eqRegion = false;
+                        e2.Reset();
+                    }
+                }
+
+                if (eqRegion)
+                    return !e2.MoveNext();
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Determines whether the <paramref name="first"/> sequence contains the <paramref name="second"/> sequence
+        /// by using the default equality comparer to compare values.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of the input sequences.</typeparam>
+        /// <param name="first">An <see cref="IEnumerable{T}"/> to compare to second.</param>
+        /// <param name="second">An <see cref="IEnumerable{T}"/> to compare to the first sequence.</param>
+        /// <returns>
+        /// <c>true</c> if the <paramref name="first"/> sequence contains the <paramref name="second"/> sequence; otherwise, <c>false</c>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException"><paramref name="first"/> or <paramref name="second"/> is null.</exception>
+        public static bool Contains<TSource>(this IEnumerable<TSource> first, IEnumerable<TSource> second) => Contains(first, second, null);
+
+        /// <summary>
+        /// Determines whether two sequences are equal by comparing their elements by using a specified <paramref name="predicate"/>.
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="first">An <see cref="IEnumerable{T}"/> to compare to second.</param>
+        /// <param name="second">An <see cref="IEnumerable{T}"/> to compare to the first sequence.</param>
+        /// <param name="predicate">The equality predicate for sequence elements.</param>
+        /// <returns>
+        /// <c>true</c> if the two source sequences are of equal length and
+        /// their corresponding elements are equal according to a specified <paramref name="predicate"/>;
+        /// otherwise, <c>false</c>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException"><paramref name="first"/>, <paramref name="second"/> or <paramref name="predicate"/> is null.</exception>
+        public static bool SequenceEqual<TSource>(this IEnumerable<TSource> first, IEnumerable<TSource> second, Func<TSource, TSource, bool> predicate)
+        {
+            if (first == null)
+                throw new ArgumentNullException(nameof(first));
+            if (second == null)
+                throw new ArgumentNullException(nameof(second));
+            if (predicate == null)
+                throw new ArgumentNullException(nameof(predicate));
+
+            using (var e1 = first.GetEnumerator())
+            using (var e2 = second.GetEnumerator())
+            {
+                while (e1.MoveNext())
+                {
+                    if (!e2.MoveNext())
+                        return false;
+                    if (!predicate(e1.Current, e2.Current))
+                        return false;
+                }
+                if (e2.MoveNext())
+                    return false;
+            }
+
+            return true;
         }
     }
 }
---
+++ Source/Gapotchenko.FX.Linq/EnumeratorExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gapotchenko.FX.Linq
+{
+    /// <summary>
+    /// Enumerator extensions.
+    /// </summary>
+    public static class EnumeratorExtensions
+    {
+        /// <summary>
+        /// Returns the enumeration of remaining elements for the given enumerator.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in enumerator.</typeparam>
+        /// <param name="enumerator">The enumerator.</param>
+        /// <returns>An <see cref="System.Collections.Generic.IEnumerable{T}"/> that contains the remaining elements of the given enumerator.</returns>
+        public static IEnumerable<T> Rest<T>(this IEnumerator<T> enumerator)
+        {
+            if (enumerator == null)
+                throw new ArgumentNullException(nameof(enumerator));
+            while (enumerator.MoveNext())
+                yield return enumerator.Current;
+        }
+    }
+}
