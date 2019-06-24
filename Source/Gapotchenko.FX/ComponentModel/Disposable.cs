147baa6cc6ef137cb205cee496953a9bcd3c3bd0
hrumhurum <alex.s.gap@gmail.com>
2019-06-25 01:40:34 +0300

    The stateful part of Disposable class is separated into DisposableBase class.

--- Source/Gapotchenko.FX/ComponentModel/Disposable.cs
+++ Source/Gapotchenko.FX/ComponentModel/Disposable.cs
@@ -8,38 +8,12 @@ using System.Threading;
 namespace Gapotchenko.FX.ComponentModel
 {
     /// <summary>
-    /// Defines a base class for disposable objects that implement a finalizer pattern.
+    /// Provides methods for manipulating the disposable objects.
+    /// May also server as a base class for internal disposable objects that implement a finalizer pattern,
+    /// but <see cref="DisposableBase"/> is usually more suitable for that role.
     /// </summary>
-    public abstract class Disposable : IDisposable
+    public abstract class Disposable : DisposableBase
     {
-        /// <summary>
-        /// Finalizes the instance of <see cref="Disposable"/> class.
-        /// </summary>
-        ~Disposable()
-        {
-            Dispose(false);
-        }
-
-        /// <summary>
-        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
-        /// </summary>
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-
-        /// <summary>
-        /// Releases the unmanaged resources and optionally releases the managed resources.
-        /// </summary>
-        /// <param name="disposing">
-        /// <c>true</c> to release both managed and unmanaged resources;
-        /// <c>false</c> to release only unmanaged resources.
-        /// </param>
-        protected virtual void Dispose(bool disposing)
-        {
-        }
-
         /// <summary>
         /// Tries to clear a disposable object at the specified reference
         /// by calling <see cref="IDisposable.Dispose"/> method and setting its value to <c>null</c>.
---
+++ Source/Gapotchenko.FX/ComponentModel/DisposableBase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gapotchenko.FX.ComponentModel
+{
+    /// <summary>
+    /// Defines a base class for disposable objects that implement a finalizer pattern.
+    /// </summary>
+    public abstract class DisposableBase : IDisposable
+    {
+        /// <summary>
+        /// Finalizes the instance of <see cref="Disposable"/> class.
+        /// </summary>
+        ~DisposableBase()
+        {
+            Dispose(false);
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Releases the unmanaged resources and optionally releases the managed resources.
+        /// </summary>
+        /// <param name="disposing">
+        /// <c>true</c> to release both managed and unmanaged resources;
+        /// <c>false</c> to release only unmanaged resources.
+        /// </param>
+        protected virtual void Dispose(bool disposing)
+        {
+        }
+    }
+}
