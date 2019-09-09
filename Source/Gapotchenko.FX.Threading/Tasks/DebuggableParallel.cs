ed77ce9c4c8bc548cd3b00b3e9376729d972e61a
hrumhurum <alex.s.gap@gmail.com>
2019-09-10 01:28:25 +0300

    Introduced 'DebuggableParallel' primitive in Gapotchenko.FX.Threading module. The primitive is a drop-in replacement for System.Threading.Tasks.Parallel and is useful for writing debugging-friendly code.

---
+++ Source/Gapotchenko.FX.Threading/Tasks/DebuggableParallel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gapotchenko.FX.Threading.Tasks
+{
+    /// <summary>
+    /// Provides a drop-in replacement for <see cref="Parallel"/> class that performs a sequential execution of operations when the debugger is attached and parallel execution otherwise.
+    /// <see cref="DebuggableParallel"/> is used for writing a debugging-friendly code.
+    /// </summary>
+    public static class DebuggableParallel
+    {
+        /// <summary>
+        /// Gets or sets the mode of operation.
+        /// </summary>
+        public static DebuggableParallelMode Mode { get; set; }
+
+        static bool IsParallel
+        {
+            get
+            {
+                switch (Mode)
+                {
+                    default:
+                    case DebuggableParallelMode.Auto:
+                        return !Debugger.IsAttached;
+
+                    case DebuggableParallelMode.AlwaysSequential:
+                        return false;
+
+                    case DebuggableParallelMode.AwaysParallel:
+                        return true;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Executes a <c>foreach</c> (<c>For Each</c> in Visual Basic) operation on an <see cref="IEnumerable{T}"/> in which iterations are run sequentially.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the data in the source.</typeparam>
+        /// <param name="source">An enumerable data source.</param>
+        /// <param name="body">The delegate that is invoked once per iteration.</param>
+        /// <returns>A structure that contains information about which portion of the loop completed.</returns>
+        public static ParallelLoopResult ForEach<TSource>(IEnumerable<TSource> source, Action<TSource> body) =>
+            IsParallel ?
+                Parallel.ForEach(source, body) :
+                Sequential.ForEach(source, body);
+
+        /// <summary>
+        /// Executes a <c>for</c> (<c>For</c> in Visual Basic) loop in which iterations are run sequentially.
+        /// </summary>
+        /// <param name="fromInclusive">The start index, inclusive.</param>
+        /// <param name="toExclusive">The end index, exclusive.</param>
+        /// <param name="body">The delegate that is invoked once per iteration.</param>
+        /// <returns>A structure that contains information about which portion of the loop completed.</returns>
+        public static ParallelLoopResult For(int fromInclusive, int toExclusive, Action<int> body) =>
+            IsParallel ?
+                Parallel.For(fromInclusive, toExclusive, body) :
+                Sequential.For(fromInclusive, toExclusive, body);
+
+        /// <summary>
+        /// Executes each of the provided actions, sequentially.
+        /// </summary>
+        /// <param name="actions">An array of <see cref="Action"/> to execute.</param>
+        public static void Invoke(params Action[] actions)
+        {
+            if (IsParallel)
+                Parallel.Invoke(actions);
+            else
+                Sequential.Invoke(actions);
+        }
+    }
+}
---
+++ Source/Gapotchenko.FX.Threading/Tasks/DebuggableParallelMode.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gapotchenko.FX.Threading.Tasks
+{
+    /// <summary>
+    /// Defines modes of operation for <see cref="DebuggableParallel"/> primitive.
+    /// </summary>
+    public enum DebuggableParallelMode
+    {
+        /// <summary>
+        /// The sequential or parallel execution is automatically selected based on presence of an attached debugger.
+        /// This is the default mode.
+        /// </summary>
+        Auto,
+
+        /// <summary>
+        /// <see cref="DebuggableParallel"/> always executes operations sequentially.
+        /// </summary>
+        AlwaysSequential,
+
+        /// <summary>
+        /// <see cref="DebuggableParallel"/> always executes operations in parallel.
+        /// </summary>
+        AwaysParallel
+    }
+}
