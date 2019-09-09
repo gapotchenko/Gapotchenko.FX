7febbce1e21d7544e159c8e88dab1646d78b54c0
hrumhurum <alex.s.gap@gmail.com>
2019-09-09 16:15:20 +0300

    Refactoring.

--- Source/Gapotchenko.FX/BitConverterEx.cs
+++ Source/Gapotchenko.FX/BitConverterEx.cs
@@ -8,11 +8,7 @@ namespace Gapotchenko.FX
 {
     static class BitConverterEx
     {
-#if TFF_BITCONVERTER_SINGLETOINT32BITS
-        public static int SingleToInt32Bits(float value) => BitConverter.SingleToInt32Bits(value);
-
-        public static float Int32BitsToSingle(int value) => BitConverter.Int32BitsToSingle(value);
-#else
+#if !TFF_BITCONVERTER_SINGLETOINT32BITS
         [StructLayout(LayoutKind.Explicit)]
         struct ReinterpretCastGround32
         {
@@ -22,20 +18,28 @@ namespace Gapotchenko.FX
             [FieldOffset(0)]
             public float Single;
         }
+#endif
 
         public static int SingleToInt32Bits(float value)
         {
+#if TFF_BITCONVERTER_SINGLETOINT32BITS
+            return BitConverter.SingleToInt32Bits(value);
+#else
             var rcg = new ReinterpretCastGround32();
             rcg.Single = value;
             return rcg.Int32;
+#endif
         }
 
         public static float Int32BitsToSingle(int value)
         {
+#if TFF_BITCONVERTER_SINGLETOINT32BITS
+            return BitConverter.Int32BitsToSingle(value);
+#else
             var rcg = new ReinterpretCastGround32();
             rcg.Int32 = value;
             return rcg.Single;
-        }
 #endif
+        }
     }
 }
