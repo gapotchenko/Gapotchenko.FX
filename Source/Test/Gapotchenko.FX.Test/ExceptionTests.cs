50fc899819faa750cbda904818ba64ac20b473e1
hrumhurum <alex.s.gap@gmail.com>
2019-10-29 18:22:04 +0200

    Added tests for exception classification.

---
+++ Source/Test/Gapotchenko.FX.Test/ExceptionTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Gapotchenko.FX.Test
+{
+    [TestClass]
+    public class ExceptionTests
+    {
+        [TestMethod]
+        public void Exception_ControlFlow_Basics()
+        {
+            Assert.IsTrue(new TaskCanceledException().IsCancellationException());
+            Assert.IsTrue(new OperationCanceledException().IsCancellationException());
+            Assert.IsTrue(new ThreadInterruptedException().IsCancellationException());
+           
+            Assert.IsFalse(new InvalidOperationException().IsCancellationException());
+        }
+    }
+}
