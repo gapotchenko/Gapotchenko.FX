0a0a602ff64a81aafd9b6d18849830d0312a4073
hrumhurum <alex.s.gap@gmail.com>
2019-11-02 02:27:05 +0200

    Improved testing coverage.

---
+++ Source/Test/Gapotchenko.FX.Test/ThreadingCapabilitiesTests.cs
@@ -0,0 +1,18 @@
+﻿using Gapotchenko.FX.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gapotchenko.FX.Test
+{
+    [TestClass]
+    public class ThreadingCapabilitiesTests
+    {
+        [TestMethod]
+        public void ThreadingCapabilities_LogicalProcessorCount()
+        {
+            Assert.AreEqual(Environment.ProcessorCount, ThreadingCapabilities.LogicalProcessorCount);
+        }
+    }
+}
