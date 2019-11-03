d37c586f50cef1997ea5bb302c94c1210d83e892
hrumhurum <alex.s.gap@gmail.com>
2019-11-03 13:45:19 +0200

    Refactoring.

--- Source/Gapotchenko.FX.Threading/Tasks/ExclusiveSynchronizationContext.cs
+++ Source/Gapotchenko.FX.Threading/Tasks/ExclusiveSynchronizationContext.cs
@@ -7,7 +7,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Gapotchenko.FX.Threading
+namespace Gapotchenko.FX.Threading.Tasks
 {
     sealed class ExclusiveSynchronizationContext : SynchronizationContext
     {
--- Source/Test/Gapotchenko.FX.Threading.Test/ThreadingCapabilitiesTests.cs
+++ Source/Test/Gapotchenko.FX.Threading.Test/ThreadingCapabilitiesTests.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Gapotchenko.FX.Test
+namespace Gapotchenko.FX.Threading.Test
 {
     [TestClass]
     public class ThreadingCapabilitiesTests
