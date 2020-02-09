a40cb5074f971c97cc45685563b2bcd591c16c97
hrumhurum <alex.s.gap@gmail.com>
2020-02-09 07:30:30 +0200

    XPointer implementation in  progress.

---
+++ Source/Test/Data/Gapotchenko.FX.Data.Xml.Test/XPointerTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Gapotchenko.FX.Data.Xml.Test
+{
+    [TestClass]
+    public class XPointerTests
+    {
+        [TestMethod]
+        public void XPointer_Test1()
+        {
+        }
+    }
+}
---
+++ Source/Test/Data/README.md
@@ -0,0 +1,1 @@
+`Test/Data` folder contains quality assurance (QA) projects for Gapotchenko.FX modules specialized in data access and serialization.
