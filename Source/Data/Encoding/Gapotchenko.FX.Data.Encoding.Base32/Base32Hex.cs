8c05138400520fcea0c87a604bb83e5abbdd3c30
hrumhurum <alex.s.gap@gmail.com>
2020-02-15 07:54:43 +0200

    Gapotchenko.FX.Data.Encoding in progress.

---
+++ Source/Data/Encoding/Gapotchenko.FX.Data.Encoding.Base32/Base32Hex.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace Gapotchenko.FX.Data.Encoding
+{
+    /// <summary>
+    /// base32-hex encoding conforming to RFC 4648.
+    /// </summary>
+    public class Base32Hex : Base32
+    {
+        /// <summary>
+        /// Initializes a new instance of <see cref="Base32Hex"/> class.
+        /// </summary>
+        protected Base32Hex() :
+            base("0123456789ABCDEFGHIJKLMNOPQRSTUV")
+        {
+        }
+
+        /// <inheritdoc/>
+        public override string Name => "base32-hex";
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        static volatile IDataTextEncoding m_Instance;
+
+        /// <summary>
+        /// Returns a default instance of <see cref="Base32Hex"/> encoding.
+        /// </summary>
+        [EditorBrowsable(EditorBrowsableState.Advanced)]
+        public new static IDataTextEncoding Instance
+        {
+            get
+            {
+                if (m_Instance == null)
+                    m_Instance = new Base32Hex();
+                return m_Instance;
+            }
+        }
+    }
+}
