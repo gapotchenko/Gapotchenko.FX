4a0c1370419e83776b2347947b6761ae9ea4c6f8
hrumhurum <alex.s.gap@gmail.com>
2020-03-03 07:05:00 +0200

    Refactoring.

--- Source/Data/Encoding/Gapotchenko.FX.Data.Encoding.Base64/IBase64.cs
+++ Source/Data/Encoding/Gapotchenko.FX.Data.Encoding.Base64/IBase64.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Gapotchenko.FX.Data.Encoding
+﻿namespace Gapotchenko.FX.Data.Encoding
 {
     /// <summary>
     /// A unifying interface for all possible variations of Base64 encoding.
