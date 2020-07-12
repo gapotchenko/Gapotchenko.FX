58dad0581b184d0494a981df3085538dba8a5ddf
hrumhurum <alex.s.gap@gmail.com>
2020-07-12 18:33:27 +0300

    Isolated issue with large (>32767) environment blocks on Windows.

--- Source/Test/Gapotchenko.FX.Diagnostics.Process.Test/ProcessEnvironmentTests.cs
+++ Source/Test/Gapotchenko.FX.Diagnostics.Process.Test/ProcessEnvironmentTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections;
 
 namespace Gapotchenko.FX.Diagnostics.Process.Test
 {
@@ -25,6 +26,37 @@ namespace Gapotchenko.FX.Diagnostics.Process.Test
             {
                 var env = process.ReadEnvironmentVariables();
                 Assert.AreEqual(value, env["PROC_ENV_TEST"]);
+            }
+            finally
+            {
+                process.Kill(true);
+            }
+        }
+
+        [TestMethod]
+        public void ProcEnv_ReadLarge()
+        {
+            var process = new Process();
+
+            var psi = process.StartInfo;
+            psi.FileName = "dotnet";
+            psi.Arguments = "fsi";
+
+            var expectedEnv = psi.EnvironmentVariables;
+            expectedEnv.Clear();
+
+            for (int i = 0; i < 1000; ++i)
+            {
+                string value = Guid.NewGuid().ToString("D");
+                expectedEnv["PROC_ENV_TEST_" + i] = value;
+            }
+
+            Assert.IsTrue(process.Start());
+            try
+            {
+                var actualEnv = process.ReadEnvironmentVariables();
+                foreach (DictionaryEntry i in expectedEnv)
+                    Assert.AreEqual((string)i.Value, actualEnv[(string)i.Key]);
             }
             finally
             {
