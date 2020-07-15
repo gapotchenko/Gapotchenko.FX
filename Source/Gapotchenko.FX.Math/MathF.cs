0d60675601c8d561a37a50b94d05cb08f8dcadd8
hrumhurum <alex.s.gap@gmail.com>
2020-07-15 14:06:56 +0300

    MathF polyfill in progress.

---
+++ Source/Gapotchenko.FX.Math/MathF.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+#if !TFF_MATHF
+
+namespace System
+{
+    /// <summary>
+    /// <para>
+    /// Provides constants and static methods for trigonometric, logarithmic, and other common mathematical functions.
+    /// </para>
+    /// <para>
+    /// This is a polyfill provided by Gapotchenko.FX.
+    /// </para>
+    /// </summary>
+    public static class MathF
+    {
+        /// <summary>
+        /// Represents the natural logarithmic base, specified by the constant, e.
+        /// </summary>
+        public const float E = (float)Math.E;
+
+        /// <summary>
+        /// Represents the ratio of the circumference of a circle to its diameter, specified by the constant, π.
+        /// </summary>
+        public const float PI = (float)Math.PI;
+    }
+}
+
+#else
+
+[assembly: TypeForwardedTo(typeof(MathF))]
+
+#endif
