6c0c6393394d7968211e2d7c08f54be3d38cf602
hrumhurum <alex.s.gap@gmail.com>
2020-07-19 19:59:59 +0300

    Refactoring.

--- Source/Gapotchenko.FX.Drawing/RegistryExtensions.cs
+++ Source/Gapotchenko.FX.Drawing/RegistryExtensions.cs
@@ -2,10 +2,6 @@
 
 using Microsoft.Win32;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Gapotchenko.FX.Drawing
 {
