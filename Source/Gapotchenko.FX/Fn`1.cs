85c9d85c4397c99817dd98e05c4df272735cd24e
hrumhurum <alex.s.gap@gmail.com>
2020-07-23 20:30:49 +0300

    Refactoring.

--- Source/Gapotchenko.FX/Fn`1.cs
+++ Source/Gapotchenko.FX/Fn`1.cs
@@ -8,20 +8,14 @@ namespace Gapotchenko.FX
     /// <typeparam name="T">The type to provide lambda calculus and functional composition primitives for.</typeparam>
     public static class Fn<T>
     {
-        static class Factory
-        {
-            public static readonly Func<T> Default = Fn.Default<T>;
-            public static readonly Func<T, T> Identity = Fn.Identity;
-        }
-
         /// <summary>
         /// Gets a delegate to a pure function that returns a default value of type <typeparamref name="T"/>, e.g. f() = default(T).
         /// </summary>
-        public static Func<T> Default { get; } = Factory.Default;
+        public static Func<T> Default { get; } = Fn.Default<T>;
 
         /// <summary>
         /// Gets a delegate to a pure identity function that returns a value of its single argument, e.g. f(x) = x.
         /// </summary>
-        public static Func<T, T> Identity { get; } = Factory.Identity;
+        public static Func<T, T> Identity { get; } = Fn.Identity;
     }
 }
