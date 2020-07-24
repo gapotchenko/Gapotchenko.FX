8dab0bb703aa5e3f2c2041829f4e2bf0cb4d49e4
hrumhurum <alex.s.gap@gmail.com>
2020-07-25 01:17:06 +0300

    Refactoring.

--- Source/Gapotchenko.FX/Fn.Expression.Create.cs
+++ Source/Gapotchenko.FX/Fn.Expression.Create.cs
@@ -3,7 +3,6 @@ using System.Diagnostics.CodeAnalysis;
 using System.Linq.Expressions;
 
 #nullable enable
-#pragma warning disable CS8602 // Dereference of a possibly null reference.
 
 namespace Gapotchenko.FX
 {
@@ -18,7 +17,7 @@ namespace Gapotchenko.FX
         public static Expression<Action>? Expression(Action? action) =>
             action == null ?
                 (Expression<Action>?)null :
-                () => action();
+                () => action!();
 
         /// <summary>
         /// Creates lambda expression for a specified action.
@@ -30,7 +29,7 @@ namespace Gapotchenko.FX
         public static Expression<Action<T>>? Expression<T>(Action<T>? action) =>
             action == null ?
                 (Expression<Action<T>>?)null :
-                (val) => action(val);
+                (val) => action!(val);
 
         /// <summary>
         /// Creates lambda expression for a specified action.
@@ -43,7 +42,7 @@ namespace Gapotchenko.FX
         public static Expression<Action<T1, T2>>? Expression<T1, T2>(Action<T1, T2>? action) =>
             action == null ?
                 (Expression<Action<T1, T2>>?)null :
-                (val1, val2) => action(val1, val2);
+                (val1, val2) => action!(val1, val2);
 
         /// <summary>
         /// Creates lambda expression for a specified action.
@@ -57,7 +56,7 @@ namespace Gapotchenko.FX
         public static Expression<Action<T1, T2, T3>>? Expression<T1, T2, T3>(Action<T1, T2, T3>? action) =>
             action == null ?
                 (Expression<Action<T1, T2, T3>>?)null :
-                (val1, val2, val3) => action(val1, val2, val3);
+                (val1, val2, val3) => action!(val1, val2, val3);
 
         /// <summary>
         /// Creates lambda expression for a specified action.
@@ -72,7 +71,7 @@ namespace Gapotchenko.FX
         public static Expression<Action<T1, T2, T3, T4>>? Expression<T1, T2, T3, T4>(Action<T1, T2, T3, T4>? action) =>
             action == null ?
                 (Expression<Action<T1, T2, T3, T4>>?)null :
-                (val1, val2, val3, val4) => action(val1, val2, val3, val4);
+                (val1, val2, val3, val4) => action!(val1, val2, val3, val4);
 
         /// <summary>
         /// Creates lambda expression for a specified action.
@@ -88,7 +87,7 @@ namespace Gapotchenko.FX
         public static Expression<Action<T1, T2, T3, T4, T5>>? Expression<T1, T2, T3, T4, T5>(Action<T1, T2, T3, T4, T5>? action) =>
             action == null ?
                 (Expression<Action<T1, T2, T3, T4, T5>>?)null :
-                (val1, val2, val3, val4, val5) => action(val1, val2, val3, val4, val5);
+                (val1, val2, val3, val4, val5) => action!(val1, val2, val3, val4, val5);
 
         /// <summary>
         /// Creates lambda expression for a specified action.
@@ -105,7 +104,7 @@ namespace Gapotchenko.FX
         public static Expression<Action<T1, T2, T3, T4, T5, T6>>? Expression<T1, T2, T3, T4, T5, T6>(Action<T1, T2, T3, T4, T5, T6>? action) =>
             action == null ?
                 (Expression<Action<T1, T2, T3, T4, T5, T6>>?)null :
-                (val1, val2, val3, val4, val5, val6) => action(val1, val2, val3, val4, val5, val6);
+                (val1, val2, val3, val4, val5, val6) => action!(val1, val2, val3, val4, val5, val6);
 
         /// <summary>
         /// Creates lambda expression for a specified action.
@@ -123,7 +122,7 @@ namespace Gapotchenko.FX
         public static Expression<Action<T1, T2, T3, T4, T5, T6, T7>>? Expression<T1, T2, T3, T4, T5, T6, T7>(Action<T1, T2, T3, T4, T5, T6, T7>? action) =>
             action == null ?
                 (Expression<Action<T1, T2, T3, T4, T5, T6, T7>>?)null :
-                (val1, val2, val3, val4, val5, val6, val7) => action(val1, val2, val3, val4, val5, val6, val7);
+                (val1, val2, val3, val4, val5, val6, val7) => action!(val1, val2, val3, val4, val5, val6, val7);
 
         /// <summary>
         /// Creates lambda expression for a specified action.
@@ -142,7 +141,7 @@ namespace Gapotchenko.FX
         public static Expression<Action<T1, T2, T3, T4, T5, T6, T7, T8>>? Expression<T1, T2, T3, T4, T5, T6, T7, T8>(Action<T1, T2, T3, T4, T5, T6, T7, T8>? action) =>
             action == null ?
                 (Expression<Action<T1, T2, T3, T4, T5, T6, T7, T8>>?)null :
-                (val1, val2, val3, val4, val5, val6, val7, val8) => action(val1, val2, val3, val4, val5, val6, val7, val8);
+                (val1, val2, val3, val4, val5, val6, val7, val8) => action!(val1, val2, val3, val4, val5, val6, val7, val8);
 
         /// <summary>
         /// Creates lambda expression for a specified function.
@@ -154,7 +153,7 @@ namespace Gapotchenko.FX
         public static Expression<Func<TResult>>? Expression<TResult>(Func<TResult>? func) =>
             func == null ?
                 (Expression<Func<TResult>>?)null :
-                () => func();
+                () => func!();
 
         /// <summary>
         /// Creates lambda expression for a specified function.
@@ -167,7 +166,7 @@ namespace Gapotchenko.FX
         public static Expression<Func<T, TResult>>? Expression<T, TResult>(Func<T, TResult>? func) =>
             func == null ?
                 (Expression<Func<T, TResult>>?)null :
-                (val) => func(val);
+                (val) => func!(val);
 
         /// <summary>
         /// Creates lambda expression for a specified function.
@@ -181,7 +180,7 @@ namespace Gapotchenko.FX
         public static Expression<Func<T1, T2, TResult>>? Expression<T1, T2, TResult>(Func<T1, T2, TResult>? func) =>
             func == null ?
                 (Expression<Func<T1, T2, TResult>>?)null :
-                (val1, val2) => func(val1, val2);
+                (val1, val2) => func!(val1, val2);
 
         /// <summary>
         /// Creates lambda expression for a specified function.
@@ -196,7 +195,7 @@ namespace Gapotchenko.FX
         public static Expression<Func<T1, T2, T3, TResult>>? Expression<T1, T2, T3, TResult>(Func<T1, T2, T3, TResult>? func) =>
             func == null ?
                 (Expression<Func<T1, T2, T3, TResult>>?)null :
-                (val1, val2, val3) => func(val1, val2, val3);
+                (val1, val2, val3) => func!(val1, val2, val3);
 
         /// <summary>
         /// Creates lambda expression for a specified function.
@@ -212,7 +211,7 @@ namespace Gapotchenko.FX
         public static Expression<Func<T1, T2, T3, T4, TResult>>? Expression<T1, T2, T3, T4, TResult>(Func<T1, T2, T3, T4, TResult>? func) =>
             func == null ?
                 (Expression<Func<T1, T2, T3, T4, TResult>>?)null :
-                (val1, val2, val3, val4) => func(val1, val2, val3, val4);
+                (val1, val2, val3, val4) => func!(val1, val2, val3, val4);
 
         /// <summary>
         /// Creates lambda expression for a specified function.
@@ -229,7 +228,7 @@ namespace Gapotchenko.FX
         public static Expression<Func<T1, T2, T3, T4, T5, TResult>>? Expression<T1, T2, T3, T4, T5, TResult>(Func<T1, T2, T3, T4, T5, TResult>? func) =>
             func == null ?
                 (Expression<Func<T1, T2, T3, T4, T5, TResult>>?)null :
-                (val1, val2, val3, val4, val5) => func(val1, val2, val3, val4, val5);
+                (val1, val2, val3, val4, val5) => func!(val1, val2, val3, val4, val5);
 
         /// <summary>
         /// Creates lambda expression for a specified function.
@@ -247,7 +246,7 @@ namespace Gapotchenko.FX
         public static Expression<Func<T1, T2, T3, T4, T5, T6, TResult>>? Expression<T1, T2, T3, T4, T5, T6, TResult>(Func<T1, T2, T3, T4, T5, T6, TResult>? func) =>
             func == null ?
                 (Expression<Func<T1, T2, T3, T4, T5, T6, TResult>>?)null :
-                (val1, val2, val3, val4, val5, val6) => func(val1, val2, val3, val4, val5, val6);
+                (val1, val2, val3, val4, val5, val6) => func!(val1, val2, val3, val4, val5, val6);
 
         /// <summary>
         /// Creates lambda expression for a specified function.
@@ -266,7 +265,7 @@ namespace Gapotchenko.FX
         public static Expression<Func<T1, T2, T3, T4, T5, T6, T7, TResult>>? Expression<T1, T2, T3, T4, T5, T6, T7, TResult>(Func<T1, T2, T3, T4, T5, T6, T7, TResult>? func) =>
             func == null ?
                 (Expression<Func<T1, T2, T3, T4, T5, T6, T7, TResult>>?)null :
-                (val1, val2, val3, val4, val5, val6, val7) => func(val1, val2, val3, val4, val5, val6, val7);
+                (val1, val2, val3, val4, val5, val6, val7) => func!(val1, val2, val3, val4, val5, val6, val7);
 
         /// <summary>
         /// Creates lambda expression for a specified function.
@@ -286,6 +285,6 @@ namespace Gapotchenko.FX
         public static Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, TResult>>? Expression<T1, T2, T3, T4, T5, T6, T7, T8, TResult>(Func<T1, T2, T3, T4, T5, T6, T7, T8, TResult>? func) =>
             func == null ?
                 (Expression<Func<T1, T2, T3, T4, T5, T6, T7, T8, TResult>>?)null :
-                (val1, val2, val3, val4, val5, val6, val7, val8) => func(val1, val2, val3, val4, val5, val6, val7, val8);
+                (val1, val2, val3, val4, val5, val6, val7, val8) => func!(val1, val2, val3, val4, val5, val6, val7, val8);
     }
 }
