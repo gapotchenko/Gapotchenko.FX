e3b6f4928b5923b97de81762f34163536ce9666e
hrumhurum <alex.s.gap@gmail.com>
2020-07-25 03:04:24 +0300

    Release preparation.

---
+++ Source/Gapotchenko.FX.AppModel.Information/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("25c8cf2b-1c4b-44f4-9fa8-550085ede271")]
+
+[assembly: CLSCompliant(true)]
