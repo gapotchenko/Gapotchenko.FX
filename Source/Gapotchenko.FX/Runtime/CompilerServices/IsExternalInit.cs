bf00706a9fbf5d6a0674060709bbaf26e1fd8cd9
hrumhurum <alex.s.gap@gmail.com>
2020-10-23 17:30:34 +0300

    Added .NET 5.0 target. Added IsExternalInit polyfill for init-only properties.

---
+++ Source/Gapotchenko.FX/Runtime/CompilerServices/IsExternalInit.cs
@@ -0,0 +1,28 @@
+﻿#if !TFF_ISEXTERNALINIT
+
+using System.ComponentModel;
+
+namespace System.Runtime.CompilerServices
+{
+    /// <summary>
+    /// <para>
+    /// Reserved to be used by the compiler for tracking metadata.
+    /// This class should not be used by developers in source code.
+    /// </para>
+    /// <para>
+    /// This is a polyfill provided by Gapotchenko.FX.
+    /// </para>
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static class IsExternalInit
+    {
+    }
+}
+
+#else
+
+using System.Runtime.CompilerServices;
+
+[assembly: TypeForwardedTo(typeof(IsExternalInit))]
+
+#endif
