9e36d3f23d6cd973c34b234067e21fa5e55ca296
hrumhurum <alex.s.gap@gmail.com>
2020-11-05 21:57:50 +0200

    Implemented string.Contains(char) polyfill.

--- Source/Gapotchenko.FX.Text/StringExtensions.cs
+++ Source/Gapotchenko.FX.Text/StringExtensions.cs
@@ -52,5 +52,31 @@ namespace Gapotchenko.FX.Text
             int n = s.Length;
             return n != 0 && s[n - 1] == value;
         }
+
+        /// <summary>
+        /// <para>
+        /// Returns a value indicating whether a specified character occurs within this string.
+        /// </para>
+        /// <para>
+        /// This is a polyfill provided by Gapotchenko.FX.
+        /// </para>
+        /// </summary>
+        /// <param name="s">The string instance.</param>
+        /// <param name="value">The character to seek.</param>
+        /// <returns><c>true</c> if the <paramref name="value parameter"/> occurs within this string; otherwise, <c>false</c>.</returns>
+#if TFF_STRING_CONTAINS_CHAR
+        [EditorBrowsable(EditorBrowsableState.Never)]
+#endif
+        public static bool Contains(this string s, char value)
+        {
+            if (s == null)
+                throw new ArgumentNullException(nameof(s));
+
+#if TFF_STRING_CONTAINS_CHAR
+            return s.Contains(value);
+#else
+            return s.IndexOf(value) != -1;
+#endif
+        }
     }
 }
