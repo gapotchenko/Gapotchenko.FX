6a9809478fae113aec8cd33bd791841a85fa0626
hrumhurum <alex.s.gap@gmail.com>
2020-11-19 18:41:21 +0200

    Added tests for Gapotchenko.FX.Math.Combinatorics module.

---
+++ Source/Tests/Gapotchenko.FX.Math.Combinatorics.Tests/CartesianProductTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using Gapotchenko.FX.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Gapotchenko.FX.Math.Combinatorics.Tests
+{
+    [TestClass]
+    public class CartesianProductTests
+    {
+        [TestMethod]
+        public void CartesianProduct_Of2x3Elements()
+        {
+            var factors = new[]
+            {
+                new[] { 1, 2 },
+                new[] { 5, 6, 7 }
+            };
+
+            int cardinality = CartesianProduct.Cardinality(factors.Select(x => x.Length));
+            Assert.AreEqual(6, cardinality);
+
+            var p = CartesianProduct.Generate(factors).AsReadOnly();
+
+            Assert.AreEqual(cardinality, p.Count);
+
+            Assert.IsTrue(p[0].SequenceEqual(new[] { 1, 5 }));
+            Assert.IsTrue(p[1].SequenceEqual(new[] { 2, 5 }));
+            Assert.IsTrue(p[2].SequenceEqual(new[] { 1, 6 }));
+            Assert.IsTrue(p[3].SequenceEqual(new[] { 2, 6 }));
+            Assert.IsTrue(p[4].SequenceEqual(new[] { 1, 7 }));
+            Assert.IsTrue(p[5].SequenceEqual(new[] { 2, 7 }));
+        }
+
+        [TestMethod]
+        public void CartesianProduct_Of2x0Elements()
+        {
+            var factors = new[]
+            {
+                new[] { 1, 2 },
+                new int[] { }
+            };
+
+            int cardinality = CartesianProduct.Cardinality(factors.Select(x => x.Length));
+            Assert.AreEqual(0, cardinality);
+
+            var p = CartesianProduct.Generate(factors);
+            Assert.AreEqual(cardinality, p.Count());
+        }
+    }
+}
---
+++ Source/Tests/Gapotchenko.FX.Math.Combinatorics.Tests/PermutationTests.cs
@@ -0,0 +1,56 @@
+﻿using Gapotchenko.FX.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace Gapotchenko.FX.Math.Combinatorics.Tests
+{
+    [TestClass]
+    public class PermutationTests
+    {
+        [TestMethod]
+        public void Permutations_Of2Elements()
+        {
+            var source = new[] { 1, 2 };
+
+            int cardinality = Permutations.Cardinality(source.Length);
+            Assert.AreEqual(2, cardinality);
+
+            var p = Permutations.Generate(source).AsReadOnly();
+            Assert.AreEqual(cardinality, p.Count);
+
+            var s = p
+                .Select(x => x.ToArray())
+                .ToHashSet(ArrayEqualityComparer<int>.Default);
+
+            Assert.AreEqual(cardinality, s.Count);
+
+            Assert.IsTrue(s.Contains(new[] { 1, 2 }));
+            Assert.IsTrue(s.Contains(new[] { 2, 1 }));
+        }
+
+        [TestMethod]
+        public void Permutations_Of3Elements()
+        {
+            var source = new[] { 1, 2, 3 };
+
+            int cardinality = Permutations.Cardinality(source.Length);
+            Assert.AreEqual(6, cardinality);
+
+            var p = Permutations.Generate(source).AsReadOnly();
+            Assert.AreEqual(cardinality, p.Count);
+
+            var s = p
+                .Select(x => x.ToArray())
+                .ToHashSet(ArrayEqualityComparer<int>.Default);
+
+            Assert.AreEqual(cardinality, s.Count);
+
+            Assert.IsTrue(s.Contains(new[] { 1, 2, 3 }));
+            Assert.IsTrue(s.Contains(new[] { 1, 3, 2 }));
+            Assert.IsTrue(s.Contains(new[] { 2, 1, 3 }));
+            Assert.IsTrue(s.Contains(new[] { 2, 3, 1 }));
+            Assert.IsTrue(s.Contains(new[] { 3, 1, 2 }));
+            Assert.IsTrue(s.Contains(new[] { 3, 2, 1 }));
+        }
+    }
+}
