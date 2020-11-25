12c8b1515f99fb6681cb31a8214549484901c671
hrumhurum <alex.s.gap@gmail.com>
2020-11-25 03:18:46 +0200

    Improved Cartesian product efficiency for correlated multipliers.

--- Source/Gapotchenko.FX.Math.Combinatorics/CartesianProduct.Kernel.cs
+++ Source/Gapotchenko.FX.Math.Combinatorics/CartesianProduct.Kernel.cs
@@ -7,14 +7,16 @@ namespace Gapotchenko.FX.Math.Combinatorics
 {
     partial class CartesianProduct
     {
-        static IEnumerable<Row<T>> Multiply<T>(IEnumerable<IEnumerable<T>> factors)
+        static IEnumerable<IRow<T>> Multiply<T>(IEnumerable<IEnumerable<T>> factors)
         {
-            var items = factors.SelectExceptLast(EnumerableEx.Memoize).AsReadOnly();
-
+            var items = MemoizeMultipliers(factors).AsReadOnly();
             int rank = items.Count;
 
             if (rank == 0)
+            {
+                // No multipliers.
                 yield break;
+            }
 
             var enumerators = items.Select(x => x.GetEnumerator()).ToArray();
 
@@ -41,16 +43,54 @@ namespace Gapotchenko.FX.Math.Combinatorics
                     if (enumerator.MoveNext())
                         break;
 
+                    if (i == rank - 1)
+                        yield break;
+
+                    // Reset the enumerator by retrieving a new one.
                     var newEnumerator = items[i].GetEnumerator();
                     if (!newEnumerator.MoveNext())
                         throw new InvalidOperationException("Cartesian product pool is prematurely exhausted.");
-
                     enumerators[i] = newEnumerator;
-
-                    if (i == rank - 1)
-                        yield break;
                 }
             }
+        }
+
+        static IEnumerable<IEnumerable<T>> MemoizeMultipliers<T>(IEnumerable<IEnumerable<T>> factors)
+        {
+            var gtc = new GroupTransformCorrelator<IEnumerable<T>>(EnumerableEx.Memoize);
+            factors = factors.Memoize();
+            foreach (var factor in factors.SkipLast(1))
+                gtc.Transform(factor);
+            return factors.Select(x => gtc.Correlate(x));
+        }
+
+        static IEnumerable<IEnumerable<T>> DistinctMultipliers<T>(IEnumerable<IEnumerable<T>> factors, IEqualityComparer<T>? equalityComparer)
+        {
+            var gtc = new GroupTransformCorrelator<IEnumerable<T>>(x => Enumerable.Distinct(x, equalityComparer));
+            factors = factors.Memoize();
+            foreach (var factor in factors)
+                gtc.Transform(factor);
+            return factors.Select(x => gtc.Correlate(x));
+        }
+
+        sealed class GroupTransformCorrelator<T> where T : class
+        {
+            public GroupTransformCorrelator(Func<T, T> transform)
+            {
+                m_Transform = transform;
+                m_Map = new Dictionary<T, T>(ReferenceEqualityComparer.Instance);
+            }
+
+            readonly Func<T, T> m_Transform;
+            readonly Dictionary<T, T> m_Map;
+
+            public void Transform(T value)
+            {
+                if (!m_Map.ContainsKey(value))
+                    m_Map.Add(value, m_Transform(value));
+            }
+
+            public T Correlate(T value) => m_Map.TryGetValue(value, out var transformedValue) ? transformedValue : value;
         }
 
         internal static IEnumerable<TResult> Multiply<TFirst, TSecond, TResult>(
