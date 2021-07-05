82dc8767178319dc0cbdb7cacc0e44b0d5816ef0
hrumhurum <alex.s.gap@gmail.com>
2021-07-05 16:58:32 +0300

    LINQ polyfills in progress.

--- Source/Gapotchenko.FX.Linq/EnumerableEx.cs
+++ Source/Gapotchenko.FX.Linq/EnumerableEx.cs
@@ -80,6 +80,7 @@ namespace Gapotchenko.FX.Linq
         /// <param name="source">An <see cref="IEnumerable{T}"/> to create a <see cref="HashSet{T}"/> from.</param>
         /// <returns>A <see cref="HashSet{T}"/> that contains values of type <typeparamref name="TSource"/> retrieved from the <paramref name="source"/>.</returns>
 #if TFF_ENUMERABLE_TOHASHSET
+        [EditorBrowsable(EditorBrowsableState.Never)]
         public static HashSet<TSource> ToHashSet<TSource>(IEnumerable<TSource> source) => Enumerable.ToHashSet(source);
 #else
         public static HashSet<TSource> ToHashSet<TSource>(this IEnumerable<TSource> source) => ToHashSet(source, null);
@@ -98,6 +99,7 @@ namespace Gapotchenko.FX.Linq
         /// <param name="comparer">An <see cref="IEqualityComparer{T}"/> to compare keys.</param>
         /// <returns>A <see cref="HashSet{T}"/> that contains values of type <typeparamref name="TSource"/> retrieved from the <paramref name="source"/>.</returns>
 #if TFF_ENUMERABLE_TOHASHSET
+        [EditorBrowsable(EditorBrowsableState.Never)]
         public static HashSet<TSource> ToHashSet<TSource>(IEnumerable<TSource> source, IEqualityComparer<TSource>? comparer) => Enumerable.ToHashSet(source, comparer);
 #else
         public static HashSet<TSource> ToHashSet<TSource>(this IEnumerable<TSource> source, IEqualityComparer<TSource>? comparer)
@@ -106,6 +108,68 @@ namespace Gapotchenko.FX.Linq
                 throw new ArgumentNullException(nameof(source));
 
             return new HashSet<TSource>(source, comparer);
+        }
+#endif
+
+        /// <summary>
+        /// Returns the first element of a sequence, or the specified default value if the sequence contains no elements.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of source.</typeparam>
+        /// <param name="source">An <see cref="IEnumerable{T}"/> to return the first element of.</param>
+        /// <param name="defaultValue">The default value.</param>
+        /// <returns>
+        /// <paramref name="defaultValue"/> if source is empty;
+        /// otherwise, the first element in source.
+        /// </returns>
+#if TFF_ENUMERABLE_FIRSTORDEFAULT_VALUE
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public static TSource FirstOrDefault<TSource>(IEnumerable<TSource> source, TSource defaultValue) => Enumerable.FirstOrDefault(source, defaultValue);
+#else
+        public static TSource FirstOrDefault<TSource>(this IEnumerable<TSource> source, TSource defaultValue)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            using var enumerator = source.GetEnumerator();
+
+            if (!enumerator.MoveNext())
+            {
+                // The sequence is empty.
+                return defaultValue;
+            }
+
+            return enumerator.Current;
+        }
+#endif
+
+        /// <summary>
+        /// Returns the first element of the sequence that satisfies a condition or the specified default value if no such element is found.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of source.</typeparam>
+        /// <param name="source">An <see cref="IEnumerable{T}"/> to return the an element from.</param>
+        /// <param name="predicate">A function to test each element for a condition.</param>
+        /// <param name="defaultValue">The default value.</param>
+        /// <returns>
+        /// <paramref name="defaultValue"/> if source is empty or if no element passes the test specified by predicate;
+        /// otherwise, the first element in source that passes the test specified by predicate.
+        /// </returns>
+#if TFF_ENUMERABLE_FIRSTORDEFAULT_VALUE
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public static TSource FirstOrDefault<TSource>(IEnumerable<TSource> source, Func<TSource, bool> predicate, TSource defaultValue) =>
+            Enumerable.FirstOrDefault(source, predicate, defaultValue);
+#else
+        public static TSource FirstOrDefault<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate, TSource defaultValue)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            if (predicate == null)
+                throw new ArgumentNullException(nameof(predicate));
+
+            foreach (var i in source)
+                if (predicate(i))
+                    return i;
+
+            return defaultValue;
         }
 #endif
 
--- Source/Tests/Gapotchenko.FX.Linq.Tests/EnumerableExTests.cs
+++ Source/Tests/Gapotchenko.FX.Linq.Tests/EnumerableExTests.cs
@@ -145,6 +145,122 @@ namespace Gapotchenko.FX.Linq.Tests
             Check("c", "abc");
         }
 
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Linq_Enumerable_FirstOrDefault_Value_NullArg()
+        {
+            EnumerableEx.FirstOrDefault<int>(null!, 0);
+        }
+
+        [TestMethod]
+        public void Linq_Enumerable_FirstOrDefault_Value_Empty()
+        {
+            var seq = new int[0];
+            var result = seq.FirstOrDefault(10);
+            Assert.AreEqual(10, result);
+        }
+
+        [TestMethod]
+        public void Linq_Enumerable_FirstOrDefault_Value_Single()
+        {
+            var seq = new int[] { 1 };
+            var result = seq.FirstOrDefault(10);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Linq_Enumerable_FirstOrDefault_Value_Multiple()
+        {
+            var seq = new int[] { 1, 2 };
+            var result = seq.FirstOrDefault(10);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Linq_Enumerable_FirstOrDefault_Predicate_Value_NullSourceArg()
+        {
+            EnumerableEx.FirstOrDefault(null!, _ => true, 10);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Linq_Enumerable_FirstOrDefault_Predicate_Value_NullPredicateArg()
+        {
+            EnumerableEx.FirstOrDefault(new int[0], null!, 10);
+        }
+
+        [TestMethod]
+        public void Linq_Enumerable_FirstOrDefault_Predicate_Value_Empty()
+        {
+            var seq = new int[0];
+            var result = seq.FirstOrDefault(_ => true, 10);
+            Assert.AreEqual(10, result);
+        }
+
+        [TestMethod]
+        public void Linq_Enumerable_FirstOrDefault_Predicate_Value_Empty_NoMatch()
+        {
+            var seq = new int[0];
+            var result = seq.FirstOrDefault(_ => false, 10);
+            Assert.AreEqual(10, result);
+        }
+
+        [TestMethod]
+        public void Linq_Enumerable_FirstOrDefault_Predicate_Value_Single()
+        {
+            var seq = new int[] { 1 };
+            var result = seq.FirstOrDefault(_ => true, 10);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Linq_Enumerable_FirstOrDefault_Predicate_Value_Single_NoMatch()
+        {
+            var seq = new int[] { 1 };
+            var result = seq.FirstOrDefault(_ => false, 10);
+            Assert.AreEqual(10, result);
+        }
+
+        [TestMethod]
+        public void Linq_Enumerable_FirstOrDefault_Predicate_Value_Multiple()
+        {
+            var seq = new int[] { 1, 2 };
+            var result = seq.FirstOrDefault(_ => true, 10);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Linq_Enumerable_FirstOrDefault_Predicate_Value_Multiple_NoMatch()
+        {
+            var seq = new int[] { 1, 2 };
+            var result = seq.FirstOrDefault(_ => false, 10);
+            Assert.AreEqual(10, result);
+        }
+
+        [TestMethod]
+        public void Linq_Enumerable_FirstOrDefault_Predicate_Value_NoMatch()
+        {
+            var seq = new[] { "ABC", "DEF", "GHJ" };
+            var result = seq.FirstOrDefault(x => x.StartsWith("X", StringComparison.Ordinal), "X");
+            Assert.AreEqual("X", result);
+        }
+
+        [TestMethod]
+        public void Linq_Enumerable_FirstOrDefault_Predicate_Value_SingleMatch()
+        {
+            var seq = new[] { "ABC", "DEF", "GHJ" };
+            var result = seq.FirstOrDefault(x => x.StartsWith("D", StringComparison.Ordinal), "X");
+            Assert.AreEqual("DEF", result);
+        }
+
+        [TestMethod]
+        public void Linq_Enumerable_FirstOrDefault_Predicate_Value_MultpipleMatch()
+        {
+            var seq = new[] { "ABC", "DEF", "GHJ", "AMBER" };
+            var result = seq.FirstOrDefault(x => x.StartsWith("A", StringComparison.Ordinal), "X");
+            Assert.AreEqual("ABC", result);
+        }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentNullException))]
