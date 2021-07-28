7cbe12cf4390bbf372fc354c0aa2b63e6e526f98
hrumhurum <alex.s.gap@gmail.com>
2021-07-28 14:17:25 +0300

    Improved documentation.

--- Source/Gapotchenko.FX.Diagnostics.Process/ProcessExtensions.cs
+++ Source/Gapotchenko.FX.Diagnostics.Process/ProcessExtensions.cs
@@ -51,13 +51,13 @@ namespace Gapotchenko.FX.Diagnostics
         }
 
         /// <summary>
-        /// Enumerates parent processes.
+        /// Enumerates observable parent processes.
         /// </summary>
         /// <remarks>
         /// The closest parents are returned first.
         /// </remarks>
         /// <param name="process">The process to get the parents for.</param>
-        /// <returns>The sequence of parent processes.</returns>
+        /// <returns>The sequence of observable parent processes.</returns>
         public static IEnumerable<Process> EnumerateParents(this Process process)
         {
             if (process == null)
@@ -80,7 +80,7 @@ namespace Gapotchenko.FX.Diagnostics
         /// </summary>
         /// <remarks>
         /// Usually, the value returned by this method equals to the value returned by <see cref="ProcessModule.FileName"/> property of the main process module.
-        /// The difference becomes apparent when the current process cannot access the module information due to security restrictions imposed by the host platform.
+        /// The difference becomes apparent when the current process cannot access the module information due to security restrictions imposed by the host environment.
         /// While <see cref="ProcessModule.FileName"/> may not work in that situation, this method always works.
         /// </remarks>
         /// <param name="process">The process to get image file name for.</param>
