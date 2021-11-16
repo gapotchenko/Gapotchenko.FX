531e6ceb8f6c7f07ebab827915188ed86d25a027
hrumhurum <alex.s.gap@gmail.com>
2021-11-16 22:13:58 +0200

    KeyValuePair polyfill refactoring.

---
+++ Source/Compatibility/Gapotchenko.FX.Collections.CL/Generic/KeyValuePair.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Gapotchenko.FX.Collections.Generic;
+
+[EditorBrowsable(EditorBrowsableState.Never)]
+public static class KeyValuePair
+{
+    public static KeyValuePair<TKey, TValue> Create<TKey, TValue>(TKey key, TValue value) => new(key, value);
+
+    public static void Deconstruct<TKey, TValue>(KeyValuePair<TKey, TValue> pair, out TKey key, out TValue value)
+    {
+        key = pair.Key;
+        value = pair.Value;
+    }
+}
