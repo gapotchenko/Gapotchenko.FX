6eb1ec467e840cfacba33fd03196a97266dbe3da
hrumhurum <alex.s.gap@gmail.com>
2021-12-07 17:52:39 +0200

    Added IList<T>.InsertRange polyfill.

--- Source/Gapotchenko.FX.Collections/Generic/ListExtensions.cs
+++ Source/Gapotchenko.FX.Collections/Generic/ListExtensions.cs
@@ -45,6 +45,36 @@ namespace Gapotchenko.FX.Collections.Generic
         /// <typeparam name="T">The type of elements in the list.</typeparam>
         /// <param name="list">The list.</param>
         /// <returns>A cloned list.</returns>
-        public static List<T> Clone<T>(this List<T> list) => new List<T>(list ?? throw new ArgumentNullException(nameof(list)));
+        public static List<T> Clone<T>(this List<T> list) => new(list ?? throw new ArgumentNullException(nameof(list)));
+
+        /// <summary>
+        /// Inserts the elements of a collection into the <see cref="IList{T}"/> at the specified index.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the list.</typeparam>
+        /// <param name="list">The list where the elements should be inserted to.</param>
+        /// <param name="index">The index where the elements should be inserted at.</param>
+        /// <param name="collection">The collection whose elements should be inserted.</param>
+        public static void InsertRange<T>(this IList<T> list, int index, IEnumerable<T> collection)
+        {
+            if (list == null)
+                throw new ArgumentNullException(nameof(list));
+            if (collection == null)
+                throw new ArgumentNullException(nameof(collection));
+
+            if (index > list.Count)
+                throw new ArgumentOutOfRangeException(nameof(index));
+
+            if (list is List<T> l)
+            {
+                // Optimized implementation for List<T>.
+                l.InsertRange(index, collection);
+            }
+            else
+            {
+                // Generic implementation.
+                foreach (var i in collection)
+                    list.Insert(index++, i);
+            }
+        }
     }
 }
