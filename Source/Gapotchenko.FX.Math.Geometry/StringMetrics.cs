a2470740f27e3f65dac525a33a73b246bde650f0
kolos450@gmail.com <kolos450@gmail.com>
2021-12-09 20:13:41 +0200

    Implemented Damerau–Levenshtein distance calculator.

--- Source/Gapotchenko.FX.Math.Geometry/StringMetrics.cs
+++ Source/Gapotchenko.FX.Math.Geometry/StringMetrics.cs
@@ -378,5 +378,146 @@ namespace Gapotchenko.FX.Math.Geometry
 
             return 1 - jaroSimilarity;
         }
+
+        /// <summary>
+        /// Calculates Damerau–Levenshtein distance between two sequences.
+        /// </summary>
+        /// <typeparam name="T">The type of the sequence elements.</typeparam>
+        /// <param name="a">The first sequence.</param>
+        /// <param name="b">The second sequence.</param>
+        /// <returns>The Damerau–Levenshtein distance.</returns>
+        public static int DamerauLevenshteinDistance<T>(IEnumerable<T> a, IEnumerable<T> b) =>
+            DamerauLevenshteinDistance(a, b, null, null);
+
+        /// <summary>
+        /// Calculates Damerau–Levenshtein distance between two sequences.
+        /// </summary>
+        /// <typeparam name="T">The type of the sequence elements.</typeparam>
+        /// <param name="a">The first sequence.</param>
+        /// <param name="b">The second sequence.</param>
+        /// <param name="maxDistance">The inclusive upped bound of the edit distance.</param>
+        /// <returns>The Damerau–Levenshtein distance.</returns>
+        public static int DamerauLevenshteinDistance<T>(IEnumerable<T> a, IEnumerable<T> b, int? maxDistance) =>
+            DamerauLevenshteinDistance(a, b, maxDistance, null);
+
+        /// <summary>
+        /// Calculates Damerau–Levenshtein distance between two sequences.
+        /// </summary>
+        /// <typeparam name="T">The type of the sequence elements.</typeparam>
+        /// <param name="a">The first sequence.</param>
+        /// <param name="b">The second sequence.</param>
+        /// <param name="equalityComparer">The equality comparer.</param>
+        /// <returns>The Damerau–Levenshtein distance.</returns>
+        public static int DamerauLevenshteinDistance<T>(IEnumerable<T> a, IEnumerable<T> b, IEqualityComparer<T>? equalityComparer) =>
+            DamerauLevenshteinDistance(a, b, null, equalityComparer);
+
+        /// <summary>
+        /// Calculates Damerau–Levenshtein distance between two sequences.
+        /// </summary>
+        /// <typeparam name="T">The type of the sequence elements.</typeparam>
+        /// <param name="a">The first sequence.</param>
+        /// <param name="b">The second sequence.</param>
+        /// <param name="maxDistance">The inclusive upped bound of the edit distance.</param>
+        /// <param name="equalityComparer">The equality comparer.</param>
+        /// <returns>The Damerau–Levenshtein distance.</returns>
+        public static int DamerauLevenshteinDistance<T>(IEnumerable<T> a, IEnumerable<T> b, int? maxDistance, IEqualityComparer<T>? equalityComparer)
+        {
+            if (a == null)
+                throw new ArgumentNullException(nameof(a));
+            if (b == null)
+                throw new ArgumentNullException(nameof(b));
+            if (maxDistance < 0)
+                throw new ArgumentOutOfRangeException(nameof(maxDistance));
+
+            if (maxDistance == 0)
+                return 0;
+            if (ReferenceEquals(a, b))
+                return 0;
+
+            var aList = a.AsReadOnly();
+            var bList = b.AsReadOnly();
+
+            int aLength = aList.Count;
+            int bLength = bList.Count;
+
+            if (bLength == 0)
+                return aLength;
+            if (aLength == 0)
+                return bLength;
+
+            equalityComparer ??= EqualityComparer<T>.Default;
+
+            // The max possible distance.
+            int maxDist = bList.Count + aList.Count;
+
+            // Sequence elements map.
+            var da = new Dictionary<T, int>(equalityComparer);
+
+            // Create the distance matrix d[0 .. a.Length + 1][0 .. b.Length + 1].
+            int[,] d = new int[aLength + 2, bLength + 2];
+
+            // Initialize the left and top edges of d.
+            for (int i = 0; i <= aLength; i++)
+            {
+                d[i + 1, 0] = maxDist;
+                d[i + 1, 1] = i;
+            }
+
+            for (int j = 0; j <= bLength; j++)
+            {
+                d[0, j + 1] = maxDist;
+                d[1, j + 1] = j;
+            }
+
+            // Fill in the distance matrix d.
+            for (int aIdx = 1; aIdx <= aLength; aIdx++)
+            {
+                int db = 0;
+                var aElement = aList[aIdx - 1];
+
+                var best = aIdx;
+
+                for (int bIdx = 1; bIdx <= bLength; bIdx++)
+                {
+                    int k, l;
+
+                    var bElement = bList[bIdx - 1];
+                    if (!da.TryGetValue(bElement, out k))
+                        k = 0;
+                    l = db;
+
+                    int substitutionCost = 1;
+                    if (equalityComparer.Equals(aElement, bElement))
+                    {
+                        substitutionCost = 0;
+                        db = bIdx;
+                    }
+
+                    var currentDistance = Min(
+                        d[aIdx, bIdx] + substitutionCost,             // Substitution
+                        d[aIdx + 1, bIdx] + 1,                        // Insertion
+                        d[aIdx, bIdx + 1] + 1,                        // Deletion
+                        d[k, l] + (aIdx - k - 1) + 1 + (bIdx - l - 1) // Transposition
+                    );
+
+                    d[aIdx + 1, bIdx + 1] = currentDistance;
+
+                    best = System.Math.Min(best, currentDistance);
+                }
+
+                if (best >= maxDistance)
+                    return maxDistance.Value;
+
+                da[aList[aIdx - 1]] = aIdx;
+            }
+
+            var distance = d[aLength + 1, bLength + 1];
+            if (distance >= maxDistance)
+                return maxDistance.Value;
+            return distance;
+        }
+
+        static int Min(int val1, int val2, int val3, int val4) =>
+            System.Math.Min(val1, System.Math.Min(val2, System.Math.Min(val3, val4)));
     }
 }
