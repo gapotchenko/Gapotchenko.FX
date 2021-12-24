81dbfc9e0e6940de94b851b5904c479e86556e8f
kolos450@gmail.com <kolos450@gmail.com>
2021-12-24 20:57:40 +0200

    Replaced MSTest with xunit for the Gapotchenko.FX.Collections.Tests project.

--- Source/Tests/Gapotchenko.FX.Collections.Tests/Generic/Kit/ReadOnlySetBaseTests.cs
+++ Source/Tests/Gapotchenko.FX.Collections.Tests/Generic/Kit/ReadOnlySetBaseTests.cs
@@ -1,187 +1,186 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Xunit;
 
 namespace Gapotchenko.FX.Collections.Tests.Generic.Kit
 {
-    [TestClass]
     public class ReadOnlySetBaseTests
     {
-        [TestMethod]
+        [Fact]
         public void ReadOnlySetBase_IsProperSubsetOf()
         {
             var s1 = new ReadOnlySetChimera<int>(new HashSet<int> { 3, 2, 1 });
 
             foreach (var s in Util.Sets(new[] { 1, 2, 3, 4 }))
-                Assert.IsTrue(s1.IsProperSubsetOf(s));
+                Assert.True(s1.IsProperSubsetOf(s));
 
             foreach (var s in Util.Sets(new[] { 1, 2, 3 }))
-                Assert.IsFalse(s1.IsProperSubsetOf(s));
+                Assert.False(s1.IsProperSubsetOf(s));
 
             foreach (var s in Util.Sets(new[] { 1, 2 }))
-                Assert.IsFalse(s1.IsProperSubsetOf(s));
+                Assert.False(s1.IsProperSubsetOf(s));
 
             foreach (var s in Util.SetsEnumerable(s1))
-                Assert.IsFalse(s1.IsProperSubsetOf(s));
+                Assert.False(s1.IsProperSubsetOf(s));
 
             var s2 = ReadOnlySetChimera<int>.Empty;
 
             foreach (var s in Util.Sets(new[] { 1, 2, 3 }))
-                Assert.IsTrue(s2.IsProperSubsetOf(s));
+                Assert.True(s2.IsProperSubsetOf(s));
 
             foreach (var s in Util.Sets(Empty<int>.Array))
-                Assert.IsFalse(s2.IsProperSubsetOf(s));
+                Assert.False(s2.IsProperSubsetOf(s));
 
             foreach (var s in Util.SetsEnumerable(s2))
-                Assert.IsFalse(s2.IsProperSubsetOf(s));
+                Assert.False(s2.IsProperSubsetOf(s));
         }
 
-        [TestMethod]
+        [Fact]
         public void ReadOnlySetBase_IsProperSupersetOf()
         {
             var s1 = new ReadOnlySetChimera<int>(new HashSet<int> { 3, 2, 1 });
 
             foreach (var s in Util.Sets(Empty<int>.Array))
-                Assert.IsTrue(s1.IsProperSupersetOf(s));
+                Assert.True(s1.IsProperSupersetOf(s));
 
             foreach (var s in Util.Sets(new[] { 1, 2 }))
-                Assert.IsTrue(s1.IsProperSupersetOf(s));
+                Assert.True(s1.IsProperSupersetOf(s));
 
             foreach (var s in Util.Sets(new[] { 1, 2, 3 }))
-                Assert.IsFalse(s1.IsProperSupersetOf(s));
+                Assert.False(s1.IsProperSupersetOf(s));
 
             foreach (var s in Util.Sets(new[] { 1, 2, 3, 4 }))
-                Assert.IsFalse(s1.IsProperSupersetOf(s));
+                Assert.False(s1.IsProperSupersetOf(s));
 
             foreach (var s in Util.SetsEnumerable(s1))
-                Assert.IsFalse(s1.IsProperSupersetOf(s));
+                Assert.False(s1.IsProperSupersetOf(s));
 
             var s2 = ReadOnlySetChimera<int>.Empty;
 
             foreach (var s in Util.Sets(new[] { 1, 2, 3 }))
-                Assert.IsFalse(s2.IsProperSupersetOf(s));
+                Assert.False(s2.IsProperSupersetOf(s));
 
             foreach (var s in Util.Sets(Empty<int>.Array))
-                Assert.IsFalse(s2.IsProperSupersetOf(s));
+                Assert.False(s2.IsProperSupersetOf(s));
 
             foreach (var s in Util.SetsEnumerable(s2))
-                Assert.IsFalse(s2.IsProperSupersetOf(s));
+                Assert.False(s2.IsProperSupersetOf(s));
         }
 
-        [TestMethod]
+        [Fact]
         public void ReadOnlySetBase_IsSubsetOf()
         {
             var s1 = new ReadOnlySetChimera<int>(new HashSet<int> { 3, 2, 1 });
 
             foreach (var s in Util.Sets(new[] { 1, 2, 3, 4 }))
-                Assert.IsTrue(s1.IsSubsetOf(s));
+                Assert.True(s1.IsSubsetOf(s));
 
             foreach (var s in Util.Sets(new[] { 1, 2, 3 }))
-                Assert.IsTrue(s1.IsSubsetOf(s));
+                Assert.True(s1.IsSubsetOf(s));
 
             foreach (var s in Util.Sets(new[] { 1, 2 }))
-                Assert.IsFalse(s1.IsSubsetOf(s));
+                Assert.False(s1.IsSubsetOf(s));
 
             foreach (var s in Util.SetsEnumerable(s1))
-                Assert.IsTrue(s1.IsSubsetOf(s));
+                Assert.True(s1.IsSubsetOf(s));
 
             var s2 = ReadOnlySetChimera<int>.Empty;
 
             foreach (var s in Util.Sets(new[] { 1, 2, 3 }))
-                Assert.IsTrue(s2.IsSubsetOf(s));
+                Assert.True(s2.IsSubsetOf(s));
 
             foreach (var s in Util.Sets(Empty<int>.Array))
-                Assert.IsTrue(s2.IsSubsetOf(s));
+                Assert.True(s2.IsSubsetOf(s));
 
             foreach (var s in Util.SetsEnumerable(s2))
-                Assert.IsTrue(s2.IsSubsetOf(s));
+                Assert.True(s2.IsSubsetOf(s));
         }
 
-        [TestMethod]
+        [Fact]
         public void ReadOnlySetBase_IsSupersetOf()
         {
             var s1 = new ReadOnlySetChimera<int>(new HashSet<int> { 3, 2, 1 });
 
             foreach (var s in Util.Sets(Empty<int>.Array))
-                Assert.IsTrue(s1.IsSupersetOf(s));
+                Assert.True(s1.IsSupersetOf(s));
 
             foreach (var s in Util.Sets(new[] { 1, 2 }))
-                Assert.IsTrue(s1.IsSupersetOf(s));
+                Assert.True(s1.IsSupersetOf(s));
 
             foreach (var s in Util.Sets(new[] { 1, 2, 3 }))
-                Assert.IsTrue(s1.IsSupersetOf(s));
+                Assert.True(s1.IsSupersetOf(s));
 
             foreach (var s in Util.Sets(new[] { 1, 2, 3, 4 }))
-                Assert.IsFalse(s1.IsSupersetOf(s));
+                Assert.False(s1.IsSupersetOf(s));
 
             foreach (var s in Util.SetsEnumerable(s1))
-                Assert.IsTrue(s1.IsSupersetOf(s));
+                Assert.True(s1.IsSupersetOf(s));
         }
 
-        [TestMethod]
+        [Fact]
         public void ReadOnlySetBase_Overlaps()
         {
             var s1 = new ReadOnlySetChimera<int>(new HashSet<int> { 3, 2, 1 });
 
             foreach (var s in Util.Sets(Empty<int>.Array))
-                Assert.IsFalse(s1.Overlaps(s));
+                Assert.False(s1.Overlaps(s));
 
             foreach (var s in Util.Sets(new[] { 1, 2 }))
-                Assert.IsTrue(s1.Overlaps(s));
+                Assert.True(s1.Overlaps(s));
 
             foreach (var s in Util.Sets(new[] { 1, 2, 3 }))
-                Assert.IsTrue(s1.Overlaps(s));
+                Assert.True(s1.Overlaps(s));
 
             foreach (var s in Util.Sets(new[] { 1, 2, 3, 4 }))
-                Assert.IsTrue(s1.Overlaps(s));
+                Assert.True(s1.Overlaps(s));
 
             foreach (var s in Util.Sets(new[] { 10, 20 }))
-                Assert.IsFalse(s1.Overlaps(s));
+                Assert.False(s1.Overlaps(s));
 
             foreach (var s in Util.Sets(new[] { 10, 20, 30 }))
-                Assert.IsFalse(s1.Overlaps(s));
+                Assert.False(s1.Overlaps(s));
 
             foreach (var s in Util.Sets(new[] { 10, 20, 30, 40 }))
-                Assert.IsFalse(s1.Overlaps(s));
+                Assert.False(s1.Overlaps(s));
 
             foreach (var s in Util.Sets(new[] { 1, 20 }))
-                Assert.IsTrue(s1.Overlaps(s));
+                Assert.True(s1.Overlaps(s));
 
             foreach (var s in Util.Sets(new[] { 10, 2, 30 }))
-                Assert.IsTrue(s1.Overlaps(s));
+                Assert.True(s1.Overlaps(s));
 
             foreach (var s in Util.Sets(new[] { 10, 20, 3, 40 }))
-                Assert.IsTrue(s1.Overlaps(s));
+                Assert.True(s1.Overlaps(s));
 
             foreach (var s in Util.SetsEnumerable(s1))
-                Assert.IsTrue(s1.Overlaps(s));
+                Assert.True(s1.Overlaps(s));
 
             var s2 = ReadOnlySetChimera<int>.Empty;
 
             foreach (var s in Util.SetsEnumerable(s2))
-                Assert.IsFalse(s2.Overlaps(s));
+                Assert.False(s2.Overlaps(s));
         }
 
-        [TestMethod]
+        [Fact]
         public void ReadOnlySetBase_SetEquals()
         {
             var s1 = new ReadOnlySetChimera<int>(new HashSet<int> { 3, 2, 1 });
 
             foreach (var s in Util.Sets(Empty<int>.Array))
-                Assert.IsFalse(s1.SetEquals(s));
+                Assert.False(s1.SetEquals(s));
 
             foreach (var s in Util.Sets(new[] { 1, 2 }))
-                Assert.IsFalse(s1.SetEquals(s));
+                Assert.False(s1.SetEquals(s));
 
             foreach (var s in Util.Sets(new[] { 1, 2, 3 }))
-                Assert.IsTrue(s1.SetEquals(s));
+                Assert.True(s1.SetEquals(s));
 
             foreach (var s in Util.Sets(new[] { 1, 2, 3, 4 }))
-                Assert.IsFalse(s1.SetEquals(s));
+                Assert.False(s1.SetEquals(s));
         }
 
-        [TestMethod]
+        [Fact]
         public void ReadOnlySetBase_CopyTo()
         {
             var s1 = new ReadOnlySetChimera<int>(new HashSet<int> { 1, 2, 3 });
@@ -190,26 +189,26 @@ namespace Gapotchenko.FX.Collections.Tests.Generic.Kit
 
             arr = new int[3];
             s1.CopyTo(arr);
-            Assert.IsTrue(new HashSet<int>(arr).SetEquals(s1));
+            Assert.True(new HashSet<int>(arr).SetEquals(s1));
 
             arr = new int[2];
-            Assert.ThrowsException<ArgumentException>(() => s1.CopyTo(arr));
+            Assert.Throws<ArgumentException>(() => s1.CopyTo(arr));
 
             arr = new int[4];
             s1.CopyTo(arr);
-            Assert.IsTrue(new HashSet<int>(arr.Take(3)).SetEquals(new[] { 1, 2, 3 }));
-            Assert.AreEqual(0, arr[3]);
+            Assert.True(new HashSet<int>(arr.Take(3)).SetEquals(new[] { 1, 2, 3 }));
+            Assert.Equal(0, arr[3]);
 
             arr = new int[4];
             s1.CopyTo(arr, 1);
-            Assert.IsTrue(new HashSet<int>(arr.Skip(1)).SetEquals(new[] { 1, 2, 3 }));
-            Assert.AreEqual(0, arr[0]);
+            Assert.True(new HashSet<int>(arr.Skip(1)).SetEquals(new[] { 1, 2, 3 }));
+            Assert.Equal(0, arr[0]);
 
             arr = new int[4];
             s1.CopyTo(arr, 1, 2);
-            Assert.IsTrue(new HashSet<int>(arr.Skip(1).Take(2)).SetEquals(new[] { 1, 2 }));
-            Assert.AreEqual(0, arr[0]);
-            Assert.AreEqual(0, arr[3]);
+            Assert.True(new HashSet<int>(arr.Skip(1).Take(2)).SetEquals(new[] { 1, 2 }));
+            Assert.Equal(0, arr[0]);
+            Assert.Equal(0, arr[3]);
         }
     }
 }
--- Source/Tests/Gapotchenko.FX.Collections.Tests/Generic/Kit/SetBaseTests.cs
+++ Source/Tests/Gapotchenko.FX.Collections.Tests/Generic/Kit/SetBaseTests.cs
@@ -1,10 +1,9 @@
 ﻿using Gapotchenko.FX.Collections.Generic.Kit;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
+using Xunit;
 
 namespace Gapotchenko.FX.Collections.Tests.Generic.Kit
 {
-    [TestClass]
     public class SetBaseTests
     {
         sealed class SetChimera<T> : SetBase<T>
@@ -31,7 +30,7 @@ namespace Gapotchenko.FX.Collections.Tests.Generic.Kit
             public override void Clear() => m_BaseSet.Clear();
         }
 
-        [TestMethod]
+        [Fact]
         public void SetBase_ExceptWith()
         {
             var s1 = new SetChimera<int>(new HashSet<int> { 1, 2, 3, 4, 5 });
@@ -39,25 +38,25 @@ namespace Gapotchenko.FX.Collections.Tests.Generic.Kit
             s1.SetEquals(new[] { 2, 4, 5 });
 
             s1.ExceptWith(s1);
-            Assert.AreEqual(0, s1.Count);
+            Assert.Equal(0, s1.Count);
         }
 
-        [TestMethod]
+        [Fact]
         public void SetBase_IntersectWith()
         {
             var s1 = new SetChimera<int>(new HashSet<int>());
             s1.IntersectWith(new[] { 1, 2, 3 });
-            Assert.AreEqual(0, s1.Count);
+            Assert.Equal(0, s1.Count);
 
             s1 = new SetChimera<int>(new HashSet<int> { 1, 2, 3 });
             s1.IntersectWith(Empty<int>.Array);
-            Assert.AreEqual(0, s1.Count);
+            Assert.Equal(0, s1.Count);
 
             foreach (var s2 in Util.Sets(Empty<int>.Array))
             {
                 s1 = new SetChimera<int>(new HashSet<int> { 1, 2, 3, 4, 5 });
                 s1.IntersectWith(s2);
-                Assert.AreEqual(0, s1.Count);
+                Assert.Equal(0, s1.Count);
             }
 
             foreach (var s2 in Util.Sets(new[] { 10, 2, 3, 40 }))
@@ -68,7 +67,7 @@ namespace Gapotchenko.FX.Collections.Tests.Generic.Kit
             }
         }
 
-        [TestMethod]
+        [Fact]
         public void SetBase_SymmetricExceptWith()
         {
             foreach (var s2 in Util.Sets(new[] { 3, 4, 5 }))
@@ -79,7 +78,7 @@ namespace Gapotchenko.FX.Collections.Tests.Generic.Kit
             }
         }
 
-        [TestMethod]
+        [Fact]
         public void SetBase_UnionWith()
         {
             var s1 = new SetChimera<int>(new HashSet<int> { 1, 2, 3 });
