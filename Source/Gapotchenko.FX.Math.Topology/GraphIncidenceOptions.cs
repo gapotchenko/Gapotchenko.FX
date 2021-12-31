07e17f5b4a0f0b27a94b9c8afeb49ced853bc470
hrumhurum <alex.s.gap@gmail.com>
2021-12-31 19:48:12 +0200

    Gapotchenko.FX.Math.Topology in progress.

--- Source/Gapotchenko.FX.Math.Topology/GraphIncidenceOptions.cs
+++ Source/Gapotchenko.FX.Math.Topology/GraphIncidenceOptions.cs
@@ -14,13 +14,18 @@ namespace Gapotchenko.FX.Math.Topology
         None,
 
         /// <summary>
+        /// <para>
         /// Perform reflexive reduction by not calling a graph incidence function for all v → v edges where v ∈ V and V is a set of vertices of the graph.
+        /// </para>
+        /// <para>
+        /// This option efficiently eliminates loops (also called self-loops or buckles) from a resulting graph.
+        /// </para>
         /// </summary>
         ReflexiveReduction = 1 << 1,
 
         /// <summary>
         /// <para>
-        /// Exclude isolated vertices.
+        /// Instructs to exclude isolated vertices from a resulting graph.
         /// </para>
         /// <para>
         /// Isolated vertices are vertices with degree zero; that is, vertices that are not incident with any edge.
