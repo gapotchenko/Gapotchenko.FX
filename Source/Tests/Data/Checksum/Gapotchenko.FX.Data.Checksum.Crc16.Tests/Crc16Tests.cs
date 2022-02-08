8c9a15ae157b59092f81852b93bf9b9d7180e6c1
hrumhurum <alex.s.gap@gmail.com>
2022-02-08 16:28:05 +0200

    CRC-16 in progress.

--- Source/Tests/Data/Checksum/Gapotchenko.FX.Data.Checksum.Crc16.Tests/Crc16Tests.cs
+++ Source/Tests/Data/Checksum/Gapotchenko.FX.Data.Checksum.Crc16.Tests/Crc16Tests.cs
@@ -9,55 +9,68 @@ namespace Gapotchenko.FX.Data.Checksum.Tests
     [TestClass]
     public class Crc16Tests
     {
-        static readonly byte[] CheckData = Encoding.ASCII.GetBytes("123456789");
+        /// <summary>
+        /// The test vector that corresponds to the bytes of ASCII string "123456789".
+        /// </summary>
+        static readonly byte[] TV19 = Encoding.ASCII.GetBytes("123456789");
 
-        [TestMethod]
-        public void Check(Crc16 crc, ushort check)
+        static void Check<T>(IChecksumAlgorithm<T> algorithm, byte[] data, ushort check)
+            where T : struct
         {
-            Assert.AreEqual(check, crc.ComputeChecksum(CheckData), "Checksum computation failed.");
+            Assert.AreEqual(check, algorithm.ComputeChecksum(data), "Checksum computation failed.");
 
-            var iterator = crc.CreateIterator();
-            iterator.ComputeBlock(CheckData.AsSpan(0, 5));
-            iterator.ComputeBlock(CheckData.AsSpan(5, 4));
+            var iterator = algorithm.CreateIterator();
+            iterator.ComputeBlock(data.AsSpan(0, 5));
+            iterator.ComputeBlock(data.AsSpan(5, 4));
             Assert.AreEqual(check, iterator.ComputeFinal(), "Iterator checksum computation failed.");
 
-            iterator.ComputeBlock(CheckData.AsSpan(0, 2));
-            iterator.ComputeBlock(CheckData.AsSpan(2, 7));
+            iterator.ComputeBlock(data.AsSpan(0, 2));
+            iterator.ComputeBlock(data.AsSpan(2, 7));
             Assert.AreEqual(check, iterator.ComputeFinal(), "Reused iterator checksum computation failed.");
 
-            iterator.ComputeBlock(CheckData.AsSpan(0, 5));
+            iterator.ComputeBlock(data.AsSpan(0, 5));
             iterator.Reset();
-            iterator.ComputeBlock(CheckData.AsSpan(0, 3));
-            iterator.ComputeBlock(CheckData.AsSpan(3, 6));
+            iterator.ComputeBlock(data.AsSpan(0, 3));
+            iterator.ComputeBlock(data.AsSpan(3, 6));
             Assert.AreEqual(check, iterator.ComputeFinal(), "Reseted iterator checksum computation failed.");
 
-            var ms = new MemoryStream(CheckData, false);
-            Assert.AreEqual(check, crc.ComputeChecksum(ms), "Stream checksum computation failed.");
+            var ms = new MemoryStream(data, false);
+            Assert.AreEqual(check, algorithm.ComputeChecksum(ms), "Stream checksum computation failed.");
 
             ms.Position = 0;
-            var checksum = TaskBridge.Execute(() => crc.ComputeChecksumAsync(ms));
+            var checksum = TaskBridge.Execute(() => algorithm.ComputeChecksumAsync(ms));
             Assert.AreEqual(check, checksum, "Asynchronous stream checksum computation failed.");
         }
 
-        [TestMethod]
-        public void Crc16_Standard_Check() => Check(Crc16.Standard, 0xbb3d);
+        /// <summary>
+        /// Checks a specified checksum algorithm for correctness working with "123456789" ASCII test vector.
+        /// </summary>
+        /// <typeparam name="T">The type of the checksum value.</typeparam>
+        /// <param name="algorithm">The checksum algorithm to check.</param>
+        /// <param name="check">The expected checksum.</param>
+        static void Check19<T>(IChecksumAlgorithm<T> algorithm, ushort check)
+            where T : struct =>
+            Check(algorithm, TV19, check);
 
         [TestMethod]
-        public void Crc16_Ccitt_Check() => Check(Crc16.Ccitt, 0x2189);
+        public void Crc16_Standard_Check() => Check19(Crc16.Standard, 0xbb3d);
 
         [TestMethod]
-        public void Crc16_IsoIec14443_3_A_Check() => Check(Crc16.IsoIec14443_3_A, 0xbf05);
+        public void Crc16_Ccitt_Check() => Check19(Crc16.Ccitt, 0x2189);
 
         [TestMethod]
-        public void Crc16_IsoIec14443_3_B_Check() => Check(Crc16.IsoIec14443_3_B, 0x906e);
+        public void Crc16_IsoIec14443_3_A_Check() => Check19(Crc16.IsoIec14443_3_A, 0xbf05);
 
         [TestMethod]
-        public void Crc16_Maxim_Check() => Check(Crc16.Maxim, 0x44c2);
+        public void Crc16_IsoIec14443_3_B_Check() => Check19(Crc16.IsoIec14443_3_B, 0x906e);
 
         [TestMethod]
-        public void Crc16_Nrsc5_Check() => Check(Crc16.Nrsc5, 0xa066);
+        public void Crc16_Maxim_Check() => Check19(Crc16.Maxim, 0x44c2);
 
         [TestMethod]
-        public void Crc16_SpiFujitsu_Check() => Check(Crc16.SpiFujitsu, 0xe5cc);
+        public void Crc16_Nrsc5_Check() => Check19(Crc16.Nrsc5, 0xa066);
+
+        [TestMethod]
+        public void Crc16_SpiFujitsu_Check() => Check19(Crc16.SpiFujitsu, 0xe5cc);
     }
 }
