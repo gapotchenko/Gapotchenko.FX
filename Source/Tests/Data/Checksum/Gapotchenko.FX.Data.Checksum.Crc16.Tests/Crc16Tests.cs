d569fffb44ce85237ab7dc255d209fcf16040061
hrumhurum <alex.s.gap@gmail.com>
2022-02-09 19:50:23 +0200

    CRC in progress.

--- Source/Tests/Data/Checksum/Gapotchenko.FX.Data.Checksum.Crc16.Tests/Crc16Tests.cs
+++ Source/Tests/Data/Checksum/Gapotchenko.FX.Data.Checksum.Crc16.Tests/Crc16Tests.cs
@@ -1,56 +1,12 @@
-﻿using Gapotchenko.FX.Threading.Tasks;
+﻿using Gapotchenko.FX.Data.Checksum.Tests.Bench;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.IO;
-using System.Text;
 
 namespace Gapotchenko.FX.Data.Checksum.Tests
 {
     [TestClass]
     public class Crc16Tests
     {
-        /// <summary>
-        /// The test vector that corresponds to the bytes of ASCII string "123456789".
-        /// </summary>
-        static readonly byte[] TV19 = Encoding.ASCII.GetBytes("123456789");
-
-        static void Check<T>(IChecksumAlgorithm<T> algorithm, byte[] data, ushort check)
-            where T : struct
-        {
-            Assert.AreEqual(check, algorithm.ComputeChecksum(data), "Checksum computation failed.");
-
-            var iterator = algorithm.CreateIterator();
-            iterator.ComputeBlock(data.AsSpan(0, 5));
-            iterator.ComputeBlock(data.AsSpan(5, 4));
-            Assert.AreEqual(check, iterator.ComputeFinal(), "Iterator checksum computation failed.");
-
-            iterator.ComputeBlock(data.AsSpan(0, 2));
-            iterator.ComputeBlock(data.AsSpan(2, 7));
-            Assert.AreEqual(check, iterator.ComputeFinal(), "Reused iterator checksum computation failed.");
-
-            iterator.ComputeBlock(data.AsSpan(0, 5));
-            iterator.Reset();
-            iterator.ComputeBlock(data.AsSpan(0, 3));
-            iterator.ComputeBlock(data.AsSpan(3, 6));
-            Assert.AreEqual(check, iterator.ComputeFinal(), "Reset iterator checksum computation failed.");
-
-            var ms = new MemoryStream(data, false);
-            Assert.AreEqual(check, algorithm.ComputeChecksum(ms), "Stream checksum computation failed.");
-
-            ms.Position = 0;
-            var checksum = TaskBridge.Execute(() => algorithm.ComputeChecksumAsync(ms));
-            Assert.AreEqual(check, checksum, "Asynchronous stream checksum computation failed.");
-        }
-
-        /// <summary>
-        /// Checks a specified checksum algorithm for correctness working with "123456789" ASCII test vector.
-        /// </summary>
-        /// <typeparam name="T">The type of the checksum value.</typeparam>
-        /// <param name="algorithm">The checksum algorithm to check.</param>
-        /// <param name="check">The expected checksum.</param>
-        static void Check19<T>(IChecksumAlgorithm<T> algorithm, ushort check)
-            where T : struct =>
-            Check(algorithm, TV19, check);
+        static void Check19(IChecksumAlgorithm<ushort> algorithm, ushort check) => ChecksumTestBench.Check(algorithm, ChecksumTestBench.TV19, check);
 
         [TestMethod]
         public void Crc16_Standard_Check() => Check19(Crc16.Standard, 0xbb3d);
@@ -60,7 +16,7 @@ namespace Gapotchenko.FX.Data.Checksum.Tests
         {
             var algorithm = Crc16.Standard;
             var ha = algorithm.CreateHashAlgorithm();
-            var hash = ha.ComputeHash(TV19);
+            var hash = ha.ComputeHash(ChecksumTestBench.TV19);
             Assert.AreEqual(2, hash.Length);
             Assert.AreEqual(0xbb3d, LittleEndianBitConverter.ToUInt16(hash));
         }
@@ -69,7 +25,7 @@ namespace Gapotchenko.FX.Data.Checksum.Tests
         {
             var algorithm = Crc16.Standard;
             var ha = algorithm.CreateHashAlgorithm(bitConverter);
-            var hash = ha.ComputeHash(TV19);
+            var hash = ha.ComputeHash(ChecksumTestBench.TV19);
             Assert.AreEqual(2, hash.Length);
             Assert.AreEqual(0xbb3d, bitConverter.ToUInt16(hash));
         }
---
+++ Source/Tests/Data/Checksum/Gapotchenko.FX.Data.Checksum.Tests.Bench/ChecksumTestBench.cs
@@ -0,0 +1,44 @@
+﻿using Gapotchenko.FX.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using System.Text;
+
+namespace Gapotchenko.FX.Data.Checksum.Tests.Bench
+{
+    public static class ChecksumTestBench
+    {
+        /// <summary>
+        /// Gets the test vector corresponding to the bytes of ASCII string "123456789".
+        /// </summary>
+        public static byte[] TV19 { get; } = Encoding.ASCII.GetBytes("123456789");
+
+        public static void Check<T>(IChecksumAlgorithm<T> algorithm, byte[] data, T check)
+            where T : struct
+        {
+            Assert.AreEqual(check, algorithm.ComputeChecksum(data), "Checksum computation failed.");
+
+            var iterator = algorithm.CreateIterator();
+            iterator.ComputeBlock(data.AsSpan(0, 5));
+            iterator.ComputeBlock(data.AsSpan(5, 4));
+            Assert.AreEqual(check, iterator.ComputeFinal(), "Iterator checksum computation failed.");
+
+            iterator.ComputeBlock(data.AsSpan(0, 2));
+            iterator.ComputeBlock(data.AsSpan(2, 7));
+            Assert.AreEqual(check, iterator.ComputeFinal(), "Reused iterator checksum computation failed.");
+
+            iterator.ComputeBlock(data.AsSpan(0, 5));
+            iterator.Reset();
+            iterator.ComputeBlock(data.AsSpan(0, 3));
+            iterator.ComputeBlock(data.AsSpan(3, 6));
+            Assert.AreEqual(check, iterator.ComputeFinal(), "Reset iterator checksum computation failed.");
+
+            var ms = new MemoryStream(data, false);
+            Assert.AreEqual(check, algorithm.ComputeChecksum(ms), "Stream checksum computation failed.");
+
+            ms.Position = 0;
+            var checksum = TaskBridge.Execute(() => algorithm.ComputeChecksumAsync(ms));
+            Assert.AreEqual(check, checksum, "Asynchronous stream checksum computation failed.");
+        }
+    }
+}
