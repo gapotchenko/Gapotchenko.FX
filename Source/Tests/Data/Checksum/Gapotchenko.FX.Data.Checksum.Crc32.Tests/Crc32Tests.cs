a33a3e937389a047f1b8e92ec5b0c2cfb6291d74
hrumhurum <alex.s.gap@gmail.com>
2022-02-09 21:29:58 +0200

    CRC-32 in progress.

--- Source/Tests/Data/Checksum/Gapotchenko.FX.Data.Checksum.Crc32.Tests/Crc32Tests.cs
+++ Source/Tests/Data/Checksum/Gapotchenko.FX.Data.Checksum.Crc32.Tests/Crc32Tests.cs
@@ -12,6 +12,31 @@ namespace Gapotchenko.FX.Data.Checksum.Tests
         public void Crc32_Standard_Check() => Check19(Crc32.Standard, 0xcbf43926);
 
         [TestMethod]
+        public void Crc32_Standard_Hash()
+        {
+            var algorithm = Crc32.Standard;
+            var ha = algorithm.CreateHashAlgorithm();
+            var hash = ha.ComputeHash(ChecksumTestBench.TV19);
+            Assert.AreEqual(4, hash.Length);
+            Assert.AreEqual(0xcbf43926, LittleEndianBitConverter.ToUInt32(hash));
+        }
+
+        void Crc32_Standard_Hash(IBitConverter bitConverter)
+        {
+            var algorithm = Crc32.Standard;
+            var ha = algorithm.CreateHashAlgorithm(bitConverter);
+            var hash = ha.ComputeHash(ChecksumTestBench.TV19);
+            Assert.AreEqual(4, hash.Length);
+            Assert.AreEqual(0xcbf43926, bitConverter.ToUInt32(hash));
+        }
+
+        [TestMethod]
+        public void Crc32_Standard_Hash_LE() => Crc32_Standard_Hash(LittleEndianBitConverter.Instance);
+
+        [TestMethod]
+        public void Crc32_Standard_Hash_BE() => Crc32_Standard_Hash(BigEndianBitConverter.Instance);
+
+        [TestMethod]
         public void Crc32_C_Check() => Check19(Crc32.Attested.C, 0xe3069283);
 
         [TestMethod]
