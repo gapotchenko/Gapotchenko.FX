ac769871c56cd5ee9ffe6d840003eb59c6166918
hrumhurum <alex.s.gap@gmail.com>
2022-02-09 20:28:13 +0200

    CRC-32 in progress.

---
+++ Source/Tests/Data/Checksum/Gapotchenko.FX.Data.Checksum.Crc32.Tests/Crc32Tests.cs
@@ -0,0 +1,12 @@
+﻿using Gapotchenko.FX.Data.Checksum.Tests.Bench;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Gapotchenko.FX.Data.Checksum.Tests
+{
+    [TestClass]
+    public class Crc32Tests
+    {
+        static void Check19(IChecksumAlgorithm<uint> algorithm, uint check) => ChecksumTestBench.Check(algorithm, ChecksumTestBench.TV19, check);
+
+    }
+}
