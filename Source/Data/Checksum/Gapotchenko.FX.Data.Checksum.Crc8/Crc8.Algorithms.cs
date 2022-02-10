713eb0c3cad4f6cdb10a9bfcd07cfcc239c13c38
hrumhurum <alex.s.gap@gmail.com>
2022-02-10 21:03:28 +0200

    CRC-8 in progress.

---
+++ Source/Data/Checksum/Gapotchenko.FX.Data.Checksum.Crc8/Crc8.Algorithms.cs
@@ -0,0 +1,30 @@
+﻿namespace Gapotchenko.FX.Data.Checksum
+{
+    partial class Crc8
+    {
+        /// <summary>
+        /// <para>
+        /// Gets the standard CRC-8 algorithm
+        /// which performs checksum computation using x^8 + x^2 + x + 1 polynomial with initial value of 0.
+        /// </para>
+        /// <para>
+        /// Alias: CRC-8/SMBUS.
+        /// </para>
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// Parameters: poly=0x07, init=0x00, refin=false, refout=false, xorout=0x00, check=0xf4.
+        /// </para>
+        /// </remarks>
+        public static Crc8 Standard => Impl.Standard.Instance;
+
+        static class Impl
+        {
+            public sealed class Standard : GenericCrc8
+            {
+                Standard() : base(0x07, 0, false, false, 0) { }
+                public static readonly Standard Instance = new();
+            }
+        }
+    }
+}
---
+++ Source/Tests/Data/Checksum/Gapotchenko.FX.Data.Checksum.Crc8.Tests/Crc8Tests.cs
@@ -0,0 +1,19 @@
+﻿using Gapotchenko.FX.Data.Checksum.Tests.Bench;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Gapotchenko.FX.Data.Checksum.Tests
+{
+    [TestClass]
+    public class Crc8Tests
+    {
+        static void Check19(IChecksumAlgorithm<byte> algorithm, byte check)
+        {
+            Assert.AreEqual(8, algorithm.ChecksumSize);
+            ChecksumTestBench.Check(algorithm, ChecksumTestBench.TV19, check);
+        }
+
+        [TestMethod]
+        public void Crc8_Standard_Check() => Check19(Crc8.Standard, 0xf4);
+    }
+}
