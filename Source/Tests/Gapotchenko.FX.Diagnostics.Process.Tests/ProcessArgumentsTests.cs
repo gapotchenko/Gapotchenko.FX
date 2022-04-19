da38c25358b4cb5e301d68bc79f51b623fcb70bf
hrumhurum <alex.s.gap@gmail.com>
2022-04-19 13:02:16 +0300

    Added tests for Process.ReadArguments() and Process.ReadArgumentList() extension methods.

---
+++ Source/Tests/Gapotchenko.FX.Diagnostics.Process.Tests/ProcessArgumentsTests.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace Gapotchenko.FX.Diagnostics.Process.Tests
+{
+    using Process = System.Diagnostics.Process;
+
+    [TestClass]
+    public class ProcessArgumentsTests
+    {
+        [TestMethod]
+        public void Process_ReadArguments()
+        {
+            using var process = new Process();
+
+            var psi = process.StartInfo;
+            psi.FileName = "dotnet";
+            psi.Arguments = "fsi";
+            psi.UseShellExecute = false;
+            psi.RedirectStandardOutput = true;
+
+            Assert.IsTrue(process.Start());
+            try
+            {
+                // Ensure that the process is fully initialized.
+                process.StandardOutput.ReadLine();
+
+                var commandLine = process.ReadArguments();
+                Assert.AreEqual("dotnet fsi", CommandLine.Build(CommandLine.Split(commandLine)));
+            }
+            finally
+            {
+                process.Kill();
+            }
+        }
+
+        [TestMethod]
+        public void Process_ReadArgumentList()
+        {
+            using var process = new Process();
+
+            var psi = process.StartInfo;
+            psi.FileName = "dotnet";
+            psi.Arguments = "fsi";
+            psi.UseShellExecute = false;
+            psi.RedirectStandardOutput = true;
+
+            Assert.IsTrue(process.Start());
+            try
+            {
+                // Ensure that the process is fully initialized.
+                process.StandardOutput.ReadLine();
+
+                var arguments = process.ReadArgumentList().ToList();
+                Assert.AreEqual(2, arguments.Count);
+                Assert.AreEqual("dotnet", arguments[0]);
+                Assert.AreEqual("fsi", arguments[1]);
+            }
+            finally
+            {
+                process.Kill();
+            }
+        }
+    }
+}
