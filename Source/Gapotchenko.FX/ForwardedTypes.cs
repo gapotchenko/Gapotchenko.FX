bb2949ebe52a6a2256404e28cf8ee70dc6743dd4
hrumhurum <alex.s.gap@gmail.com>
2022-07-24 12:13:55 +0200

    Refactoring.

---
+++ Source/Gapotchenko.FX/ForwardedTypes.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.ExceptionServices;
+using System.Runtime.InteropServices;
+
+[assembly: TypeForwardedTo(typeof(WeakReference<>))]
+[assembly: TypeForwardedTo(typeof(Volatile))]
+[assembly: TypeForwardedTo(typeof(CustomAttributeExtensions))]
+[assembly: TypeForwardedTo(typeof(ExceptionDispatchInfo))]
+[assembly: TypeForwardedTo(typeof(Architecture))]
+[assembly: TypeForwardedTo(typeof(OSPlatform))]
+[assembly: TypeForwardedTo(typeof(RuntimeInformation))]
