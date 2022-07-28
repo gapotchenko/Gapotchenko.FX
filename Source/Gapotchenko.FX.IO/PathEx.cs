b7975bca42c77c23b2d5d6fedd7c1d02221432a1
hrumhurum <alex.s.gap@gmail.com>
2022-07-28 16:18:49 +0200

    Added polyfill for Path.TrimEndingDirectorySeparator method.

---
+++ Source/Gapotchenko.FX.IO/PathEx.cs
@@ -0,0 +1,20 @@
+﻿namespace Gapotchenko.FX.IO;
+
+/// <summary>
+/// Provides polyfills for <see cref="Path"/> class.
+/// </summary>
+public static class PathEx
+{
+    /// <summary>
+    /// Trims one trailing directory separator beyond the root of the specified path.
+    /// </summary>
+    /// <param name="path">The path to trim.</param>
+    /// <returns>The path without any trailing directory separators.</returns>
+    public static string TrimEndingDirectorySeparator(string path) =>
+#if NETCOREAPP3_0_OR_GREATER
+        Path.TrimEndingDirectorySeparator(path);
+#else
+        (path ?? throw new ArgumentNullException(nameof(path)))
+        .TrimEnd(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar);
+#endif
+}
