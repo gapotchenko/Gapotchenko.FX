13e60ee2c81ba9ad017b96def53036356672eb51
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2022-12-15 23:04:43 +0100

    Refactoring.

--- Source/Gapotchenko.FX/ForwardedTypes.cs
+++ Source/Gapotchenko.FX/ForwardedTypes.cs
@@ -3,10 +3,11 @@ using System.Runtime.CompilerServices;
 using System.Runtime.ExceptionServices;
 using System.Runtime.InteropServices;
 
-[assembly: TypeForwardedTo(typeof(WeakReference<>))]
-[assembly: TypeForwardedTo(typeof(Volatile))]
+[assembly: TypeForwardedTo(typeof(Architecture))]
 [assembly: TypeForwardedTo(typeof(CustomAttributeExtensions))]
 [assembly: TypeForwardedTo(typeof(ExceptionDispatchInfo))]
-[assembly: TypeForwardedTo(typeof(Architecture))]
+[assembly: TypeForwardedTo(typeof(HashCode))]
 [assembly: TypeForwardedTo(typeof(OSPlatform))]
 [assembly: TypeForwardedTo(typeof(RuntimeInformation))]
+[assembly: TypeForwardedTo(typeof(Volatile))]
+[assembly: TypeForwardedTo(typeof(WeakReference<>))]
--- Source/Gapotchenko.FX/HashCode.cs
+++
@@ -1,3 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: TypeForwardedTo(typeof(HashCode))]
