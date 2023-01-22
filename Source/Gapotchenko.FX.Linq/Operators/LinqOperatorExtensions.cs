2bc9a16597c7eed33b52c03f2e78f2d6bf275e3a
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2023-01-22 17:41:42 +0100

    Improved pipe operator.

--- Source/Gapotchenko.FX.Linq/Operators/LinqOperatorExtensions.cs
+++ Source/Gapotchenko.FX.Linq/Operators/LinqOperatorExtensions.cs
@@ -1,7 +1,7 @@
 ﻿namespace Gapotchenko.FX.Linq.Operators;
 
 /// <summary>
-/// Provides extension methods for compiler operator polyfills.
+/// Provides extension methods for operator polyfills.
 /// </summary>
 [EditorBrowsable(EditorBrowsableState.Never)]
 public static class LinqOperatorExtensions
@@ -12,12 +12,27 @@ public static class LinqOperatorExtensions
     /// <remarks>
     /// This is a polyfill provided by Gapotchenko.FX.
     /// </remarks>
-    /// <typeparam name="TInput">The type of pipe input.</typeparam>
+    /// <typeparam name="T">The type of pipe input.</typeparam>
     /// <typeparam name="TResult">The type of pipe result.</typeparam>
     /// <param name="input">The pipe input.</param>
     /// <param name="func">The function which calculates the pipe result from the specified pipe <paramref name="input"/>.</param>
     /// <returns>The pipe result which is calculated by <paramref name="func"/> from the specified pipe <paramref name="input"/>.</returns>
     /// <exception cref="ArgumentNullException">The <paramref name="func"/> is <see langword="null"/>.</exception>
-    public static TResult Pipe<TInput, TResult>(this TInput input, Func<TInput, TResult> func) =>
-        (func ?? throw new ArgumentNullException(nameof(func)))(input);
+    public static TResult Pipe<T, TResult>(this T input, Func<T, TResult> func) =>
+        (func ?? throw new ArgumentNullException(nameof(func)))
+        (input);
+
+    /// <summary>
+    /// A LINQ-style pipe operator.
+    /// </summary>
+    /// <remarks>
+    /// This is a polyfill provided by Gapotchenko.FX.
+    /// </remarks>
+    /// <typeparam name="T">The type of pipe input.</typeparam>
+    /// <param name="input">The pipe input.</param>
+    /// <param name="action">The method that receives the specified pipe <paramref name="input"/>.</param>
+    /// <exception cref="ArgumentNullException">The <paramref name="action"/> is <see langword="null"/>.</exception>
+    public static void Pipe<T>(this T input, Action<T> action) =>
+        (action ?? throw new ArgumentNullException(nameof(action)))
+        (input);
 }
