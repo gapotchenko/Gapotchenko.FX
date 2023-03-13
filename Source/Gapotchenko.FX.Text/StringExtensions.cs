d879f19507a8a4a44690588cc8503e33f408edd5
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2023-03-14 00:40:48 +0100

    Added polyfill for System.String.Contains(System.Char, System.StringComparison) method.

--- Source/Gapotchenko.FX.Text/StringExtensions.cs
+++ Source/Gapotchenko.FX.Text/StringExtensions.cs
@@ -3,6 +3,7 @@
 /// <summary>
 /// Provides polyfill methods for <see cref="string"/> type.
 /// </summary>
+[EditorBrowsable(EditorBrowsableState.Never)]
 public static class StringExtensions
 {
     /// <summary>
@@ -85,6 +86,39 @@ public static class StringExtensions
         return s.Contains(value);
 #else
         return s.IndexOf(value) != -1;
+#endif
+    }
+
+    /// <summary>
+    /// <para>
+    /// Returns a value indicating whether a specified character occurs within this string,
+    /// using the specified comparison rules.
+    /// </para>
+    /// <para>
+    /// This is a polyfill provided by Gapotchenko.FX.
+    /// </para>
+    /// </summary>
+    /// <param name="s">The string instance.</param>
+    /// <param name="value">The character to seek.</param>
+    /// <param name="comparisonType">One of the enumeration values that specifies the rules to use in the comparison.</param>
+    /// <returns><see langword="true"/> if the <paramref name="value parameter"/> occurs within this string; otherwise, <see langword="false"/>.</returns>
+#if TFF_STRING_CONTAINS_CHAR
+    [EditorBrowsable(EditorBrowsableState.Never)]
+#endif
+    public static bool Contains(
+#if !TFF_STRING_CONTAINS_CHAR
+        this
+#endif
+        string s, char value, StringComparison comparisonType)
+    {
+        if (s == null)
+            throw new ArgumentNullException(nameof(s));
+
+#if TFF_STRING_CONTAINS_CHAR
+        return s.Contains(value, comparisonType);
+#else
+        ReadOnlySpan<char> span = stackalloc char[] { value };
+        return s.AsSpan().Contains(span, comparisonType);
 #endif
     }
 }
