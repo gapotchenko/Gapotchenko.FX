e8e47367ce6ae80cdb78c3063b1119cd93cf6a40
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2023-03-18 23:45:20 +0100

    Refactoring.

--- Source/Gapotchenko.FX.Text/StringExtensions.cs
+++ Source/Gapotchenko.FX.Text/StringExtensions.cs
@@ -88,37 +88,4 @@ public static class StringExtensions
         return s.IndexOf(value) != -1;
 #endif
     }
-
-    /// <summary>
-    /// <para>
-    /// Returns a value indicating whether a specified character occurs within this string,
-    /// using the specified comparison rules.
-    /// </para>
-    /// <para>
-    /// This is a polyfill provided by Gapotchenko.FX.
-    /// </para>
-    /// </summary>
-    /// <param name="s">The string instance.</param>
-    /// <param name="value">The character to seek.</param>
-    /// <param name="comparisonType">One of the enumeration values that specifies the rules to use in the comparison.</param>
-    /// <returns><see langword="true"/> if the <paramref name="value parameter"/> occurs within this string; otherwise, <see langword="false"/>.</returns>
-#if TFF_STRING_CONTAINS_CHAR
-    [EditorBrowsable(EditorBrowsableState.Never)]
-#endif
-    public static bool Contains(
-#if !TFF_STRING_CONTAINS_CHAR
-        this
-#endif
-        string s, char value, StringComparison comparisonType)
-    {
-        if (s == null)
-            throw new ArgumentNullException(nameof(s));
-
-#if TFF_STRING_CONTAINS_CHAR
-        return s.Contains(value, comparisonType);
-#else
-        ReadOnlySpan<char> span = stackalloc char[] { value };
-        return s.AsSpan().Contains(span, comparisonType);
-#endif
-    }
 }
