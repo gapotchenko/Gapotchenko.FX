96e20641061da705c24abbc081760c805a7f754c
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2023-04-03 03:44:29 +0200

    Improved pipe operator polyfill.

--- Source/Gapotchenko.FX.Linq/Operators/LinqOperatorPolyfills.cs
+++ Source/Gapotchenko.FX.Linq/Operators/LinqOperatorPolyfills.cs
@@ -7,26 +7,36 @@
 public static class LinqOperatorPolyfills
 {
     /// <summary>
-    /// A LINQ-style pipe operator.
+    /// <para>
+    /// A LINQ-style pipe operator for programming languages that do not have a built-in pipe operator.
+    /// </para>
+    /// <para>
+    /// This is a polyfill provided by Gapotchenko.FX.
+    /// </para>
     /// </summary>
-    /// <typeparam name="T">The type of pipe input.</typeparam>
-    /// <typeparam name="TResult">The type of pipe result.</typeparam>
+    /// <typeparam name="TInput">The type of pipe input.</typeparam>
+    /// <typeparam name="TOutput">The type of pipe output.</typeparam>
     /// <param name="input">The pipe input.</param>
     /// <param name="func">The function which calculates the pipe result from the specified pipe <paramref name="input"/>.</param>
-    /// <returns>The pipe result which is calculated by <paramref name="func"/> from the specified pipe <paramref name="input"/>.</returns>
+    /// <returns>A pipe output which is calculated by <paramref name="func"/> from the specified pipe <paramref name="input"/>.</returns>
     /// <exception cref="ArgumentNullException">The <paramref name="func"/> is <see langword="null"/>.</exception>
-    public static TResult Pipe<T, TResult>(this T input, Func<T, TResult> func) =>
+    public static TOutput Pipe<TInput, TOutput>(this TInput input, Func<TInput, TOutput> func) =>
         (func ?? throw new ArgumentNullException(nameof(func)))
         (input);
 
     /// <summary>
-    /// A LINQ-style pipe operator.
+    /// <para>
+    /// A LINQ-style pipe operator for programming languages that do not have a built-in pipe operator.
+    /// </para>
+    /// <para>
+    /// This is a polyfill provided by Gapotchenko.FX.
+    /// </para>
     /// </summary>
-    /// <typeparam name="T">The type of pipe input.</typeparam>
+    /// <typeparam name="TInput">The type of pipe input.</typeparam>
     /// <param name="input">The pipe input.</param>
     /// <param name="action">The method that receives the specified pipe <paramref name="input"/>.</param>
     /// <exception cref="ArgumentNullException">The <paramref name="action"/> is <see langword="null"/>.</exception>
-    public static void Pipe<T>(this T input, Action<T> action) =>
+    public static void Pipe<TInput>(this TInput input, Action<TInput> action) =>
         (action ?? throw new ArgumentNullException(nameof(action)))
         (input);
 }
