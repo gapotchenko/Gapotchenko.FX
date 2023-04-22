fb24b6c2ffbf2554ea7e1aa60f9fa5756f8b2381
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2023-04-22 02:02:54 +0200

    Pipe operator polyfill renamed to PipeOperator.

--- Source/Gapotchenko.FX.Linq/Operators/LinqOperatorPolyfills.cs
+++ Source/Gapotchenko.FX.Linq/Operators/LinqOperatorPolyfills.cs
@@ -20,7 +20,7 @@ public static class LinqOperatorPolyfills
     /// <param name="func">The function which calculates the pipe result from the specified pipe <paramref name="input"/>.</param>
     /// <returns>A pipe output which is calculated by <paramref name="func"/> from the specified pipe <paramref name="input"/>.</returns>
     /// <exception cref="ArgumentNullException">The <paramref name="func"/> is <see langword="null"/>.</exception>
-    public static TOutput Pipe<TInput, TOutput>(this TInput input, Func<TInput, TOutput> func) =>
+    public static TOutput PipeOperator<TInput, TOutput>(this TInput input, Func<TInput, TOutput> func) =>
         (func ?? throw new ArgumentNullException(nameof(func)))
         (input);
 
@@ -36,7 +36,7 @@ public static class LinqOperatorPolyfills
     /// <param name="input">The pipe input.</param>
     /// <param name="action">The method that receives the specified pipe <paramref name="input"/>.</param>
     /// <exception cref="ArgumentNullException">The <paramref name="action"/> is <see langword="null"/>.</exception>
-    public static void Pipe<TInput>(this TInput input, Action<TInput> action) =>
+    public static void PipeOperator<TInput>(this TInput input, Action<TInput> action) =>
         (action ?? throw new ArgumentNullException(nameof(action)))
         (input);
 }
