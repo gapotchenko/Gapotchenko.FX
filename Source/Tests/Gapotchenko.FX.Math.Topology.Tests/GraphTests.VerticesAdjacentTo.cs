653dbeb680866af1f560fe4871c73e1f9213058d
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2023-04-26 15:22:21 +0200

    Graph: tests in progress.

--- Source/Tests/Gapotchenko.FX.Math.Topology.Tests/GraphTests.VerticesAdjacentTo.cs
+++ Source/Tests/Gapotchenko.FX.Math.Topology.Tests/GraphTests.VerticesAdjacentTo.cs
@@ -4,6 +4,57 @@ namespace Gapotchenko.FX.Math.Topology.Tests;
 
 partial class GraphTests
 {
+    [TestMethod]
+    [DataRow(true)]
+    [DataRow(false)]
+    public void Graph_VerticesAdjacentTo(bool directed)
+    {
+        var g = new Graph<int>
+        {
+            IsDirected = directed,
+            Edges = { (0, 1), (1, 2), (3, 2), (2, 5), (7, 1), (7, 2), (7, 5) }
+        };
+
+        var result = g.VerticesAdjacentTo(2).ToList();
+        Assert.AreEqual(4, result.Count);
+
+        Assert.IsTrue(new HashSet<int> { 1, 3, 5, 7 }.SetEquals(result));
+    }
+
+    [TestMethod]
+    [DataRow(true)]
+    [DataRow(false)]
+    public void Graph_VerticesAdjacentTo_WithBuckle(bool directed)
+    {
+        var g = new Graph<int>
+        {
+            IsDirected = directed,
+            Edges = { (0, 1), (1, 2), (3, 2), (2, 5), (7, 1), (7, 2), (7, 5), (2, 2) }
+        };
+
+        var result = g.VerticesAdjacentTo(2).ToList();
+        Assert.AreEqual(5, result.Count);
+
+        Assert.IsTrue(new HashSet<int> { 1, 2, 3, 5, 7 }.SetEquals(result));
+    }
+
+    [TestMethod]
+    [DataRow(true)]
+    [DataRow(false)]
+    public void Graph_VerticesAdjacentTo_WithBackReference(bool directed)
+    {
+        var g = new Graph<int>
+        {
+            IsDirected = directed,
+            Edges = { (0, 1), (1, 2), (3, 2), (2, 5), (7, 1), (7, 2), (7, 5), (5, 2) }
+        };
+
+        var result = g.VerticesAdjacentTo(2).ToList();
+        Assert.AreEqual(4, result.Count);
+
+        Assert.IsTrue(new HashSet<int> { 1, 3, 5, 7 }.SetEquals(result));
+    }
+
     #region Directed
 
     [TestMethod]
@@ -60,48 +111,6 @@ partial class GraphTests
         Assert.AreEqual(2, result.Count);
 
         Assert.IsTrue(new HashSet<int> { 2, 5 }.SetEquals(result));
-    }
-
-    [TestMethod]
-    public void Graph_Directed_VerticesAdjacentTo()
-    {
-        var g = new Graph<int>
-        {
-            Edges = { (0, 1), (1, 2), (3, 2), (2, 5), (7, 1), (7, 2), (7, 5) }
-        };
-
-        var result = g.VerticesAdjacentTo(2).ToList();
-        Assert.AreEqual(4, result.Count);
-
-        Assert.IsTrue(new HashSet<int> { 1, 3, 5, 7 }.SetEquals(result));
-    }
-
-    [TestMethod]
-    public void Graph_Directed_VerticesAdjacentTo_WithBuckle()
-    {
-        var g = new Graph<int>
-        {
-            Edges = { (0, 1), (1, 2), (3, 2), (2, 5), (7, 1), (7, 2), (7, 5), (2, 2) }
-        };
-
-        var result = g.VerticesAdjacentTo(2).ToList();
-        Assert.AreEqual(5, result.Count);
-
-        Assert.IsTrue(new HashSet<int> { 1, 2, 3, 5, 7 }.SetEquals(result));
-    }
-
-    [TestMethod]
-    public void Graph_Directed_VerticesAdjacentTo_WithBackReference()
-    {
-        var g = new Graph<int>
-        {
-            Edges = { (0, 1), (1, 2), (3, 2), (2, 5), (7, 1), (7, 2), (7, 5), (5, 2) }
-        };
-
-        var result = g.VerticesAdjacentTo(2).ToList();
-        Assert.AreEqual(4, result.Count);
-
-        Assert.IsTrue(new HashSet<int> { 1, 3, 5, 7 }.SetEquals(result));
     }
 
     #endregion
