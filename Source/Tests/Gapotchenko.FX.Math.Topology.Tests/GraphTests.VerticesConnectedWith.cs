398ab4b2b9e54a32576a9efb078129df02a605a7
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2023-04-26 17:51:23 +0200

    Graph: tests in progress.

--- Source/Tests/Gapotchenko.FX.Math.Topology.Tests/GraphTests.VerticesConnectedWith.cs
+++ Source/Tests/Gapotchenko.FX.Math.Topology.Tests/GraphTests.VerticesConnectedWith.cs
@@ -1,9 +1,69 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Gapotchenko.FX.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Gapotchenko.FX.Math.Topology.Tests;
 
 partial class GraphTests
 {
+    [TestMethod]
+    [DataRow(true)]
+    public void Graph_VerticesConnectedWith(bool directed)
+    {
+        var g = new Graph<int>
+        {
+            IsDirected = directed,
+            Edges = { (0, 1), (1, 2), (3, 2), (2, 5), (7, 5) }
+        };
+
+        Graph_VerticesConnectedWith(g, 2, new HashSet<int> { 0, 1, 3, 5 });
+    }
+
+    static void Graph_VerticesConnectedWith<T>(Graph<T> graph, T from, HashSet<T> expectedResult)
+    {
+        var actualResult = graph.VerticesConnectedWith(from).ToList();
+        Graph_VerticesConnectedWith(actualResult, expectedResult);
+
+        if (!graph.IsDirected)
+        {
+            //Assert.IsTrue(actualResult.SequenceEqual(graph.IncomingVerticesConnectedWith(from)));
+            //Assert.IsTrue(actualResult.SequenceEqual(graph.OutgoingVerticesConnectedWith(from)));
+        }
+    }
+
+    static void Graph_VerticesConnectedWith<T>(IEnumerable<T> actualResult, HashSet<T> expectedResult)
+    {
+        actualResult = actualResult.Memoize();
+
+        Assert.IsTrue(expectedResult.SetEquals(actualResult));
+        Assert.AreEqual(expectedResult.Count, actualResult.Count(), "The result contains duplicates.");
+    }
+
+    [TestMethod]
+    [DataRow(true)]
+    public void Graph_VerticesConnectedWith_WithBuckle(bool directed)
+    {
+        var g = new Graph<int>
+        {
+            IsDirected = directed,
+            Edges = { (0, 1), (1, 2), (3, 2), (2, 5), (7, 5), (2, 2) }
+        };
+
+        Graph_VerticesConnectedWith(g, 2, new HashSet<int> { 0, 1, 2, 3, 5 });
+    }
+
+    [TestMethod]
+    [DataRow(true)]
+    public void Graph_VerticesConnectedWith_WithBackReference(bool directed)
+    {
+        var g = new Graph<int>
+        {
+            IsDirected = directed,
+            Edges = { (0, 1), (1, 2), (3, 2), (2, 5), (7, 5), (2, 6), (6, 8), (8, 0) }
+        };
+
+        Graph_VerticesConnectedWith(g, 2, new HashSet<int> { 0, 1, 2, 3, 5, 6, 8 });
+    }
+
     #region Directed
 
     [TestMethod]
@@ -14,10 +74,9 @@ partial class GraphTests
             Edges = { (0, 1), (1, 2), (3, 2), (2, 5), (7, 1), (7, 2), (7, 5) }
         };
 
-        var result = g.IncomingVerticesConnectedWith(2).ToList();
-        Assert.AreEqual(4, result.Count);
-
-        Assert.IsTrue(new HashSet<int> { 0, 1, 3, 7 }.SetEquals(result));
+        Graph_VerticesConnectedWith(
+            g.IncomingVerticesConnectedWith(2),
+            new HashSet<int> { 0, 1, 3, 7 });
     }
 
     [TestMethod]
@@ -28,10 +87,9 @@ partial class GraphTests
             Edges = { (0, 1), (1, 2), (3, 2), (2, 5), (7, 1), (7, 2), (7, 5), (2, 2) }
         };
 
-        var result = g.IncomingVerticesConnectedWith(2).ToList();
-        Assert.AreEqual(5, result.Count);
-
-        Assert.IsTrue(new HashSet<int> { 0, 1, 2, 3, 7 }.SetEquals(result));
+        Graph_VerticesConnectedWith(
+            g.IncomingVerticesConnectedWith(2),
+            new HashSet<int> { 0, 1, 2, 3, 7 });
     }
 
     [TestMethod]
@@ -42,10 +100,9 @@ partial class GraphTests
             Edges = { (1, 0), (2, 1), (2, 3), (5, 2), (1, 7), (2, 7), (5, 7) }
         };
 
-        var result = g.OutgoingVerticesConnectedWith(2).ToList();
-        Assert.AreEqual(4, result.Count);
-
-        Assert.IsTrue(new HashSet<int> { 0, 1, 3, 7 }.SetEquals(result));
+        Graph_VerticesConnectedWith(
+            g.OutgoingVerticesConnectedWith(2),
+            new HashSet<int> { 0, 1, 3, 7 });
     }
 
     [TestMethod]
@@ -56,57 +113,10 @@ partial class GraphTests
             Edges = { (1, 0), (2, 1), (2, 3), (5, 2), (1, 7), (2, 7), (5, 7), (2, 2) }
         };
 
-        var result = g.OutgoingVerticesConnectedWith(2).ToList();
-        Assert.AreEqual(5, result.Count);
-
-        Assert.IsTrue(new HashSet<int> { 0, 1, 2, 3, 7 }.SetEquals(result));
+        Graph_VerticesConnectedWith(
+            g.OutgoingVerticesConnectedWith(2),
+            new HashSet<int> { 0, 1, 2, 3, 7 });
     }
-
-    [TestMethod]
-    public void Graph_Directed_VerticesConnectedWith()
-    {
-        var g = new Graph<int>
-        {
-            Edges = { (0, 1), (1, 2), (3, 2), (2, 5), (7, 5) }
-        };
-
-        var result = g.VerticesConnectedWith(2).ToList();
-        Assert.AreEqual(4, result.Count);
-
-        Assert.IsTrue(new HashSet<int> { 0, 1, 3, 5 }.SetEquals(result));
-    }
-
-    [TestMethod]
-    public void Graph_Directed_VerticesConnectedWith_WithBuckle()
-    {
-        var g = new Graph<int>
-        {
-            Edges = { (0, 1), (1, 2), (3, 2), (2, 5), (7, 5), (2, 2) }
-        };
-
-        var result = g.VerticesConnectedWith(2).ToList();
-        Assert.AreEqual(5, result.Count);
-
-        Assert.IsTrue(new HashSet<int> { 0, 1, 2, 3, 5 }.SetEquals(result));
-    }
-
-    [TestMethod]
-    public void Graph_Directed_VerticesConnectedWith_WithBackReference()
-    {
-        var g = new Graph<int>
-        {
-            Edges = { (0, 1), (1, 2), (3, 2), (2, 5), (7, 5), (2, 6), (6, 8), (8, 0) }
-        };
-
-        var result = g.VerticesConnectedWith(2).ToList();
-        Assert.AreEqual(7, result.Count);
-
-        Assert.IsTrue(new HashSet<int> { 0, 1, 2, 3, 5, 6, 8 }.SetEquals(result));
-    }
-
-    #endregion
-
-    #region Undirected
 
     #endregion
 }
