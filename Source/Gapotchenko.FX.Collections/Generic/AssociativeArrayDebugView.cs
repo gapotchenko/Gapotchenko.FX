0b9b83348ae8e40dd44187e663abe5df1b1bab19
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2023-05-15 01:04:26 +0200

    Refactoring.

--- Source/Gapotchenko.FX.Collections/Generic/AssociativeArrayDebugView.cs
+++ Source/Gapotchenko.FX.Collections/Generic/AssociativeArrayDebugView.cs
@@ -4,19 +4,19 @@ namespace Gapotchenko.FX.Collections.Generic;
 
 sealed class AssociativeArrayDebugView<TKey, TValue>
 {
-    readonly AssociativeArray<TKey, TValue> _associativeArray;
+    readonly AssociativeArray<TKey, TValue> m_AssociativeArray;
 
     public AssociativeArrayDebugView(AssociativeArray<TKey, TValue> associativeArray) =>
-        _associativeArray = associativeArray ?? throw new ArgumentNullException(nameof(associativeArray));
+        m_AssociativeArray = associativeArray ?? throw new ArgumentNullException(nameof(associativeArray));
 
     [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
     public KeyValuePair<TKey, TValue>[] Items
     {
         get
         {
-            var items = new KeyValuePair<TKey, TValue>[_associativeArray.Count];
+            var items = new KeyValuePair<TKey, TValue>[m_AssociativeArray.Count];
 #pragma warning disable CS8714
-            ((IDictionary<TKey, TValue>)_associativeArray).CopyTo(items, 0);
+            ((IDictionary<TKey, TValue>)m_AssociativeArray).CopyTo(items, 0);
 #pragma warning restore CS8714
             return items;
         }
@@ -25,10 +25,10 @@ sealed class AssociativeArrayDebugView<TKey, TValue>
 
 sealed class AssociativeArrayKeyValueCollectionDebugView<TKey, TValue, T>
 {
-    readonly ICollection<T> _collection;
+    readonly ICollection<T> m_Collection;
 
     public AssociativeArrayKeyValueCollectionDebugView(ICollection<T> collection) =>
-        _collection = collection ?? throw new ArgumentNullException(nameof(collection));
+        m_Collection = collection ?? throw new ArgumentNullException(nameof(collection));
 
     [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
     public T[] Items
@@ -36,8 +36,8 @@ sealed class AssociativeArrayKeyValueCollectionDebugView<TKey, TValue, T>
         get
         {
 
-            var items = new T[_collection.Count];
-            _collection.CopyTo(items, 0);
+            var items = new T[m_Collection.Count];
+            m_Collection.CopyTo(items, 0);
             return items;
         }
     }
