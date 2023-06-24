61726b8513416a48ce81bf484a1e2474a428c9a5
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2023-06-24 23:20:27 +0200

    Pipe operator is hidden from the editor to avoid the pollution.

--- Source/Gapotchenko.FX.Linq/Operators/LinqOperatorPolyfills.cs
+++ Source/Gapotchenko.FX.Linq/Operators/LinqOperatorPolyfills.cs
@@ -18,6 +18,7 @@ public static class LinqOperatorPolyfills
     /// <param name="func">The function which calculates the pipe result from the specified pipe <paramref name="input"/>.</param>
     /// <returns>A pipe output which is calculated by <paramref name="func"/> from the specified pipe <paramref name="input"/>.</returns>
     /// <exception cref="ArgumentNullException">The <paramref name="func"/> is <see langword="null"/>.</exception>
+    [EditorBrowsable(EditorBrowsableState.Never)]
     public static TOutput PipeOperator<TInput, TOutput>(this TInput input, Func<TInput, TOutput> func) =>
         (func ?? throw new ArgumentNullException(nameof(func)))
         (input);
@@ -32,6 +33,7 @@ public static class LinqOperatorPolyfills
     /// <param name="input">The pipe input.</param>
     /// <param name="action">The method that receives the specified pipe <paramref name="input"/>.</param>
     /// <exception cref="ArgumentNullException">The <paramref name="action"/> is <see langword="null"/>.</exception>
+    [EditorBrowsable(EditorBrowsableState.Never)]
     public static void PipeOperator<TInput>(this TInput input, Action<TInput> action) =>
         (action ?? throw new ArgumentNullException(nameof(action)))
         (input);
