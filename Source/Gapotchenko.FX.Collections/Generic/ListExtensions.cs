7f6044785b2dfe36aba2b5a2c2a31b27ab010a8a
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2023-07-01 14:58:32 +0200

    Improved documentation.

--- Source/Gapotchenko.FX.Collections/Generic/ListExtensions.cs
+++ Source/Gapotchenko.FX.Collections/Generic/ListExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace Gapotchenko.FX.Collections.Generic;
+﻿using Gapotchenko.FX.Collections.Utils;
+
+namespace Gapotchenko.FX.Collections.Generic;
 
 /// <summary>
 /// <see cref="List{T}"/> extensions.
@@ -7,18 +9,19 @@
 public static class ListExtensions
 {
     /// <summary>
-    /// Performs in-place stable sort of the elements in entire <see cref="List{T}"/> using the specified comparer.
+    /// Sorts the elements in the entire <see cref="List{T}"/>
+    /// using the specified comparer
+    /// and a stable sorting algorithm.
     /// </summary>
     /// <typeparam name="T">The type of elements in the list.</typeparam>
-    /// <param name="list">The list.</param>
+    /// <param name="list">The <see cref="List{T}"/>.</param>
     /// <param name="comparer">
     /// The <see cref="IComparer{T}"/> implementation to use when comparing elements,
     /// or <see langword="null"/> to use the default comparer.
     /// </param>
     public static void StableSort<T>(this List<T> list, IComparer<T>? comparer)
     {
-        if (list == null)
-            throw new ArgumentNullException(nameof(list));
+        ExceptionHelpers.ThrowIfArgumentIsNull(list);
 
         if (list.Count < 2)
             return;
@@ -30,17 +33,19 @@ public static class ListExtensions
     }
 
     /// <summary>
-    /// Performs in-place stable sort of the elements in entire <see cref="List{T}"/> using the default comparer.
+    /// Sorts the elements in the entire <see cref="List{T}"/>
+    /// using the default comparer
+    /// and a stable sorting algorithm.
     /// </summary>
     /// <typeparam name="T">The type of elements in the list.</typeparam>
     /// <param name="list">The list.</param>
     public static void StableSort<T>(this List<T> list) => StableSort(list, null);
 
     /// <summary>
-    /// Clones the list.
+    /// Clones the <see cref="List{T}"/>.
     /// </summary>
-    /// <typeparam name="T">The type of elements in the list.</typeparam>
-    /// <param name="list">The list.</param>
-    /// <returns>A cloned list.</returns>
+    /// <typeparam name="T">The type of elements in the <see cref="List{T}"/>.</typeparam>
+    /// <param name="list">The <see cref="List{T}"/>.</param>
+    /// <returns>A cloned <see cref="List{T}"/>.</returns>
     public static List<T> Clone<T>(this List<T> list) => new(list ?? throw new ArgumentNullException(nameof(list)));
 }
