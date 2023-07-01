6c8e58841dcedfcfdd624a2e6dbb87cab3451081
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2023-07-01 14:59:25 +0200

    Improved documentation.

--- Source/Gapotchenko.FX.Collections/Generic/ListExtensions.cs
+++ Source/Gapotchenko.FX.Collections/Generic/ListExtensions.cs
@@ -3,7 +3,7 @@
 namespace Gapotchenko.FX.Collections.Generic;
 
 /// <summary>
-/// <see cref="List{T}"/> extensions.
+/// Provides extension methods for <see cref="List{T}"/>.
 /// </summary>
 [EditorBrowsable(EditorBrowsableState.Never)]
 public static class ListExtensions
