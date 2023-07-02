31355ddd48548b53f3828833003bf05184ed09a8
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2023-07-02 18:28:03 +0200

    Deque tests in progress.

---
+++ Source/Tests/Gapotchenko.FX.Collections.Tests/Generic/Deque/Deque.Tests.Specializations.cs
@@ -0,0 +1,22 @@
+﻿namespace Gapotchenko.FX.Collections.Tests.Generic.Deque;
+
+public sealed class Deque_Tests_String : Deque_Tests<string>
+{
+    protected override string CreateT(int seed) => CreateString(new Random(seed));
+
+    static string CreateString(Random random)
+    {
+        int length = random.Next(5, 15);
+        var bytes = new byte[length];
+        random.NextBytes(bytes);
+
+        return Convert.ToBase64String(bytes);
+    }
+}
+
+public sealed class Deque_Tests_Int32 : Deque_Tests<int>
+{
+    protected override int CreateT(int seed) => CreateInt32(new Random(seed));
+
+    static int CreateInt32(Random random) => random.Next();
+}
---
+++ Source/Tests/Gapotchenko.FX.Collections.Tests/Generic/Deque/Deque.Tests.cs
@@ -0,0 +1,11 @@
+﻿using Gapotchenko.FX.Collections.Generic;
+using Gapotchenko.FX.Collections.Tests.Bench;
+
+namespace Gapotchenko.FX.Collections.Tests.Generic.Deque;
+
+public abstract class Deque_Tests<T> : IList_Generic_Tests<T>
+{
+    protected override IList<T> GenericIListFactory() => new Deque<T>();
+
+    protected abstract override T CreateT(int seed);
+}
