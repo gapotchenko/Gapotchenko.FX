4b50f063663677aa1de0bca795c220caa453d713
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2023-07-02 13:42:20 +0200

    Refactoring.

--- Source/Gapotchenko.FX.Collections/Generic/KeyValuePair.cs
+++ Source/Gapotchenko.FX.Collections/Generic/KeyValuePair.cs
@@ -1,14 +1,10 @@
 ﻿using System.Runtime.CompilerServices;
 
-#if TFF_KEYVALUEPAIR
-[assembly: TypeForwardedTo(typeof(System.Collections.Generic.KeyValuePair))]
-#endif
+#if !TFF_KEYVALUEPAIR
 
 #pragma warning disable IDE0130 // Namespace does not match folder structure
 
 namespace System.Collections.Generic;
-
-#if !TFF_KEYVALUEPAIR
 
 /// <summary>
 /// <para>
@@ -31,30 +27,8 @@ public static class KeyValuePair
     public static KeyValuePair<TKey, TValue> Create<TKey, TValue>(TKey key, TValue value) => new(key, value);
 }
 
-#endif
+#else
 
-/// <summary>
-/// <see cref="KeyValuePair{TKey, TValue}"/> polyfills.
-/// </summary>
-[EditorBrowsable(EditorBrowsableState.Never)]
-public static class KeyValuePairPolyfills
-{
-    /// <summary>
-    /// Deconstructs a <see cref="KeyValuePair{TKey, TValue}"/> value.
-    /// </summary>
-    /// <typeparam name="TKey">The type of key.</typeparam>
-    /// <typeparam name="TValue">The type of value.</typeparam>
-    /// <param name="pair">The pair.</param>
-    /// <param name="key">The key.</param>
-    /// <param name="value">The value.</param>
-    [EditorBrowsable(EditorBrowsableState.Never)]
-    public static void Deconstruct<TKey, TValue>(
-#if !TFF_KEYVALUEPAIR
-        this
+[assembly: TypeForwardedTo(typeof(KeyValuePair))]
+
 #endif
-        KeyValuePair<TKey, TValue> pair, out TKey key, out TValue value)
-    {
-        key = pair.Key;
-        value = pair.Value;
-    }
-}
---
+++ Source/Gapotchenko.FX.Collections/Generic/KeyValuePairPolyfills.cs
@@ -0,0 +1,33 @@
+﻿#pragma warning disable IDE0130 // Namespace does not match folder structure
+
+namespace System.Collections.Generic;
+
+/// <summary>
+/// Provides polyfill methods for <see cref="KeyValuePair{TKey, TValue}"/>.
+/// </summary>
+[EditorBrowsable(EditorBrowsableState.Never)]
+public static class KeyValuePairPolyfills
+{
+#if !TFF_KEYVALUEPAIR || BINARY_COMPATIBILITY
+    /// <summary>
+    /// Deconstructs a <see cref="KeyValuePair{TKey, TValue}"/> value.
+    /// </summary>
+    /// <typeparam name="TKey">The type of key.</typeparam>
+    /// <typeparam name="TValue">The type of value.</typeparam>
+    /// <param name="pair">The pair.</param>
+    /// <param name="key">The key.</param>
+    /// <param name="value">The value.</param>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static void Deconstruct<TKey, TValue>(
+#if !TFF_KEYVALUEPAIR
+        this
+#endif
+        KeyValuePair<TKey, TValue> pair,
+        out TKey key,
+        out TValue value)
+    {
+        key = pair.Key;
+        value = pair.Value;
+    }
+#endif
+}
