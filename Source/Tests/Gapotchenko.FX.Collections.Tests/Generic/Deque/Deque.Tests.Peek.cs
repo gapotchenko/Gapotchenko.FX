9872e6943edc416ef4a6c1c32d3077ef248910c9
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2023-07-04 15:34:26 +0200

    Deque<T> tests in progress.

---
+++ Source/Tests/Gapotchenko.FX.Collections.Tests/Generic/Deque/Deque.Tests.Peek.cs
@@ -0,0 +1,98 @@
+﻿// Gapotchenko.FX
+// Copyright © Gapotchenko and Contributors
+//
+// Portions © Masashi Mizuno
+//
+// File introduced by: Oleksiy Gapotchenko
+// Year of introduction: 2023
+
+using Gapotchenko.FX.Collections.Generic;
+using Gapotchenko.FX.Linq;
+using Xunit;
+
+namespace Gapotchenko.FX.Collections.Tests.Generic.Deque;
+
+partial class Deque_Tests<T>
+{
+    #region PeekFront
+
+    [Fact]
+    public void PeekFront()
+    {
+        var data = Enumerable.Range(1, int.MaxValue).Select(CreateT).Distinct().Take(3).ReifyList();
+
+        var deque = new Deque<T>(data);
+        Assert.Equal(data[0], deque.PeekFront());
+    }
+
+    [Fact]
+    public void PeekFront_ThrowsOnEmpty()
+    {
+        var deque = new Deque<T>();
+        Assert.Throws<InvalidOperationException>(() => deque.PeekFront());
+    }
+
+    #endregion
+
+    #region PeekBack
+
+    [Fact]
+    public void PeekBack()
+    {
+        var data = Enumerable.Range(1, int.MaxValue).Select(CreateT).Distinct().Take(3).ReifyList();
+
+        var deque = new Deque<T>(data);
+        Assert.Equal(data[^1], deque.PeekBack());
+    }
+
+    [Fact]
+    public void PeekBack_ThrowsOnEmpty()
+    {
+        var deque = new Deque<T>();
+        Assert.Throws<InvalidOperationException>(() => deque.PeekBack());
+    }
+
+    #endregion
+
+    #region TryPeekFront
+
+    [Fact]
+    public void TryPeekFront()
+    {
+        var data = Enumerable.Range(1, int.MaxValue).Select(CreateT).Distinct().Take(3).ReifyList();
+
+        var deque = new Deque<T>(data);
+        Assert.True(deque.TryPeekFront(out var item));
+        Assert.Equal(data[0], item);
+    }
+
+    [Fact]
+    public void TryPeekFront_Empty()
+    {
+        var deque = new Deque<T>();
+        Assert.False(deque.TryPeekFront(out _));
+    }
+
+    #endregion
+
+    #region TryPeekBack
+
+    [Fact]
+    public void TryPeekBack()
+    {
+        var data = Enumerable.Range(1, int.MaxValue).Select(CreateT).Distinct().Take(3).ReifyList();
+
+        var deque = new Deque<T>(data);
+        Assert.True(deque.TryPeekBack(out var item));
+        Assert.Equal(data[^1], item);
+    }
+
+    [Fact]
+    public void TryPeekBack_Empty()
+    {
+        var deque = new Deque<T>();
+        Assert.False(deque.TryPeekBack(out _));
+    }
+
+    #endregion
+}
--- Source/Tests/Gapotchenko.FX.Collections.Tests/Generic/Deque/Deque.Tests.Pop.cs
+++ Source/Tests/Gapotchenko.FX.Collections.Tests/Generic/Deque/Deque.Tests.Pop.cs
@@ -1,4 +1,12 @@
-﻿using Gapotchenko.FX.Collections.Generic;
+﻿// Gapotchenko.FX
+// Copyright © Gapotchenko and Contributors
+//
+// Portions © Masashi Mizuno
+//
+// File introduced by: Oleksiy Gapotchenko
+// Year of introduction: 2023
+
+using Gapotchenko.FX.Collections.Generic;
 using Gapotchenko.FX.Collections.Tests.Utils;
 using Gapotchenko.FX.Linq;
 using Xunit;
@@ -86,6 +94,45 @@ partial class Deque_Tests<T>
             });
 
         Assert.Equal(list, deque);
+    }
+
+    [Fact]
+    public void TryPopFront_Empty()
+    {
+        var deque = new Deque<T>();
+        Assert.False(deque.TryPopFront(out _));
+    }
+
+    #endregion
+
+    #region TryPopBack
+
+    [Fact]
+    public void TryPopBack()
+    {
+        var data = Enumerable.Range(1, int.MaxValue).Select(CreateT).Distinct().Take(3).ReifyList();
+
+        var list = new List<T>(data);
+        var deque = new Deque<T>(data);
+
+        list.RemoveAt(list.Count - 1);
+
+        ModificationTrackingVerifier.EnsureModified(
+            deque,
+            () =>
+            {
+                Assert.True(deque.TryPopBack(out var item));
+                Assert.Equal(data[^1], item);
+            });
+
+        Assert.Equal(list, deque);
+    }
+
+    [Fact]
+    public void TryPopBack_Empty()
+    {
+        var deque = new Deque<T>();
+        Assert.False(deque.TryPopBack(out _));
     }
 
     #endregion
