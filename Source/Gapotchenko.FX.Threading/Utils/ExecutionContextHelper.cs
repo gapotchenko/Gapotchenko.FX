991947ba6e30fed5ebe57351cbc5074971499d17
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2023-07-08 13:07:43 +0200

    AsyncRecursiveMutex in progress.

--- Source/Gapotchenko.FX.Threading/Utils/ExecutionContextHelper.cs
+++ Source/Gapotchenko.FX.Threading/Utils/ExecutionContextHelper.cs
@@ -3,6 +3,18 @@
 //
 // File introduced by: Oleksiy Gapotchenko
 // Year of introduction: 2023
+//
+// This file contains an implementation of the backbone algorithm that makes
+// reentrancy tracking in asynchronous .NET code possible. Before that, the
+// tricks like that were thought to be impossible in .NET because
+// AsyncLocal<T> class only supports the inward flow of ambient data.
+//
+// Needless to say that this whole situation led to the industry stagnation
+// circa 2015-2023 because nobody had enough persistence in solving that
+// puzzle. In turn, that led to a plethora of half-baked attempts in cracking
+// asynchronous recursion that never really worked - they were either too slow
+// (by using StackTrace) or unreliable (by using Task.CurrentId which is prone
+// to collisions).
 
 using System.Diagnostics;
 
@@ -20,25 +32,26 @@ static class ExecutionContextHelper
     /// which only supports the inward flow of the ambient data.
     /// </remarks>
     /// <param name="action">The <see cref="Action{T}"/> that directly or indirectly modifies an <see cref="AsyncLocal{T}.Value"/> property.</param>
+    public static AsyncLocalModificationContext ModifyAsyncLocal(Action action) =>
+        new(action);
+
+    /// <inheritdoc cref="ModifyAsyncLocal(Action)"/>
     public static AsyncLocalModificationContext<TValue> ModifyAsyncLocal<TValue>(Action<TValue> action) =>
         new(action);
 
-    public sealed class AsyncLocalModificationContext<TValue>
+    public abstract class AsyncLocalModificationContextBase
     {
-        internal AsyncLocalModificationContext(Action<TValue> action)
+        protected AsyncLocalModificationContextBase()
         {
-            m_Action = action;
-
             Debug.Assert(
                 m_FlowState.Value is var flowState &&
                 (flowState == null || flowState.Context.m_State is State.Committed or State.Discarded),
-                $"{nameof(ModifyAsyncLocal)} does not support recursion.");
+                $"{nameof(ModifyAsyncLocal)} does not support recursion - be sure to Commit or Discard the previously created context before creating a new one.");
+
             m_FlowState.Value = new FlowState(this, false);
         }
 
-        readonly Action<TValue> m_Action;
-
-        record FlowState(AsyncLocalModificationContext<TValue> Context, bool ActionHandled);
+        record FlowState(AsyncLocalModificationContextBase Context, bool ActionHandled);
 
         static readonly AsyncLocal<FlowState?> m_FlowState = new(FlowStateChanged);
 
@@ -116,24 +129,22 @@ static class ExecutionContextHelper
 
         // Using volatile access to ensure that a committed value is always visible in the committed state.
         volatile State m_State;
-        Volatile<TValue?> m_CommittedValue;
 
-        /// <summary>
-        /// Commits the changes.
-        /// </summary>
-        public void Commit(TValue value)
+        protected void ValidateCommit()
         {
             if (m_State != State.Initialized)
                 throw new InvalidOperationException();
+        }
 
-            m_CommittedValue.Value = value;
+        protected void DoCommit()
+        {
             m_State = State.Committed;
 
             // Apply the changes to the current control flow branch.
             if (m_FlowState.Value is not null and var flowState)
                 UpdateFlowState(flowState); // either using the flow state
             else
-                m_Action(value); // or directly
+                DoAction(); // or directly
         }
 
         /// <summary>
@@ -150,10 +161,62 @@ static class ExecutionContextHelper
             m_State = State.Discarded;
         }
 
-        void DoAction()
+        protected virtual void DoAction()
         {
             Debug.Assert(m_State == State.Committed);
+        }
+    }
 
+    public sealed class AsyncLocalModificationContext : AsyncLocalModificationContextBase
+    {
+        internal AsyncLocalModificationContext(Action action)
+        {
+            m_Action = action;
+        }
+
+        readonly Action m_Action;
+
+        /// <summary>
+        /// Commits the changes.
+        /// </summary>
+        public void Commit()
+        {
+            ValidateCommit();
+            DoCommit();
+        }
+
+        protected override void DoAction()
+        {
+            base.DoAction();
+            m_Action();
+        }
+    }
+
+    public sealed class AsyncLocalModificationContext<TValue> : AsyncLocalModificationContextBase
+    {
+        internal AsyncLocalModificationContext(Action<TValue> action)
+        {
+            m_Action = action;
+        }
+
+        readonly Action<TValue> m_Action;
+
+        // Using volatile access to ensure that a committed value is always visible in the committed state.
+        Volatile<TValue?> m_CommittedValue;
+
+        /// <summary>
+        /// Commits the changes.
+        /// </summary>
+        public void Commit(TValue value)
+        {
+            ValidateCommit();
+            m_CommittedValue.Value = value;
+            DoCommit();
+        }
+
+        protected override void DoAction()
+        {
+            base.DoAction();
             m_Action(m_CommittedValue.Value!);
         }
     }
