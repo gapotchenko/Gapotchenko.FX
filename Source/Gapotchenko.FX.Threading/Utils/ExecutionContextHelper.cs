d99f9248a179980f990d64a0cd5568b6e68d1fc2
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2023-07-08 02:41:39 +0200

    AsyncRecursiveMutex in progress.

--- Source/Gapotchenko.FX.Threading/Utils/ExecutionContextHelper.cs
+++ Source/Gapotchenko.FX.Threading/Utils/ExecutionContextHelper.cs
@@ -1,96 +1,155 @@
-﻿namespace Gapotchenko.FX.Threading.Utils;
+﻿// Gapotchenko.FX
+// Copyright © Gapotchenko and Contributors
+//
+// File introduced by: Oleksiy Gapotchenko
+// Year of introduction: 2023
+
+namespace Gapotchenko.FX.Threading.Utils;
 
 static class ExecutionContextHelper
 {
     /// <summary>
     /// Modifies ambient data that is local to a given asynchronous control flow
-    /// by replaying the specified action in all control flow branches.
+    /// by replaying the specified action in all control flow branches
+    /// to make the data equivalent in all of them.
     /// </summary>
     /// <remarks>
-    /// The approach allows to overcome the limitations imposed by <see cref="AsyncLocal{T}"/> class
+    /// This approach allows to overcome the limitations imposed by <see cref="AsyncLocal{T}"/> class
     /// which only supports the inward flow of the ambient data.
     /// </remarks>
     /// <param name="action">The <see cref="Action{T}"/> that directly or indirectly modifies an <see cref="AsyncLocal{T}.Value"/> property.</param>
     public static AsyncLocalModificationContext<TValue> ModifyAsyncLocal<TValue>(Action<TValue> action) =>
         new(action);
 
-    public class AsyncLocalModificationContext<TValue>
+    public sealed class AsyncLocalModificationContext<TValue>
     {
         internal AsyncLocalModificationContext(Action<TValue> action)
         {
             m_Action = action;
 
-            m_FlowTracker = new(FlowStateChanged)
+            m_FlowState = new(FlowStateChanged)
             {
-                Value = new FlowState(false, this)
+                // GC root in the flow state record holds a reference to this context
+                // so it could continue to receive notifications on the flow state changes
+                // even after the context goes out of the visibility scope in the code that uses it.
+                Value = new FlowState(this, false)
             };
         }
 
         readonly Action<TValue> m_Action;
-        readonly AsyncLocal<FlowState?> m_FlowTracker;
+        readonly AsyncLocal<FlowState?> m_FlowState;
 
-        record FlowState(bool ActionHandled, object GCRoot);
+        record FlowState(object GCRoot, bool ActionHandled);
 
         void FlowStateChanged(AsyncLocalValueChangedArgs<FlowState?> args)
         {
             if (!args.ThreadContextChanged)
                 return;
 
-            if (m_Committed &&
-                args.CurrentValue != args.PreviousValue)
+            switch (m_State)
             {
-                var state = args.CurrentValue;
-                if (state != null)
-                    UpdateFlowState(state);
+                case State.Initialized:
+                    // Nothing is ready yet.
+                    break;
+
+                case State.Committed:
+                    if (args.CurrentValue != args.PreviousValue)
+                    {
+                        if (args.CurrentValue is not null and var flowState)
+                        {
+                            // Propagate the changes to the current control flow branch.
+                            UpdateFlowState(flowState);
+                        }
+                    }
+                    break;
+
+                case State.Discarded:
+                    if (args.CurrentValue is not null)
+                    {
+                        // Delete any existing flow state - they all are discarded.
+                        m_FlowState.Value = null;
+                    }
+                    break;
+
+                default:
+                    throw new InvalidOperationException();
             }
         }
 
         void UpdateFlowState(FlowState state)
         {
-            var newState = TickFlowState(state);
+            var newState = GetNextFlowState(state);
             if (newState != state)
-                m_FlowTracker.Value = newState;
+                m_FlowState.Value = newState;
         }
 
-        FlowState? TickFlowState(FlowState state)
+        FlowState? GetNextFlowState(FlowState state)
         {
             bool actionHandled = state.ActionHandled;
 
             if (!actionHandled)
             {
+                // Replay the changes to the current control flow branch.
                 m_Action(m_CommittedValue.Value!);
                 actionHandled = true;
             }
 
             FlowState? newState;
             if (actionHandled)
+            {
+                // All actions are done - no need to hold a flow state anymore.
                 newState = null;
+            }
             else
+            {
                 newState = state;
+            }
 
             return newState;
         }
 
+        enum State
+        {
+            Initialized,
+            Committed,
+            Discarded
+        }
+
+        // Using volatile access to ensure that a committed value is always visible in the committed state.
+        volatile State m_State;
         Volatile<TValue?> m_CommittedValue;
-        volatile bool m_Committed;
 
         /// <summary>
-        /// Restores the flow of the execution context across synchronous threads to the original state.
+        /// Commits the changes.
         /// </summary>
         public void Commit(TValue value)
         {
-            if (m_Committed)
+            if (m_State != State.Initialized)
                 throw new InvalidOperationException();
 
             m_CommittedValue.Value = value;
 
-            var state = m_FlowTracker.Value;
-            if (state != null)
-                UpdateFlowState(state);
+            // Apply the changes to the current control flow branch.
+            if (m_FlowState.Value is not null and var flowState)
+                UpdateFlowState(flowState); // either using the flow state
             else
-                m_Action(value);
+                m_Action(value); // or directly
 
-            m_Committed = true;
+            m_State = State.Committed;
+        }
+
+        /// <summary>
+        /// Discards the changes.
+        /// </summary>
+        public void Discard()
+        {
+            if (m_State != State.Initialized)
+                throw new InvalidOperationException();
+
+            if (m_FlowState.Value is not null)
+                m_FlowState.Value = null;
+
+            m_State = State.Discarded;
         }
     }
 }
