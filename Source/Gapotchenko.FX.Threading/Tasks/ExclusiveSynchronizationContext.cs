4c737a332739d24f3dba48c510e040241e4318f5
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2023-07-15 01:21:41 +0200

    TaskBridge should execute work items of asynchronous tasks in CER regions.

--- Source/Gapotchenko.FX.Threading/Tasks/ExclusiveSynchronizationContext.cs
+++ Source/Gapotchenko.FX.Threading/Tasks/ExclusiveSynchronizationContext.cs
@@ -6,6 +6,8 @@
 
 using Gapotchenko.FX.Threading.Utils;
 using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using System.Runtime.ExceptionServices;
 
 namespace Gapotchenko.FX.Threading.Tasks;
@@ -14,7 +16,19 @@ sealed class ExclusiveSynchronizationContext : SynchronizationContext, IDisposab
 {
     readonly BlockingCollection<KeyValuePair<SendOrPostCallback, object?>> m_Queue = new();
 
-    public override void Post(SendOrPostCallback d, object? state) => m_Queue.Add(new(d, state));
+    public override void Post(SendOrPostCallback d, object? state)
+    {
+        try
+        {
+            m_Queue.Add(new(d, state));
+        }
+        catch (ObjectDisposedException)
+        {
+            Debug.Fail("Asynchronous task cannot execute after the synchronization context has been disposed.");
+
+            // Do no rethrow the exception because it may crash the whole process.
+        }
+    }
 
     public override SynchronizationContext CreateCopy() => this;
 
@@ -59,8 +73,19 @@ sealed class ExclusiveSynchronizationContext : SynchronizationContext, IDisposab
     {
         while (m_Queue.TryTake(out var item, Timeout.Infinite))
         {
-            // Execute the queued callback.
-            item.Key(item.Value);
+#if TFF_CER
+            // Execute the chunk of work related to an asynchronous task in a constrained region
+            // because the task cannot interact with a task scheduler after the thread is aborted.
+            RuntimeHelpers.PrepareConstrainedRegionsNoOP();
+            try
+            {
+            }
+            finally
+#endif
+            {
+                // Execute the queued callback.
+                item.Key(item.Value);
+            }
         }
     }
 
