8afb92a6af0ae0685c4c127258e29cb8f2af1fdd
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2023-07-17 14:22:29 +0200

    Added AsyncRecursiveMonitor implementation.

---
+++ Source/Gapotchenko.FX.Threading/AsyncRecursiveMonitor.cs
@@ -0,0 +1,26 @@
+﻿// Gapotchenko.FX
+// Copyright © Gapotchenko and Contributors
+//
+// File introduced by: Oleksiy Gapotchenko
+// Year of introduction: 2023
+
+namespace Gapotchenko.FX.Threading;
+
+/// <summary>
+/// Represents a reentrant synchronization primitive
+/// that provides a mechanism that synchronizes access to objects.
+/// The primitive supports both synchronous and asynchronous operations.
+/// </summary>
+public sealed class AsyncRecursiveMonitor : AsyncMonitorImpl<AsyncRecursiveMutex>, IAsyncRecursiveMonitor
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AsyncRecursiveMonitor"/> class.
+    /// </summary>
+    public AsyncRecursiveMonitor() :
+        base(new AsyncRecursiveMutex())
+    {
+    }
+
+    /// <inheritdoc/>
+    public bool IsLockHeld => Lockable.IsLockHeld;
+}
---
+++ Source/Gapotchenko.FX.Threading/IAsyncRecursiveMonitor.cs
@@ -0,0 +1,15 @@
+﻿// Gapotchenko.FX
+// Copyright © Gapotchenko and Contributors
+//
+// File introduced by: Oleksiy Gapotchenko
+// Year of introduction: 2023
+
+namespace Gapotchenko.FX.Threading;
+
+/// <summary>
+/// Defines the interface of a reentrant synchronization primitive that provides a mechanism that synchronizes access to objects.
+/// The primitive supports both synchronous and asynchronous operations.
+/// </summary>
+public interface IAsyncRecursiveMonitor : IAsyncMonitor, IAsyncRecursiveLockable
+{
+}
