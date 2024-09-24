84cc0da66f8c0c4947eea43a1c8a290a5e285168
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2024-09-24 14:54:43 +0200

    Accelerated Graph<T>.GetVertexIndegree operation for graphs with a reverse adjacency list.

--- Source/Gapotchenko.FX.Math.Graphs/Graph`1.Degree.cs
+++ Source/Gapotchenko.FX.Math.Graphs/Graph`1.Degree.cs
@@ -4,19 +4,32 @@
 // File introduced by: Oleksiy Gapotchenko
 // Year of introduction: 2021
 
+using Gapotchenko.FX.Collections.Generic;
+
 namespace Gapotchenko.FX.Math.Graphs;
 
 partial class Graph<TVertex>
 {
     /// <inheritdoc />
-    public int GetVertexIndegree(TVertex vertex) =>
-        m_AdjacencyList
-            .Where(kv => kv.Value?.Contains(vertex) == true)
-            .Count();
+    public int GetVertexIndegree(TVertex vertex)
+    {
+        if (m_ReverseAdjacencyList != null)
+        {
+            return GetVertexOutdegreeCore(m_ReverseAdjacencyList, vertex);
+        }
+        else
+        {
+            return m_AdjacencyList
+                .Where(kv => kv.Value?.Contains(vertex) == true)
+                .Count();
+        }
+    }
 
     /// <inheritdoc />
-    public int GetVertexOutdegree(TVertex vertex) =>
-        m_AdjacencyList.TryGetValue(vertex, out var adjacencyRow) ?
+    public int GetVertexOutdegree(TVertex vertex) => GetVertexOutdegreeCore(m_AdjacencyList, vertex);
+
+    static int GetVertexOutdegreeCore(AssociativeArray<TVertex, AdjacencyRow?> adjacencyList, TVertex vertex) =>
+        adjacencyList.TryGetValue(vertex, out var adjacencyRow) ?
             adjacencyRow?.Count ?? 0 :
             0;
 
