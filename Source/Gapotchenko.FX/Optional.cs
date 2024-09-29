f605dfecf6536f47ab4a72fa03f95d41388b49b5
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2024-09-29 22:07:21 +0200

    Refactoring.

--- Source/Gapotchenko.FX/Optional.cs
+++ Source/Gapotchenko.FX/Optional.cs
@@ -1,4 +1,6 @@
-﻿namespace Gapotchenko.FX;
+﻿using System.Runtime.CompilerServices;
+
+namespace Gapotchenko.FX;
 
 /// <summary>
 /// Supports an optional value represented by <see cref="Optional{T}"/> type.
@@ -20,15 +22,16 @@ public static class Optional
     /// <returns>
     /// An <see cref="Optional{T}"/> object whose <see cref="Optional{T}.Value"/> property is initialized with the value parameter.
     /// </returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Optional<T> Some<T>(T value) => new(value);
 
     /// <summary>
-    /// Returns an <see cref="Optional{T}"/> value that corresponds to the specified <see cref="Nullable{T}"/> value.
+    /// Returns an <see cref="Optional{T}"/> object that corresponds to the specified <see cref="Nullable{T}"/> value.
     /// </summary>
     /// <typeparam name="T">The underlying type of the <see cref="Optional{T}"/> generic type.</typeparam>
     /// <param name="value">The <see cref="Nullable{T}"/> value.</param>
-    /// <returns>An <see cref="Optional{T}"/> value.</returns>
-    public static Optional<T> From<T>(T? value) where T : struct => value.HasValue ? new(value.Value) : default;
+    /// <returns>An <see cref="Optional{T}"/> object.</returns>
+    public static Optional<T> From<T>(T? value) where T : struct => value.HasValue ? Some(value.Value) : default;
 
     /// <summary>
     /// Either creates a new <see cref="Optional{T}"/> object initialized to the specified value,
@@ -40,7 +43,7 @@ public static class Optional
     /// An <see cref="Optional{T}"/> object without a value when the specified parameter equals to the default value of type <typeparamref name="T"/>;
     /// otherwise, an <see cref="Optional{T}"/> object whose <see cref="Optional{T}.Value"/> property is initialized with the <paramref name="value"/> parameter.
     /// </returns>
-    public static Optional<T> Discriminate<T>(T value) => Discriminate(value, default(T)!);
+    public static Optional<T> Discriminate<T>(T value) => Discriminate(value, default(T));
 
     /// <summary>
     /// Either creates a new <see cref="Optional{T}"/> object initialized to the specified value,
@@ -53,7 +56,7 @@ public static class Optional
     /// An <see cref="Optional{T}"/> object without a value when the specified <paramref name="value"/> equals to the <paramref name="noneValue"/> parameter;
     /// otherwise, an <see cref="Optional{T}"/> object whose <see cref="Optional{T}.Value"/> property is initialized with the <paramref name="value"/> parameter.
     /// </returns>
-    public static Optional<T> Discriminate<T>(T value, [AllowNull] T noneValue) =>
+    public static Optional<T> Discriminate<T>(T value, T? noneValue) =>
         EqualityComparer<T>.Default.Equals(value, noneValue) ? default : Some(value);
 
     /// <summary>
@@ -81,10 +84,8 @@ public static class Optional
     /// <typeparam name="T">The underlying type of the <see cref="Optional{T}"/> generic type.</typeparam>
     /// <param name="x">The first optional value to compare.</param>
     /// <param name="y">The second optional value to compare.</param>
-    /// <param name="valueComparer">The value equality comparer.</param>
     /// <returns><see langword="true"/> if the specified objects are equal; otherwise, <see langword="false"/>.</returns>
-    public static bool Equals<T>(Optional<T> x, Optional<T> y, IEqualityComparer<T>? valueComparer) =>
-        OptionalEqualityComparer<T>.EqualsCore(x, y, valueComparer ?? EqualityComparer<T>.Default);
+    public static bool Equals<T>(Optional<T> x, Optional<T> y) => Equals(x, y, null);
 
     /// <summary>
     /// Determines whether the specified optional values are equal.
@@ -92,8 +93,18 @@ public static class Optional
     /// <typeparam name="T">The underlying type of the <see cref="Optional{T}"/> generic type.</typeparam>
     /// <param name="x">The first optional value to compare.</param>
     /// <param name="y">The second optional value to compare.</param>
+    /// <param name="valueComparer">The value equality comparer.</param>
     /// <returns><see langword="true"/> if the specified objects are equal; otherwise, <see langword="false"/>.</returns>
-    public static bool Equals<T>(Optional<T> x, Optional<T> y) => Equals(x, y, null);
+    public static bool Equals<T>(Optional<T> x, Optional<T> y, IEqualityComparer<T>? valueComparer) =>
+        OptionalEqualityComparer<T>.EqualsCore(x, y, valueComparer ?? EqualityComparer<T>.Default);
+
+    /// <summary>
+    /// Returns a hash code for the specified optional value.
+    /// </summary>
+    /// <typeparam name="T">The underlying type of the <see cref="Optional{T}"/> generic type.</typeparam>
+    /// <param name="optional">The optional value.</param>
+    /// <returns>A hash code for the specified optional value.</returns>
+    public static int GetHashCode<T>(Optional<T> optional) => GetHashCode(optional, null);
 
     /// <summary>
     /// Returns a hash code for the specified optional value.
@@ -106,12 +117,13 @@ public static class Optional
         OptionalEqualityComparer<T>.GetHashCodeCore(optional, valueComparer ?? EqualityComparer<T>.Default);
 
     /// <summary>
-    /// Returns a hash code for the specified optional value.
+    /// Compares two optional values.
     /// </summary>
     /// <typeparam name="T">The underlying type of the <see cref="Optional{T}"/> generic type.</typeparam>
-    /// <param name="optional">The optional value.</param>
-    /// <returns>A hash code for the specified optional value.</returns>
-    public static int GetHashCode<T>(Optional<T> optional) => GetHashCode(optional, null);
+    /// <param name="x">The first optional value to compare.</param>
+    /// <param name="y">The second optional value to compare.</param>
+    /// <returns>The comparison result.</returns>
+    public static int Compare<T>(Optional<T> x, Optional<T> y) => Compare(x, y, null);
 
     /// <summary>
     /// Compares two optional values using a specified comparer for values.
@@ -123,15 +135,6 @@ public static class Optional
     /// <returns>The comparison result.</returns>
     public static int Compare<T>(Optional<T> x, Optional<T> y, IComparer<T>? valueComparer) =>
         OptionalComparer<T>.CompareCore(x, y, valueComparer ?? Comparer<T>.Default);
-
-    /// <summary>
-    /// Compares two optional values.
-    /// </summary>
-    /// <typeparam name="T">The underlying type of the <see cref="Optional{T}"/> generic type.</typeparam>
-    /// <param name="x">The first optional value to compare.</param>
-    /// <param name="y">The second optional value to compare.</param>
-    /// <returns>The comparison result.</returns>
-    public static int Compare<T>(Optional<T> x, Optional<T> y) => Compare(x, y, null);
 
     /// <summary>
     /// Creates a new comparer for <see cref="Optional{T}"/> values with a specified comparer for <typeparamref name="T"/> values.
--- Source/Gapotchenko.FX/OptionalExtensions.cs
+++ Source/Gapotchenko.FX/OptionalExtensions.cs
@@ -14,8 +14,14 @@ public static class OptionalExtensions
     /// <returns>A <see cref="Nullable{T}"/> value.</returns>
     public static T? ToNullable<T>(this Optional<T> optional) where T : struct => optional.HasValue ? optional.Value : null;
 
+#if SOURCE_COMPATIBILITY || BINARY_COMPATIBILITY
     /// <inheritdoc cref="ToNullable{T}(Optional{T})"/>
     [Obsolete("Use ToNullable method instead.")]
     [EditorBrowsable(EditorBrowsableState.Never)]
-    public static T? AsNullable<T>(this Optional<T> optional) where T : struct => ToNullable(optional);
+    public static T? AsNullable<T>(
+#if SOURCE_COMPATIBILITY
+        this
+#endif
+        Optional<T> optional) where T : struct => ToNullable(optional);
+#endif
 }
