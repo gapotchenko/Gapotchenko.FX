205e1d6ea39b21c18ef13e585f00861d7f4b6149
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2024-09-29 16:21:39 +0200

    Formalized Optional<T>.ToNullable() extension method.

--- Source/Gapotchenko.FX/OptionalExtensions.cs
+++ Source/Gapotchenko.FX/OptionalExtensions.cs
@@ -10,6 +10,12 @@ public static class OptionalExtensions
     /// Returns <see cref="Nullable{T}"/> that corresponds to the given optional value.
     /// </summary>
     /// <typeparam name="T">The underlying type of the <see cref="Optional{T}"/> value.</typeparam>
+    /// <param name="optional">The optional value.</param>
     /// <returns>A <see cref="Nullable{T}"/> value.</returns>
-    public static T? AsNullable<T>(this Optional<T> optional) where T : struct => optional.HasValue ? optional.Value : null;
+    public static T? ToNullable<T>(this Optional<T> optional) where T : struct => optional.HasValue ? optional.Value : null;
+
+    /// <inheritdoc cref="ToNullable{T}(Optional{T})"/>
+    [Obsolete("Use ToNullable method instead.")]
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static T? AsNullable<T>(this Optional<T> optional) where T : struct => ToNullable(optional);
 }
