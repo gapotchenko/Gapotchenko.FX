68df314dd44ad34e55352eaf09ec43f222b46697
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2024-10-01 15:42:34 +0200

    Refactoring.

--- Source/Modules/Catalog/Gapotchenko.FX.ValueTuple/ValueTupleEqualityComparer`4.cs
+++ Source/Modules/Catalog/Gapotchenko.FX.ValueTuple/ValueTupleEqualityComparer`4.cs
@@ -1,24 +1,12 @@
 ﻿namespace Gapotchenko.FX.ValueTuple;
 
-sealed class ValueTupleEqualityComparer<T1, T2, T3, T4> : IEqualityComparer<ValueTuple<T1, T2, T3, T4>>
+sealed class ValueTupleEqualityComparer<T1, T2, T3, T4>(
+    IEqualityComparer<T1>? comparer1,
+    IEqualityComparer<T2>? comparer2,
+    IEqualityComparer<T3>? comparer3,
+    IEqualityComparer<T4>? comparer4) :
+    IEqualityComparer<ValueTuple<T1, T2, T3, T4>>
 {
-    public ValueTupleEqualityComparer(
-        IEqualityComparer<T1>? comparer1,
-        IEqualityComparer<T2>? comparer2,
-        IEqualityComparer<T3>? comparer3,
-        IEqualityComparer<T4>? comparer4)
-    {
-        m_Comparer1 = comparer1 ?? EqualityComparer<T1>.Default;
-        m_Comparer2 = comparer2 ?? EqualityComparer<T2>.Default;
-        m_Comparer3 = comparer3 ?? EqualityComparer<T3>.Default;
-        m_Comparer4 = comparer4 ?? EqualityComparer<T4>.Default;
-    }
-
-    readonly IEqualityComparer<T1> m_Comparer1;
-    readonly IEqualityComparer<T2> m_Comparer2;
-    readonly IEqualityComparer<T3> m_Comparer3;
-    readonly IEqualityComparer<T4> m_Comparer4;
-
     public bool Equals(ValueTuple<T1, T2, T3, T4> x, ValueTuple<T1, T2, T3, T4> y) =>
         m_Comparer1.Equals(x.Item1, y.Item1) &&
         m_Comparer2.Equals(x.Item2, y.Item2) &&
@@ -31,4 +19,9 @@ sealed class ValueTupleEqualityComparer<T1, T2, T3, T4> : IEqualityComparer<Valu
             Util.GetSafeHashCode(m_Comparer2, obj.Item2),
             Util.GetSafeHashCode(m_Comparer3, obj.Item3),
             Util.GetSafeHashCode(m_Comparer4, obj.Item4));
+
+    readonly IEqualityComparer<T1> m_Comparer1 = comparer1 ?? EqualityComparer<T1>.Default;
+    readonly IEqualityComparer<T2> m_Comparer2 = comparer2 ?? EqualityComparer<T2>.Default;
+    readonly IEqualityComparer<T3> m_Comparer3 = comparer3 ?? EqualityComparer<T3>.Default;
+    readonly IEqualityComparer<T4> m_Comparer4 = comparer4 ?? EqualityComparer<T4>.Default;
 }
