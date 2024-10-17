613fd2c391c7819a66cb612020dfb3a9d8b42aaa
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2024-10-17 23:47:51 +0200

    Gapotchenko.FX.ValueTuple module in progress.

--- Source/Modules/Catalog/Gapotchenko.FX.ValueTuple/ValueTupleEqualityComparer.cs
+++ Source/Modules/Catalog/Gapotchenko.FX.ValueTuple/ValueTupleEqualityComparer.cs
@@ -16,8 +16,8 @@ public static class ValueTupleEqualityComparer
     /// </summary>
     /// <typeparam name="T1">The type of the value tuple's first element.</typeparam>
     /// <param name="comparer1">
-    /// The <see cref="IEqualityComparer{T}"/> implementation to use when comparing the value tuple's first element,
-    /// or <see langword="null"/> to use the default <see cref="IEqualityComparer{T}"/> implementation.
+    /// The <see cref="IEqualityComparer{T}"/> to use when comparing the value tuple's first element,
+    /// or <see langword="null"/> to use the default equality comparer for <typeparamref name="T1"/> type.
     /// </param>
     /// <returns>A new instance of value tuple equality comparer.</returns>
     public static IEqualityComparer<ValueTuple<T1>> Create<T1>(IEqualityComparer<T1>? comparer1) =>
@@ -31,12 +31,12 @@ public static class ValueTupleEqualityComparer
     /// <typeparam name="T1">The type of the value tuple's first element.</typeparam>
     /// <typeparam name="T2">The type of the value tuple's second element.</typeparam>
     /// <param name="comparer1">
-    /// The <see cref="IEqualityComparer{T}"/> implementation to use when comparing the value tuple's first element,
-    /// or <see langword="null"/> to use the default <see cref="IEqualityComparer{T}"/> implementation.
+    /// The <see cref="IEqualityComparer{T}"/> to use when comparing the value tuple's first element,
+    /// or <see langword="null"/> to use the default equality comparer for <typeparamref name="T1"/> type.
     /// </param>
     /// <param name="comparer2">
-    /// The <see cref="IEqualityComparer{T}"/> implementation to use when comparing the value tuple's second element,
-    /// or <see langword="null"/> to use the default <see cref="IEqualityComparer{T}"/> implementation.
+    /// The <see cref="IEqualityComparer{T}"/> to use when comparing the value tuple's second element,
+    /// or <see langword="null"/> to use the default equality comparer for <typeparamref name="T2"/> type.
     /// </param>
     /// <returns>A new instance of value tuple equality comparer.</returns>
     public static IEqualityComparer<ValueTuple<T1, T2>> Create<T1, T2>(IEqualityComparer<T1>? comparer1, IEqualityComparer<T2>? comparer2) =>
@@ -51,16 +51,16 @@ public static class ValueTupleEqualityComparer
     /// <typeparam name="T2">The type of the value tuple's second element.</typeparam>
     /// <typeparam name="T3">The type of the value tuple's third element.</typeparam>
     /// <param name="comparer1">
-    /// The <see cref="IEqualityComparer{T}"/> implementation to use when comparing the value tuple's first element,
-    /// or <see langword="null"/> to use the default <see cref="IEqualityComparer{T}"/> implementation.
+    /// The <see cref="IEqualityComparer{T}"/> to use when comparing the value tuple's first element,
+    /// or <see langword="null"/> to use the default equality comparer for <typeparamref name="T1"/> type.
     /// </param>
     /// <param name="comparer2">
-    /// The <see cref="IEqualityComparer{T}"/> implementation to use when comparing the value tuple's second element,
-    /// or <see langword="null"/> to use the default <see cref="IEqualityComparer{T}"/> implementation.
+    /// The <see cref="IEqualityComparer{T}"/> to use when comparing the value tuple's second element,
+    /// or <see langword="null"/> to use the default equality comparer for <typeparamref name="T2"/> type.
     /// </param>
     /// <param name="comparer3">
-    /// The <see cref="IEqualityComparer{T}"/> implementation to use when comparing the value tuple's third element,
-    /// or <see langword="null"/> to use the default <see cref="IEqualityComparer{T}"/> implementation.
+    /// The <see cref="IEqualityComparer{T}"/> to use when comparing the value tuple's third element,
+    /// or <see langword="null"/> to use the default equality comparer for <typeparamref name="T3"/> type.
     /// </param>
     /// <returns>A new instance of value tuple equality comparer.</returns>
     public static IEqualityComparer<ValueTuple<T1, T2, T3>> Create<T1, T2, T3>(
@@ -79,20 +79,20 @@ public static class ValueTupleEqualityComparer
     /// <typeparam name="T3">The type of the value tuple's third element.</typeparam>
     /// <typeparam name="T4">The type of the value tuple's fourth element.</typeparam>
     /// <param name="comparer1">
-    /// The <see cref="IEqualityComparer{T}"/> implementation to use when comparing the value tuple's first element,
-    /// or <see langword="null"/> to use the default <see cref="IEqualityComparer{T}"/> implementation.
+    /// The <see cref="IEqualityComparer{T}"/> to use when comparing the value tuple's first element,
+    /// or <see langword="null"/> to use the default equality comparer for <typeparamref name="T1"/> type.
     /// </param>
     /// <param name="comparer2">
-    /// The <see cref="IEqualityComparer{T}"/> implementation to use when comparing the value tuple's second element,
-    /// or <see langword="null"/> to use the default <see cref="IEqualityComparer{T}"/> implementation.
+    /// The <see cref="IEqualityComparer{T}"/> to use when comparing the value tuple's second element,
+    /// or <see langword="null"/> to use the default equality comparer for <typeparamref name="T2"/> type.
     /// </param>
     /// <param name="comparer3">
-    /// The <see cref="IEqualityComparer{T}"/> implementation to use when comparing the value tuple's third element,
-    /// or <see langword="null"/> to use the default <see cref="IEqualityComparer{T}"/> implementation.
+    /// The <see cref="IEqualityComparer{T}"/> to use when comparing the value tuple's third element,
+    /// or <see langword="null"/> to use the default equality comparer for <typeparamref name="T3"/> type.
     /// </param>
     /// <param name="comparer4">
-    /// The <see cref="IEqualityComparer{T}"/> implementation to use when comparing the value tuple's fourth element,
-    /// or <see langword="null"/> to use the default <see cref="IEqualityComparer{T}"/> implementation.
+    /// The <see cref="IEqualityComparer{T}"/> to use when comparing the value tuple's fourth element,
+    /// or <see langword="null"/> to use the default equality comparer for <typeparamref name="T4"/> type.
     /// </param>
     /// <returns>A new instance of value tuple equality comparer.</returns>
     public static IEqualityComparer<ValueTuple<T1, T2, T3, T4>> Create<T1, T2, T3, T4>(
