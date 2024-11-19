25c3a62726b00dd134ebb26acfdbc23fb5eb3a81
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2024-11-19 13:36:31 +0100

    Revert "Nulllable-friendly semantics of FileSystemPathStartsWith method."
    This reverts commit 6cc6a57d2af4575d77519fcd760d024d1fc4c838.

--- Source/Modules/Catalog/Gapotchenko.FX.IO/FileSystem.cs
+++ Source/Modules/Catalog/Gapotchenko.FX.IO/FileSystem.cs
@@ -103,19 +103,14 @@ public static class FileSystem
     /// <param name="path">The path.</param>
     /// <param name="value">The value.</param>
     /// <returns><see langword="true"/> when the beginning of the path matches the specified value in terms of file system equivalence; otherwise, <see langword="false"/>.</returns>
-    public static bool PathStartsWith(
-        [NotNullWhen(true)] string? path,
-        string value)
+    public static bool PathStartsWith(string? path, string? value)
     {
-        if (value is null)
-            throw new ArgumentNullException(nameof(value));
-
-        if (path is null)
-            return false;
-
         var pathComparison = PathComparison;
-        if (path.Equals(value, pathComparison))
+
+        if (string.Equals(path, value, pathComparison))
             return true;
+        if (path == null || value == null)
+            return false;
 
         path = PathUtil.Normalize(path, true);
         value = PathUtil.Normalize(value, true);
