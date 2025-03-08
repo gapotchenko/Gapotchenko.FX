8a8b057782ff26bfddebed960956c9e297ccbdfd
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2025-03-08 22:43:35 +0100

    Added Gapotchenko.FX.Data.Compression module.

---
+++ Source/Modules/Catalog/Data/Compression/Gapotchenko.FX.Data.Compression/IDataArchive.cs
@@ -0,0 +1,14 @@
+﻿using Gapotchenko.FX.IO;
+
+namespace Gapotchenko.FX.Data.Compression;
+
+/// <summary>
+/// Defines the interface of a compressible data archive.
+/// </summary>
+public interface IDataArchive : IFileSystemView
+{
+    /// <summary>
+    /// Gets a value indicating whether the current archive supports writing.
+    /// </summary>
+    bool CanWrite { get; }
+}
