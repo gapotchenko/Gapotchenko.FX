27d101e0cbd50965182c88053ac831cbeb440e4e
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2025-04-16 14:27:12 +0200

    Gapotchenko.FX.Data.Archives.Zip: isolated issue with directory lookup.

---
+++ Source/Modules/Tests/Data/Archives/Gapotchenko.FX.Data.Archives.Zip.Tests/Kits/ZipArchiveTestKit.Samples.cs
@@ -0,0 +1,22 @@
+﻿// Gapotchenko.FX
+// Copyright © Gapotchenko and Contributors
+//
+// File introduced by: Oleksiy Gapotchenko
+// Year of introduction: 2025
+
+using Gapotchenko.FX.IO.Vfs;
+
+namespace Gapotchenko.FX.Data.Archives.Zip.Tests.Kits;
+
+partial class ZipArchiveTestKit
+{
+    [TestMethod]
+    public void Zip_Samples_DotnetDev_1_1_4()
+    {
+        using var archive = Mount(Assets.OpenStream("Samples/DotnetDev.1.1.4.zip"));
+
+        Assert.IsTrue(
+            archive.DirectoryExists(archive.JoinPaths("host", "fxr")),
+            "host/fxr directory not found.");
+    }
+}
