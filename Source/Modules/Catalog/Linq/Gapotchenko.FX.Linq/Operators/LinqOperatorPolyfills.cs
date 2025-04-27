3c773cb4f2e26df6118fbcd184cf224ca0a75e05
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2025-04-27 12:04:42 +0200

    LINQ PipeOperator() method is refactored to more concisely named PipeTo() method.

--- Source/Modules/Catalog/Linq/Gapotchenko.FX.Linq/Operators/LinqOperatorPolyfills.cs
+++ Source/Modules/Catalog/Linq/Gapotchenko.FX.Linq/Operators/LinqOperatorPolyfills.cs
@@ -18,8 +18,8 @@ public static class LinqOperatorPolyfills
     /// <param name="func">The function which calculates the pipe result from the specified pipe <paramref name="input"/>.</param>
     /// <returns>A pipe output which is calculated by <paramref name="func"/> from the specified pipe <paramref name="input"/>.</returns>
     /// <exception cref="ArgumentNullException">The <paramref name="func"/> is <see langword="null"/>.</exception>
-    [EditorBrowsable(EditorBrowsableState.Never)]
-    public static TOutput PipeOperator<TInput, TOutput>(this TInput input, Func<TInput, TOutput> func) =>
+    [EditorBrowsable(EditorBrowsableState.Advanced)]
+    public static TOutput PipeTo<TInput, TOutput>(this TInput input, Func<TInput, TOutput> func) =>
         (func ?? throw new ArgumentNullException(nameof(func)))
         (input);
 
@@ -33,8 +33,34 @@ public static class LinqOperatorPolyfills
     /// <param name="input">The pipe input.</param>
     /// <param name="action">The method that receives the specified pipe <paramref name="input"/>.</param>
     /// <exception cref="ArgumentNullException">The <paramref name="action"/> is <see langword="null"/>.</exception>
-    [EditorBrowsable(EditorBrowsableState.Never)]
-    public static void PipeOperator<TInput>(this TInput input, Action<TInput> action) =>
+    [EditorBrowsable(EditorBrowsableState.Advanced)]
+    public static void PipeTo<TInput>(this TInput input, Action<TInput> action) =>
         (action ?? throw new ArgumentNullException(nameof(action)))
         (input);
+
+#if BINARY_COMPATIBILITY || SOURCE_COMPATIBILITY
+
+    /// <inheritdoc cref="PipeTo{TInput, TOutput}(TInput, Func{TInput, TOutput})"/>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    [Obsolete("Use PipeTo method instead.")]
+    public static TOutput PipeOperator<TInput, TOutput>(
+#if SOURCE_COMPATIBILITY
+        this
+#endif
+        TInput input,
+        Func<TInput, TOutput> func) =>
+        PipeTo(input, func);
+
+    /// <inheritdoc cref="PipeTo{TInput}(TInput, Action{TInput})"/>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    [Obsolete("Use PipeTo method instead.")]
+    public static void PipeOperator<TInput>(
+#if SOURCE_COMPATIBILITY
+        this
+#endif
+        TInput input,
+        Action<TInput> action) =>
+        PipeTo(input, action);
+
+#endif
 }
