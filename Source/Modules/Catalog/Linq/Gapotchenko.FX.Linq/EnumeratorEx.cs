95ce505be851709fe8ff0813cb4ed4951c6402e3
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2025-08-29 19:06:44 +0200

    Sort out EnumeratorEx method role separation.

--- Source/Modules/Catalog/Linq/Gapotchenko.FX.Linq/EnumeratorEx.cs
+++ Source/Modules/Catalog/Linq/Gapotchenko.FX.Linq/EnumeratorEx.cs
@@ -1,20 +1,16 @@
-﻿namespace Gapotchenko.FX.Linq;
+﻿// Gapotchenko.FX
+//
+// Copyright © Gapotchenko and Contributors
+//
+// File introduced by: Oleksiy Gapotchenko
+// Year of introduction: 2019
+
+namespace Gapotchenko.FX.Linq;
 
 /// <summary>
 /// Provides an extended set of static methods for querying objects that implement <see cref="IEnumerator{T}"/>.
 /// </summary>
-public static class EnumeratorEx
+[EditorBrowsable(EditorBrowsableState.Never)]
+public static partial class EnumeratorEx
 {
-    /// <summary>
-    /// Returns an enumeration of remaining elements in the <see cref="IEnumerator{T}"/>.
-    /// </summary>
-    /// <typeparam name="T">The type of elements in the enumerator.</typeparam>
-    /// <param name="enumerator">The enumerator.</param>
-    /// <returns>An <see cref="IEnumerable{T}"/> that enumerates the remaining elements in the <see cref="IEnumerator{T}"/>.</returns>
-    public static IEnumerable<T> Rest<T>(this IEnumerator<T> enumerator)
-    {
-        ArgumentNullException.ThrowIfNull(enumerator);
-        while (enumerator.MoveNext())
-            yield return enumerator.Current;
-    }
 }
---
+++ Source/Modules/Catalog/Linq/Gapotchenko.FX.Linq/EnumeratorExtensions.cs
@@ -0,0 +1,28 @@
+﻿// Gapotchenko.FX
+//
+// Copyright © Gapotchenko and Contributors
+//
+// File introduced by: Oleksiy Gapotchenko
+// Year of introduction: 2019
+
+namespace Gapotchenko.FX.Linq;
+
+/// <summary>
+/// Provides extension methods for <see cref="IEnumerator{T}"/>.
+/// </summary>
+[EditorBrowsable(EditorBrowsableState.Never)]
+public static class EnumeratorExtensions
+{
+    /// <summary>
+    /// Returns an enumeration of remaining elements in the <see cref="IEnumerator{T}"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the enumerator.</typeparam>
+    /// <param name="enumerator">The enumerator.</param>
+    /// <returns>An <see cref="IEnumerable{T}"/> that enumerates the remaining elements in the <see cref="IEnumerator{T}"/>.</returns>
+    public static IEnumerable<T> Rest<T>(this IEnumerator<T> enumerator)
+    {
+        ArgumentNullException.ThrowIfNull(enumerator);
+        while (enumerator.MoveNext())
+            yield return enumerator.Current;
+    }
+}
