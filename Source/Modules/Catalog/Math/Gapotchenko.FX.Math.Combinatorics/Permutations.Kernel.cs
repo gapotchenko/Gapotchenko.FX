fdea7a87540f34abcdf94419c166d1888665ed15
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2025-12-25 16:58:34 +0100

    Refactoring.

--- Source/Modules/Catalog/Math/Gapotchenko.FX.Math.Combinatorics/Permutations.Kernel.cs
+++ Source/Modules/Catalog/Math/Gapotchenko.FX.Math.Combinatorics/Permutations.Kernel.cs
@@ -1,4 +1,5 @@
 ﻿// Gapotchenko.FX
+//
 // Copyright © Gapotchenko and Contributors
 //
 // File introduced by: Oleksiy Gapotchenko
@@ -15,20 +16,20 @@ partial class Permutations
     {
         var items = sequence.ReifyList();
 
-        int length = items.Count;
-        var transform = new (int First, int Second)[length];
+        int n = items.Count;
+        var transform = new (int First, int Second)[n];
 
         if (!distinct || Utility.IsCompatibleSet(sequence, comparer))
         {
             // Identity transform.
-            for (int i = 0; i < length; i++)
+            for (int i = 0; i < n; i++)
                 transform[i] = (i, i);
         }
         else
         {
             var map = new AssociativeArray<T, List<int>>(comparer);
 
-            for (int i = 0; i < length; ++i)
+            for (int i = 0; i < n; ++i)
             {
                 var item = items[i];
 
@@ -51,14 +52,14 @@ partial class Permutations
             }
         }
 
-        yield return new ResultRow<T>(items, transform.Select(x => x.Second).ToArray());
+        yield return new ResultRow<T>(items, [.. transform.Select(x => x.Second)]);
 
         for (; ; )
         {
             // Reference: E. W. Dijkstra, A Discipline of Programming, Prentice-Hall, 1997
             // Find the largest partition from the back that is in decreasing (non-increasing) order.
             int decreasingPart;
-            for (decreasingPart = length - 2;
+            for (decreasingPart = n - 2;
                 decreasingPart >= 0 && transform[decreasingPart].First >= transform[decreasingPart + 1].First;
                 --decreasingPart)
             {
@@ -68,22 +69,26 @@ partial class Permutations
             if (decreasingPart < 0)
                 yield break;
 
+            ref var a = ref transform[decreasingPart];
+
             // Find the smallest element in the decreasing partition that is 
             // greater than (or equal to) the item in front of the decreasing partition.
             int greater;
-            for (greater = length - 1;
-                greater > decreasingPart && transform[decreasingPart].First >= transform[greater].First;
-                greater--)
+            for (greater = n - 1;
+                greater > decreasingPart && a.First >= transform[greater].First;
+                --greater)
             {
             }
 
+            ref var b = ref transform[greater];
+
             // Swap the two.
-            MathEx.Swap(ref transform[decreasingPart], ref transform[greater]);
+            (a, b) = (b, a);
 
             // Reverse the decreasing partition.
-            Array.Reverse(transform, decreasingPart + 1, length - decreasingPart - 1);
+            Array.Reverse(transform, decreasingPart + 1, n - decreasingPart - 1);
 
-            yield return new ResultRow<T>(items, transform.Select(x => x.Second).ToArray());
+            yield return new ResultRow<T>(items, [.. transform.Select(x => x.Second)]);
         }
     }
 }
