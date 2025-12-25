6e413eb95c52eb8eb2e1b961a45711b81f03f257
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2025-12-25 18:31:44 +0100

    Refactoring.

---
+++ Source/Modules/Catalog/Math/Gapotchenko.FX.Math/MathEx.Lerp.cs
@@ -0,0 +1,34 @@
+﻿// Gapotchenko.FX
+//
+// Copyright © Gapotchenko and Contributors
+//
+// File introduced by: Oleksiy Gapotchenko
+// Year of introduction: 2020
+
+using System.Runtime.CompilerServices;
+
+namespace Gapotchenko.FX.Math;
+
+partial class MathEx
+{
+    /// <summary>
+    /// Returns a linear interpolation between specified <paramref name="a"/> and <paramref name="b"/> values by a given <paramref name="t" /> coefficient belonging to the interval <c>[0,1]</c>.
+    /// </summary>
+    /// <remarks>
+    /// When <paramref name="t"/> is <c>0</c>, the value of <paramref name="a"/> is returned.
+    /// When <paramref name="t"/> is <c>1</c>, the value of <paramref name="b"/> is returned.
+    /// When <paramref name="t"/> is <c>0.5</c>, the midpoint of <paramref name="a"/> and <paramref name="b"/> is returned.
+    /// </remarks>
+    /// <param name="a">The start value.</param>
+    /// <param name="b">The end value.</param>
+    /// <param name="t">The interpolation coefficient between the start and end values.</param>
+    /// <returns>
+    /// The linear interpolation between <paramref name="a"/> and <paramref name="b"/> by <paramref name="t"/>.
+    /// </returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static double Lerp(double a, double b, double t) => a + (b - a) * t;
+
+    /// <inheritdoc cref="Lerp(double, double, double)"/>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static float Lerp(float a, float b, float t) => a + (b - a) * t;
+}
