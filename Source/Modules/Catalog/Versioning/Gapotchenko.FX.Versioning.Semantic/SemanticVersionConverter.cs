2872e8b652ec2d2346fe6e1c9a789c01fc923e85
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2025-12-26 01:08:58 +0100

    Improved SemanticVersionConverter.

--- Source/Modules/Catalog/Versioning/Gapotchenko.FX.Versioning.Semantic/SemanticVersionConverter.cs
+++ Source/Modules/Catalog/Versioning/Gapotchenko.FX.Versioning.Semantic/SemanticVersionConverter.cs
@@ -5,52 +5,79 @@
 // File introduced by: Oleksiy Gapotchenko
 // Year of introduction: 2025
 
+using System.ComponentModel.Design.Serialization;
+using System.Diagnostics;
 using System.Globalization;
+using System.Reflection;
 
 namespace Gapotchenko.FX.Versioning;
 
 /// <summary>
 /// Provides a type converter to convert <see cref="SemanticVersion"/> objects to and from other representations.
 /// </summary>
-public sealed class SemanticVersionConverter : TypeConverter
+public class SemanticVersionConverter : TypeConverter
 {
     /// <inheritdoc/>
     public override bool CanConvertFrom(ITypeDescriptorContext? context, Type sourceType)
     {
-        if (sourceType == typeof(string))
-            return true;
-        else
-            return base.CanConvertFrom(context, sourceType);
+        return
+            sourceType == typeof(string) ||
+            sourceType == typeof(Version) ||
+            sourceType == typeof(SemanticVersion) ||
+            base.CanConvertFrom(context, sourceType);
     }
 
     /// <inheritdoc/>
     public override object? ConvertFrom(ITypeDescriptorContext? context, CultureInfo? culture, object value)
     {
-        if (value is string s)
-            return SemanticVersion.Parse(s);
-        else
-            return base.ConvertFrom(context, culture, value);
+        return value switch
+        {
+            string s => SemanticVersion.Parse(s),
+            SemanticVersion semanticVersion => semanticVersion with { },
+            Version version => new SemanticVersion(version),
+            _ => base.ConvertFrom(context, culture, value)
+        };
     }
 
     /// <inheritdoc/>
     public override bool CanConvertTo(ITypeDescriptorContext? context, Type? destinationType)
     {
-        if (destinationType == typeof(string))
-            return true;
-        else if (destinationType == typeof(Version))
-            return true;
-        else
-            return base.CanConvertTo(context, destinationType);
+        return
+            destinationType == typeof(Version) ||
+            destinationType == typeof(SemanticVersion) ||
+            destinationType == typeof(InstanceDescriptor) ||
+            base.CanConvertTo(context, destinationType);
     }
 
     /// <inheritdoc/>
     public override object? ConvertTo(ITypeDescriptorContext? context, CultureInfo? culture, object? value, Type destinationType)
     {
-        if (destinationType == typeof(string))
-            return ((SemanticVersion?)value)?.ToString();
-        else if (destinationType == typeof(Version))
-            return (Version?)value;
-        else
-            return base.ConvertTo(context, culture, value, destinationType);
+        if (value is SemanticVersion semanticVersion)
+        {
+            if (destinationType == typeof(string))
+                return semanticVersion.ToString();
+
+            if (destinationType == typeof(Version))
+                return (Version)semanticVersion;
+
+            if (destinationType == typeof(SemanticVersion))
+                return semanticVersion with { };
+
+            if (destinationType == typeof(InstanceDescriptor))
+            {
+                var ctor = typeof(SemanticVersion).GetConstructor(
+                    BindingFlags.Public | BindingFlags.Instance,
+                    null,
+                    [typeof(int), typeof(int), typeof(int), typeof(string), typeof(string)],
+                    null);
+                Debug.Assert(ctor != null);
+
+                return new InstanceDescriptor(
+                    ctor,
+                    new object?[] { semanticVersion.Major, semanticVersion.Minor, semanticVersion.Patch, semanticVersion.Prerelease, semanticVersion.Build });
+            }
+        }
+
+        return base.ConvertTo(context, culture, value, destinationType);
     }
 }
