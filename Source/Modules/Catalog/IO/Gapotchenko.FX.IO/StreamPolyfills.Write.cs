5af43ad0e0f814868903aca11faea8a4943ce848
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2026-01-05 00:46:36 +0100

    Added System.IO.Stream.WriteAsync(ReadOnlyMemory<byte>, CancellationToken) polyfill method.

--- Source/Modules/Catalog/IO/Gapotchenko.FX.IO/StreamPolyfills.Write.cs
+++ Source/Modules/Catalog/IO/Gapotchenko.FX.IO/StreamPolyfills.Write.cs
@@ -7,6 +7,7 @@
 // Year of introduction: 2026
 
 using System.Buffers;
+using System.Runtime.InteropServices;
 
 namespace Gapotchenko.FX.IO;
 
@@ -20,14 +21,11 @@ partial class StreamPolyfills
     /// This is a polyfill provided by Gapotchenko.FX.
     /// </remarks>
     /// <param name="stream">The stream to write the bytes to.</param>
-    /// <param name="buffer">
-    /// A region of memory.
-    /// This method writes the contents of this region to the current stream.
-    /// </param>
+    /// <param name="buffer">The region of memory to write data from.</param>
 #if TFF_STREAM_SPAN
     [EditorBrowsable(EditorBrowsableState.Never)]
 #else
-    // TODO: method use should be discouraged because the polyfill implementation incurs memory allocations and copying.
+    // TODO: method use should be discouraged because the polyfill implementation incurs memory copying.
 #endif
     public static void Write(
 #if !TFF_STREAM_SPAN
@@ -53,6 +51,60 @@ partial class StreamPolyfills
         finally
         {
             pool.Return(array);
+        }
+#endif
+    }
+
+    /// <summary>
+    /// Asynchronously writes a sequence of bytes to the current stream,
+    /// advances the current position within this stream by the number of bytes written,
+    /// and monitors cancellation requests.
+    /// </summary>
+    /// <param name="stream">The stream to write the bytes to.</param>
+    /// <param name="buffer">The region of memory to write data from.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task that represents the asynchronous write operation.</returns>
+#if TFF_STREAM_SPAN
+    [EditorBrowsable(EditorBrowsableState.Never)]
+#else
+    // TODO: method use should be discouraged because the polyfill implementation incurs memory copying.
+#endif
+    public static Task WriteAsync(
+#if !TFF_STREAM_SPAN
+        this
+#endif
+        Stream stream,
+        ReadOnlyMemory<byte> buffer,
+        CancellationToken cancellationToken = default)
+    {
+        ArgumentNullException.ThrowIfNull(stream);
+
+#if TFF_STREAM_SPAN
+        return stream.WriteAsync(buffer, cancellationToken).AsTask();
+#else
+        if (MemoryMarshal.TryGetArray(buffer, out ArraySegment<byte> array))
+        {
+            return stream.WriteAsync(array.Array!, array.Offset, array.Count, cancellationToken);
+        }
+        else
+        {
+            return CoreImpl(stream, buffer, cancellationToken);
+        }
+
+        static async Task CoreImpl(Stream stream, ReadOnlyMemory<byte> buffer, CancellationToken cancellationToken)
+        {
+            int count = buffer.Length;
+            var pool = ArrayPool<byte>.Shared;
+            byte[] sharedBuffer = pool.Rent(count);
+            try
+            {
+                buffer.Span.CopyTo(sharedBuffer);
+                await stream.WriteAsync(sharedBuffer, 0, count, cancellationToken).ConfigureAwait(false);
+            }
+            finally
+            {
+                pool.Return(sharedBuffer);
+            }
         }
 #endif
     }
