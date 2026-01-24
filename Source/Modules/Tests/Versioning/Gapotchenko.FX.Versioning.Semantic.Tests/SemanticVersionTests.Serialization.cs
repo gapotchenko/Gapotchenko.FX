d2bb631e0fc15c27b2783336a29c033aecf5b471
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2026-01-24 14:57:40 +0100

    Added SemanticVersion JSON serialization tests.

--- Source/Modules/Tests/Versioning/Gapotchenko.FX.Versioning.Semantic.Tests/SemanticVersionTests.Serialization.cs
+++ Source/Modules/Tests/Versioning/Gapotchenko.FX.Versioning.Semantic.Tests/SemanticVersionTests.Serialization.cs
@@ -5,9 +5,25 @@
 // File introduced by: Oleksiy Gapotchenko
 // Year of introduction: 2025
 
+#if TFF_JSON
+using System.Text.Json;
+#endif
+
 namespace Gapotchenko.FX.Versioning.Semantic.Tests;
 
 partial class SemanticVersionTests
 {
-    // TODO
+#if TFF_JSON
+
+    [TestMethod]
+    [DataRow("\"1.2.3\"", "1.2.3")]
+    [DataRow("\"1.2.3-label\"", "1.2.3-label")]
+    [DataRow("null", null)]
+    public void SemanticVersion_Serialization_Json(string json, string? version)
+    {
+        var obj = JsonSerializer.Deserialize<SemanticVersion>(json);
+        Assert.AreEqual(SemanticVersion.Parse(version), obj);
+    }
+
+#endif
 }
