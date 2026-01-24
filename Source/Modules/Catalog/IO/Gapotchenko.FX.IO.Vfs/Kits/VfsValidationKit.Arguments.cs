44a5795b06e0f451e55c53b049c40cca52e4d4d3
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2026-01-24 23:18:14 +0100

    Refactoring.

--- Source/Modules/Catalog/IO/Gapotchenko.FX.IO.Vfs/Kits/VfsValidationKit.Arguments.cs
+++ Source/Modules/Catalog/IO/Gapotchenko.FX.IO.Vfs/Kits/VfsValidationKit.Arguments.cs
@@ -20,152 +20,152 @@ partial class VfsValidationKit
     public static class Arguments
     {
         /// <summary>
-        /// Validates the specified path argument value.
+        /// Validates the specified path argument.
         /// </summary>
         /// <param name="path">The path.</param>
-        /// <param name="argumentName">The argument name.</param>
+        /// <param name="parameterName">The parameter name.</param>
         /// <exception cref="ArgumentNullException"><paramref name="path"/> is <see langword="null"/>.</exception>
         /// <exception cref="ArgumentException">The <paramref name="path"/> is empty.</exception>
         public static void ValidatePath(
             [NotNull] string? path,
-            [CallerArgumentExpression(nameof(path))] string? argumentName = null)
+            [CallerArgumentExpression(nameof(path))] string? parameterName = null)
         {
-            ArgumentNullException.ThrowIfNull(path, argumentName);
+            ArgumentNullException.ThrowIfNull(path, parameterName);
             if (path.Length == 0)
-                throw new ArgumentException(Resources.PathIsEmpty, argumentName);
+                throw new ArgumentException(Resources.PathIsEmpty, parameterName);
         }
 
         /// <summary>
-        /// Validates the specified search pattern argument value.
+        /// Validates the specified search pattern argument.
         /// </summary>
         /// <param name="searchPattern">The search pattern.</param>
-        /// <param name="argumentName">The argument name.</param>
+        /// <param name="parameterName">The parameter name.</param>
         /// <exception cref="ArgumentNullException"><paramref name="searchPattern"/> is <see langword="null"/>.</exception>
         public static void ValidateSearchPattern(
             [NotNull] string? searchPattern,
-            [CallerArgumentExpression(nameof(searchPattern))] string? argumentName = null)
+            [CallerArgumentExpression(nameof(searchPattern))] string? parameterName = null)
         {
-            ArgumentNullException.ThrowIfNull(searchPattern, argumentName);
+            ArgumentNullException.ThrowIfNull(searchPattern, parameterName);
         }
 
         /// <summary>
-        /// Validates the specified <see cref="SearchOption"/> argument value.
+        /// Validates the specified <see cref="SearchOption"/> argument.
         /// </summary>
         /// <param name="searchOption">The <see cref="SearchOption"/> value.</param>
-        /// <param name="argumentName">The argument name.</param>
+        /// <param name="parameterName">The parameter name.</param>
         /// <exception cref="ArgumentOutOfRangeException"><paramref name="searchOption"/> value is out of legal range.</exception>
         public static void ValidateSearchOption(
             SearchOption searchOption,
-            [CallerArgumentExpression(nameof(searchOption))] string? argumentName = null)
+            [CallerArgumentExpression(nameof(searchOption))] string? parameterName = null)
         {
             if (searchOption is not (SearchOption.TopDirectoryOnly or SearchOption.AllDirectories))
-                throw new ArgumentOutOfRangeException(argumentName, Resources.EnumValueIsOutOfLegalRange);
+                throw new ArgumentOutOfRangeException(parameterName, Resources.EnumValueIsOutOfLegalRange);
         }
 
         /// <summary>
-        /// Validates the specified <see cref="EnumerationOptions"/> argument value.
+        /// Validates the specified <see cref="EnumerationOptions"/> argument.
         /// </summary>
         /// <param name="enumerationOptions">The <see cref="EnumerationOptions"/> value.</param>
-        /// <param name="argumentName">The argument name.</param>
+        /// <param name="parameterName">The parameter name.</param>
         /// <exception cref="ArgumentOutOfRangeException"><paramref name="enumerationOptions"/> value is out of legal range.</exception>
         public static void ValidateEnumerationOptions(
             EnumerationOptions enumerationOptions,
-            [CallerArgumentExpression(nameof(enumerationOptions))] string? argumentName = null)
+            [CallerArgumentExpression(nameof(enumerationOptions))] string? parameterName = null)
         {
-            ArgumentNullException.ThrowIfNull(enumerationOptions, argumentName);
+            ArgumentNullException.ThrowIfNull(enumerationOptions, parameterName);
         }
 
         /// <summary>
-        /// Validates the specified <see cref="FileMode"/> and <see cref="FileAccess"/> argument values.
+        /// Validates the specified <see cref="FileMode"/> and <see cref="FileAccess"/> argument.
         /// </summary>
         /// <param name="mode">The <see cref="FileMode"/> value.</param>
         /// <param name="access">The <see cref="FileAccess"/> value.</param>
-        /// <param name="modeArgumentName">The <see cref="FileMode"/> argument name.</param>
-        /// <param name="accessArgumentName">The <see cref="FileAccess"/> argument name.</param>
+        /// <param name="modeParameterName">The <see cref="FileMode"/> parameter name.</param>
+        /// <param name="accessParameterName">The <see cref="FileAccess"/> parameter name.</param>
         /// <exception cref="ArgumentOutOfRangeException"><paramref name="mode"/> value is out of legal range.</exception>
         /// <exception cref="ArgumentOutOfRangeException"><paramref name="access"/> value is out of legal range.</exception>
         public static void ValidateFileModeAndAccess(
             FileMode mode,
             FileAccess access,
-            [CallerArgumentExpression(nameof(mode))] string? modeArgumentName = null,
-            [CallerArgumentExpression(nameof(access))] string? accessArgumentName = null)
+            [CallerArgumentExpression(nameof(mode))] string? modeParameterName = null,
+            [CallerArgumentExpression(nameof(access))] string? accessParameterName = null)
         {
-            ValidateFileMode(mode, modeArgumentName);
-            ValidateFileAccess(access, accessArgumentName);
+            ValidateFileMode(mode, modeParameterName);
+            ValidateFileAccess(access, accessParameterName);
 
             // TODO: validate combinations
         }
 
         /// <summary>
-        /// Validates the specified <see cref="FileMode"/> argument value.
+        /// Validates the specified <see cref="FileMode"/> argument.
         /// </summary>
         /// <param name="mode">The <see cref="FileMode"/> value.</param>
-        /// <param name="argumentName">The argument name.</param>
+        /// <param name="parameterName">The parameter name.</param>
         /// <exception cref="ArgumentOutOfRangeException"><paramref name="mode"/> value is out of legal range.</exception>
         public static void ValidateFileMode(
             FileMode mode,
-            [CallerArgumentExpression(nameof(mode))] string? argumentName = null)
+            [CallerArgumentExpression(nameof(mode))] string? parameterName = null)
         {
             if (mode is not (FileMode.Append or FileMode.Create or FileMode.CreateNew or FileMode.Open or FileMode.OpenOrCreate or FileMode.Truncate))
-                throw new ArgumentOutOfRangeException(argumentName, Resources.EnumValueIsOutOfLegalRange);
+                throw new ArgumentOutOfRangeException(parameterName, Resources.EnumValueIsOutOfLegalRange);
         }
 
         /// <summary>
-        /// Validates the specified <see cref="FileAccess"/> argument value.
+        /// Validates the specified <see cref="FileAccess"/> argument.
         /// </summary>
         /// <param name="access">The <see cref="FileAccess"/> value.</param>
-        /// <param name="argumentName">The argument name.</param>
+        /// <param name="parameterName">The parameter name.</param>
         /// <exception cref="ArgumentOutOfRangeException"><paramref name="access"/> value is out of legal range.</exception>
         public static void ValidateFileAccess(
             FileAccess access,
-            [CallerArgumentExpression(nameof(access))] string? argumentName = null)
+            [CallerArgumentExpression(nameof(access))] string? parameterName = null)
         {
             if (access == 0 || (access & ~FileAccess.ReadWrite) != 0)
-                throw new ArgumentOutOfRangeException(argumentName, Resources.EnumValueIsOutOfLegalRange);
+                throw new ArgumentOutOfRangeException(parameterName, Resources.EnumValueIsOutOfLegalRange);
         }
 
         /// <summary>
-        /// Validates the specified <see cref="FileShare"/> argument value.
+        /// Validates the specified <see cref="FileShare"/> argument.
         /// </summary>
         /// <param name="share">The <see cref="FileShare"/> value.</param>
-        /// <param name="argumentName">The argument name.</param>
+        /// <param name="parameterName">The parameter name.</param>
         /// <exception cref="ArgumentOutOfRangeException"><paramref name="share"/> value is out of legal range.</exception>
         public static void ValidateFileShare(
             FileShare share,
-            [CallerArgumentExpression(nameof(share))] string? argumentName = null)
+            [CallerArgumentExpression(nameof(share))] string? parameterName = null)
         {
             if ((share & ~(FileShare.ReadWrite | FileShare.Delete | FileShare.Inheritable)) != 0)
-                throw new ArgumentOutOfRangeException(argumentName, Resources.EnumValueIsOutOfLegalRange);
+                throw new ArgumentOutOfRangeException(parameterName, Resources.EnumValueIsOutOfLegalRange);
         }
 
         /// <summary>
-        /// Validates the specified <see cref="VfsCopyOptions"/> argument value.
+        /// Validates the specified <see cref="VfsCopyOptions"/> argument.
         /// </summary>
         /// <param name="options">The <see cref="VfsCopyOptions"/> value.</param>
-        /// <param name="argumentName">The argument name.</param>
+        /// <param name="parameterName">The parameter name.</param>
         /// <exception cref="ArgumentOutOfRangeException"><paramref name="options"/> value is out of legal range.</exception>
         public static void ValidateCopyOptions(
             VfsCopyOptions options,
-            [CallerArgumentExpression(nameof(options))] string? argumentName = null)
+            [CallerArgumentExpression(nameof(options))] string? parameterName = null)
         {
             const VfsCopyOptions knownOptions = VfsCopyOptions.Archive;
             if ((options & ~knownOptions) != 0)
-                throw new ArgumentOutOfRangeException(argumentName, Resources.EnumValueIsOutOfLegalRange);
+                throw new ArgumentOutOfRangeException(parameterName, Resources.EnumValueIsOutOfLegalRange);
         }
 
         /// <summary>
-        /// Validates the specified <see cref="VfsMoveOptions"/> argument value.
+        /// Validates the specified <see cref="VfsMoveOptions"/> argument.
         /// </summary>
         /// <param name="options">The <see cref="VfsMoveOptions"/> value.</param>
-        /// <param name="argumentName">The argument name.</param>
+        /// <param name="parameterName">The parameter name.</param>
         /// <exception cref="ArgumentOutOfRangeException"><paramref name="options"/> value is out of legal range.</exception>
         public static void ValidateMoveOptions(
             VfsMoveOptions options,
-            [CallerArgumentExpression(nameof(options))] string? argumentName = null)
+            [CallerArgumentExpression(nameof(options))] string? parameterName = null)
         {
             const VfsMoveOptions knownOptions = VfsMoveOptions.None;
             if ((options & ~knownOptions) != 0)
-                throw new ArgumentOutOfRangeException(argumentName, Resources.EnumValueIsOutOfLegalRange);
+                throw new ArgumentOutOfRangeException(parameterName, Resources.EnumValueIsOutOfLegalRange);
         }
     }
 }
