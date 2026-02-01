fe7a50f40f610266bf596b3abc7801bf76ec77e8
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2026-02-02 00:29:16 +0100

    Improved documentation.

--- Source/Modules/Catalog/Gapotchenko.FX.Collections/Generic/ListExtensions.BinarySearch.cs
+++ Source/Modules/Catalog/Gapotchenko.FX.Collections/Generic/ListExtensions.BinarySearch.cs
@@ -1,6 +1,7 @@
 ﻿// Gapotchenko.FX
 //
 // Copyright © Gapotchenko and Contributors
+// Portions © .NET Foundation and its Licensors
 //
 // File introduced by: Oleksiy Gapotchenko
 // Year of introduction: 2026
@@ -12,73 +13,88 @@ namespace Gapotchenko.FX.Collections.Generic;
 partial class ListExtensions
 {
     /// <summary>
-    /// Searches the entire sorted <see cref="IReadOnlyList{T}"/> for an element using the specified value selector and comparer.
+    /// Searches the entire sorted <see cref="IReadOnlyList{T}"/> for an element using the specified key selector and comparer.
     /// </summary>
     /// <returns>
-    /// The zero-based index of a matching element, or a negative number equal to the bitwise complement of the insertion index if no element is found.
+    /// <para>
+    /// The index of a matching element in the specified <paramref name="list"/>, if element is found; otherwise, a negative number.
+    /// </para>
+    /// <para>
+    /// If element is not found and <paramref name="value"/> is less than at least one element key in <paramref name="list"/>,
+    /// the negative number returned is the bitwise complement of the index of the first element whose key is larger than <paramref name="value"/>.
+    /// </para>
+    /// <para>
+    /// If element is not found and <paramref name="value"/> is greater than all element keys in <paramref name="list"/>,
+    /// the negative number returned is the bitwise complement of (the index of the last element plus 1).
+    /// </para>
+    /// <para>
+    /// If this method is called with a non-sorted <paramref name="list"/>,
+    /// the return value can be incorrect and a negative number could be returned,
+    /// even if matching element is present in <paramref name="list"/>.
+    /// </para>
     /// </returns>
     /// <param name="list">The list of elements.</param>
     /// <param name="value">The element value to find.</param>
-    /// <param name="valueSelector">The value selector which selects a value from a list element.</param>
+    /// <param name="keySelector">The key selector.</param>
     /// <param name="comparer">The comparer, or <see langword="null"/> to use the default <typeparamref name="TValue"/> comparer.</param>
     /// <exception cref="ArgumentNullException"><paramref name="list"/> is <see langword="null"/>.</exception>
-    /// <exception cref="ArgumentNullException"><paramref name="valueSelector"/> is <see langword="null"/>.</exception>
+    /// <exception cref="ArgumentNullException"><paramref name="keySelector"/> is <see langword="null"/>.</exception>
     /// <typeparam name="TElement">The type of list elements.</typeparam>
     /// <typeparam name="TValue">The type of a value to find.</typeparam>
     public static int BinarySearchBy<TElement, TValue>(
         this IReadOnlyList<TElement> list,
         TValue value,
-        Func<TElement, TValue> valueSelector,
+        Func<TElement, TValue> keySelector,
         IComparer<TValue>? comparer = null)
     {
         ArgumentNullException.ThrowIfNull(list);
-        ArgumentNullException.ThrowIfNull(valueSelector);
+        ArgumentNullException.ThrowIfNull(keySelector);
 
-        return BinarySearchByCore(list, 0, list.Count, value, valueSelector, comparer);
+        return BinarySearchByCore(list, 0, list.Count, value, keySelector, comparer);
     }
 
     /// <summary>
-    /// Searches the entire sorted <see cref="IList{T}"/> for an element using the specified value selector and comparer.
+    /// Searches the entire sorted <see cref="IList{T}"/> for an element using the specified key selector and comparer.
     /// </summary>
     /// <inheritdoc cref="BinarySearchBy{TElement, TValue}(IReadOnlyList{TElement}, TValue, Func{TElement, TValue}, IComparer{TValue}?)"/>
     [OverloadResolutionPriority(-1)]
     public static int BinarySearchBy<TElement, TValue>(
         this IList<TElement> list,
         TValue value,
-        Func<TElement, TValue> valueSelector,
+        Func<TElement, TValue> keySelector,
         IComparer<TValue>? comparer = null)
     {
         ArgumentNullException.ThrowIfNull(list);
-        ArgumentNullException.ThrowIfNull(valueSelector);
+        ArgumentNullException.ThrowIfNull(keySelector);
 
-        return BinarySearchByCore(list, 0, list.Count, value, valueSelector, comparer);
+        return BinarySearchByCore(list, 0, list.Count, value, keySelector, comparer);
     }
 
     /// <summary>
-    /// Searches a range of elements in the sorted <see cref="IReadOnlyList{T}"/> for an element using the specified value selector and comparer.
+    /// Searches a range of elements in the sorted <see cref="IReadOnlyList{T}"/> for an element using the specified key selector and comparer.
     /// </summary>
     /// <inheritdoc cref="BinarySearchByCore{TElement, TValue}(IReadOnlyList{TElement}, int, int, TValue, Func{TElement, TValue}, IComparer{TValue}?)"/>
     /// <param name="list"><inheritdoc/></param>
     /// <param name="range">The range to search.</param>
     /// <param name="value"><inheritdoc/>The range to search</param>
-    /// <param name="valueSelector"><inheritdoc/></param>
+    /// <param name="keySelector"><inheritdoc/></param>
     /// <param name="comparer"><inheritdoc/></param>
     public static int BinarySearchBy<TElement, TValue>(
         this IReadOnlyList<TElement> list,
         Range range,
         TValue value,
-        Func<TElement, TValue> valueSelector,
+        Func<TElement, TValue> keySelector,
         IComparer<TValue>? comparer = null)
     {
         ArgumentNullException.ThrowIfNull(list);
-        ArgumentNullException.ThrowIfNull(valueSelector);
+        ArgumentNullException.ThrowIfNull(keySelector);
 
         var (offset, count) = range.GetOffsetAndLength(list.Count);
-        return BinarySearchByCore(list, offset, offset + count, value, valueSelector, comparer);
+        return BinarySearchByCore(list, offset, offset + count, value, keySelector, comparer);
     }
 
     /// <summary>
-    /// Searches a range of elements in the sorted <see cref="IList{T}"/> for an element using the specified value selector and comparer.
+    /// Searches a range of elements in the sorted <see cref="IList{T}"/> for an element using the specified key selector and comparer.
     /// </summary>
     /// <inheritdoc cref="BinarySearchBy{TElement, TValue}(IReadOnlyList{TElement}, Range, TValue, Func{TElement, TValue}, IComparer{TValue}?)"/>
     [OverloadResolutionPriority(-1)]
@@ -86,14 +102,14 @@ partial class ListExtensions
         this IList<TElement> list,
         Range range,
         TValue value,
-        Func<TElement, TValue> valueSelector,
+        Func<TElement, TValue> keySelector,
         IComparer<TValue>? comparer = null)
     {
         ArgumentNullException.ThrowIfNull(list);
-        ArgumentNullException.ThrowIfNull(valueSelector);
+        ArgumentNullException.ThrowIfNull(keySelector);
 
         var (offset, count) = range.GetOffsetAndLength(list.Count);
-        return BinarySearchByCore(list, offset, offset + count, value, valueSelector, comparer);
+        return BinarySearchByCore(list, offset, offset + count, value, keySelector, comparer);
     }
 
     // ------------------------------------------------------------------------
@@ -103,7 +119,7 @@ partial class ListExtensions
         int start, // the inclusive start index
         int end, // the exclusive end index
         TValue value,
-        Func<TElement, TValue> valueSelector,
+        Func<TElement, TValue> keySelector,
         IComparer<TValue>? comparer)
     {
         comparer ??= Comparer<TValue>.Default;
@@ -113,17 +129,17 @@ partial class ListExtensions
 
         while (lo <= hi)
         {
-            int mid = GetMid(lo, hi);
+            int i = GetMedian(lo, hi);
 
-            var midValue = valueSelector(list[mid]);
+            var key = keySelector(list[i]);
+            int c = comparer.Compare(key, value);
 
-            int cmp = comparer.Compare(midValue, value);
-            if (cmp == 0)
-                return mid;
-            else if (cmp < 0)
-                lo = mid + 1;
+            if (c == 0)
+                return i;
+            else if (c < 0)
+                lo = i + 1;
             else
-                hi = mid - 1;
+                hi = i - 1;
         }
 
         // Not found; lo is the insertion index
@@ -135,7 +151,7 @@ partial class ListExtensions
         int start, // the inclusive start index
         int end, // the exclusive end index
         TValue value,
-        Func<TElement, TValue> valueSelector,
+        Func<TElement, TValue> keySelector,
         IComparer<TValue>? comparer)
     {
         comparer ??= Comparer<TValue>.Default;
@@ -145,24 +161,24 @@ partial class ListExtensions
 
         while (lo <= hi)
         {
-            int mid = GetMid(lo, hi);
+            int i = GetMedian(lo, hi);
 
-            var midValue = valueSelector(list[mid]);
+            var key = keySelector(list[i]);
+            int c = comparer.Compare(key, value);
 
-            int cmp = comparer.Compare(midValue, value);
-            if (cmp == 0)
-                return mid;
-            else if (cmp < 0)
-                lo = mid + 1;
+            if (c == 0)
+                return i;
+            else if (c < 0)
+                lo = i + 1;
             else
-                hi = mid - 1;
+                hi = i - 1;
         }
 
         // Not found; lo is the insertion index
         return ~lo;
     }
 
-    static int GetMid(int lo, int hi) =>
+    static int GetMedian(int lo, int hi) =>
         // Avoid overflow: same technique used in BCL implementations
         lo + ((hi - lo) >> 1);
 }
