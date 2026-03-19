3b439d45b20a68504498eb3a664d61fe41e13353
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2026-03-19 03:04:22 +0100

    MS-CFB file system module is in progress. Extended ttests using sample CFB images.

---
+++ Source/Modules/Catalog/IO/FileSystems/MSCfb/Gapotchenko.FX.IO.FileSystems.MSCfb.Tests/MSCfbSampleTests.cs
@@ -0,0 +1,97 @@
+// Gapotchenko.FX
+//
+// Copyright © Gapotchenko and Contributors
+// Portions © OpenMcdf Project
+//
+// File introduced by: Oleksiy Gapotchenko
+// Year of introduction: 2026
+
+namespace Gapotchenko.FX.IO.FileSystems.MSCfb.Tests;
+
+[TestClass]
+public sealed class MSCfbSampleTests
+{
+    #region Stream
+
+    [TestMethod]
+    [DataRow("TestStream_v3_0.cfb", 0)]
+    [DataRow("TestStream_v3_63.cfb", 63)]
+    [DataRow("TestStream_v3_64.cfb", 64)]
+    [DataRow("TestStream_v3_65.cfb", 65)]
+    [DataRow("TestStream_v3_511.cfb", 511)]
+    [DataRow("TestStream_v3_512.cfb", 512)]
+    [DataRow("TestStream_v3_513.cfb", 513)]
+    [DataRow("TestStream_v3_4095.cfb", 4095)]
+    [DataRow("TestStream_v3_4096.cfb", 4096)]
+    [DataRow("TestStream_v3_4097.cfb", 4097)]
+    public void MSCfb_Sample_Stream_Read(string fileName, int expectedLength)
+    {
+        using var assetStream = Assets.OpenStream(fileName);
+        using var vfs = new MSCfbFileSystem(assetStream);
+
+        Assert.AreEqual(expectedLength, vfs.GetFileSize("/TestStream"));
+
+        using var stream = vfs.OpenFile("/TestStream", FileMode.Open, FileAccess.Read, FileShare.Read);
+        Assert.AreEqual(expectedLength, stream.Length);
+
+        // Test files are filled with bytes equal to their position modulo 256.
+        for (int i = 0; i < expectedLength; i++)
+        {
+            int value = stream.ReadByte();
+            Assert.AreNotEqual(-1, value, $"Unexpected end of stream at position {i}.");
+            Assert.AreEqual((byte)i, (byte)value, $"Unexpected byte value at position {i}.");
+        }
+
+        Assert.AreEqual(-1, stream.ReadByte(), "Expected end of stream.");
+    }
+
+    #endregion
+
+    #region Storage
+
+    [TestMethod]
+    [DataRow("MultipleStorage.cfb", 1)]
+    [DataRow("MultipleStorage2.cfb", 1)]
+    [DataRow("MultipleStorage3.cfb", 1)]
+    [DataRow("MultipleStorage4.cfb", 1)]
+    public void MSCfb_Sample_Storage_Enumerate(string fileName, int expectedCount)
+    {
+        using var assetStream = Assets.OpenStream(fileName);
+        using var vfs = new MSCfbFileSystem(assetStream);
+
+        int actualCount = vfs.EnumerateDirectories("/").Count();
+        Assert.AreEqual(expectedCount, actualCount);
+    }
+
+    [TestMethod]
+    public void MSCfb_Sample_Storage_OpenNamed()
+    {
+        using var assetStream = Assets.OpenStream("MultipleStorage.cfb");
+        using var vfs = new MSCfbFileSystem(assetStream);
+
+        Assert.IsTrue(vfs.DirectoryExists("/MyStorage"));
+        Assert.IsFalse(vfs.DirectoryExists("/NonExistentStorage"));
+    }
+
+    #endregion
+
+    #region Office documents
+
+    [TestMethod]
+    [DataRow("Samples/LibreOfficeBlankSample_v25.8.doc")]
+    [DataRow("Samples/LibreOfficeBlankSample_v25.8.xls")]
+    [DataRow("Samples/LibreOfficeBlankSample_v25.8.ppt")]
+    [DataRow("Samples/Office365BlankSample_v2507.doc")]
+    [DataRow("Samples/Office365BlankSample_v2507.xls")]
+    [DataRow("Samples/Office365BlankSample_v2507.ppt")]
+    public void MSCfb_Sample_OfficeDocument_Open(string fileName)
+    {
+        using var assetStream = Assets.OpenStream(fileName);
+        using var vfs = new MSCfbFileSystem(assetStream);
+
+        var entries = vfs.EnumerateEntries("/");
+        Assert.IsTrue(entries.Any(), "Expected at least one entry in the root storage.");
+    }
+
+    #endregion
+}
