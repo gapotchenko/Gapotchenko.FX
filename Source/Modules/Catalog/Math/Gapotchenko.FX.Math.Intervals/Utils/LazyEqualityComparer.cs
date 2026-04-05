ed485506c553ed9c58a9fb45fbfa62bb1a4ba7fb
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2026-04-05 04:57:25 +0200

    Fixed issue that could lead to a slow interval comparison.

--- Source/Modules/Catalog/Math/Gapotchenko.FX.Math.Intervals/Utils/LazyEqualityComparer.cs
+++ Source/Modules/Catalog/Math/Gapotchenko.FX.Math.Intervals/Utils/LazyEqualityComparer.cs
@@ -1,8 +1,12 @@
-﻿namespace Gapotchenko.FX.Math.Intervals.Utils;
+﻿#if !NET
+#pragma warning disable CS8767 // Nullability of reference types in type of parameter doesn't match implicitly implemented member
+#endif
+
+namespace Gapotchenko.FX.Math.Intervals.Utils;
 
 readonly struct LazyEqualityComparer<T>(IEqualityComparer<T>? comparer) : IEqualityComparer<T>
 {
     public bool Equals(T? x, T? y) => (comparer ?? EqualityComparer<T>.Default).Equals(x, y);
 
-    public int GetHashCode(T obj) => (comparer ?? EqualityComparer<T>.Default).GetHashCode();
+    public int GetHashCode([DisallowNull] T obj) => (comparer ?? EqualityComparer<T>.Default).GetHashCode(obj);
 }
