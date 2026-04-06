16c673f5ec3e2b2baba3b5df9ad204a5ddb1974e
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2026-04-06 16:06:41 +0200

    Extended Gapotchenko.FX.IO tests.

--- Source/Modules/Catalog/IO/Gapotchenko.FX.IO.Tests/Kits/StreamTestKit.cs
+++ Source/Modules/Catalog/IO/Gapotchenko.FX.IO.Tests/Kits/StreamTestKit.cs
@@ -5,13 +5,19 @@
 // File introduced by: Oleksiy Gapotchenko
 // Year of introduction: 2026
 
-using Microsoft.Testing.Platform.Extensions.Messages;
-
 namespace Gapotchenko.FX.IO.Tests.Kits;
 
 [TestCategory("io")]
 public abstract class StreamTestKit
 {
+    /// <inheritdoc cref="CreateStream(Stream?, bool)"/>
+    protected Stream CreateStream(byte[]? content, bool writable)
+    {
+        return CreateStream(
+            content is null ? null : new MemoryStream(content, false),
+            writable);
+    }
+
     /// <summary>
     /// Creates a <see cref="Stream"/> instance to test.
     /// </summary>
@@ -22,17 +28,31 @@ public abstract class StreamTestKit
 
     [TestMethod]
     [DataRow(new byte[] { 1, 2, 3 }, SeekOrigin.End, -2, 1, 2)]
-    public void IO_Stream_Seek(byte[] data, SeekOrigin seekOrigin, long seekOffset, long expectedOffset, int expectedByte)
+    [DataRow(new byte[] { 1, 2, 3 }, SeekOrigin.Current, -1, -1, 1)]
+    public void IO_Stream_Seek(byte[]? data, SeekOrigin seekOrigin, long seekOffset, long expectedPosition, int expectedByte)
     {
-        var stream = CreateStream(new MemoryStream(data, false), false);
-        Assert.AreEqual(expectedOffset, stream.Seek(seekOffset, seekOrigin), "Offset mismatch.");
+        var stream = CreateStream(data, false);
+
+        if (expectedPosition == -1)
+        {
+            long originalPosition = stream.Position;
+            Assert.ThrowsExactly<IOException>(() => stream.Seek(seekOffset, seekOrigin));
+            Assert.AreEqual(originalPosition, stream.Position);
+        }
+        else
+        {
+            Assert.AreEqual(expectedPosition, stream.Seek(seekOffset, seekOrigin), "Position mismatch.");
+        }
+
+        Assert.ThrowsExactly<ArgumentException>(() => stream.Seek(1, (SeekOrigin)100));
+
         Assert.AreEqual(expectedByte, stream.ReadByte(), "Data mismatch");
     }
 
     [TestMethod]
     public void IO_Stream_Position()
     {
-        var stream = CreateStream(new MemoryStream([1, 2, 3], false), false);
+        var stream = CreateStream([1, 2, 3], false);
 
         // Undershot.
         Assert.ThrowsExactly<ArgumentOutOfRangeException>(() => stream.Position = -1, "Undershoot is not rejected.");
