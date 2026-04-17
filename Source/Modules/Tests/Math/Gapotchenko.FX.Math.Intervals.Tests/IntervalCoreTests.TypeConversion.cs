177f14905b2168d7410feb49646287fefcdc7e1f
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2026-04-18 00:29:27 +0200

    Intervals: added type conversion tests.

---
+++ Source/Modules/Tests/Math/Gapotchenko.FX.Math.Intervals.Tests/IntervalCoreTests.TypeConversion.cs
@@ -0,0 +1,33 @@
+﻿// Gapotchenko.FX
+//
+// Copyright © Gapotchenko and Contributors
+//
+// File introduced by: Oleksiy Gapotchenko
+// Year of introduction: 2026
+
+namespace Gapotchenko.FX.Math.Intervals.Tests;
+
+partial class IntervalCoreTests
+{
+    TypeConverter GetTypeConverter()
+    {
+        var interval = EmptyInterval<int>();
+        return TypeDescriptor.GetConverter(interval);
+    }
+
+    [TestMethod]
+    public void Interval_Core_TypeConverter_CloneConversion()
+    {
+        var typeConverter = GetTypeConverter();
+
+        var intervalToConvert = NewInterval(ValueInterval.Inclusive(1, 2));
+
+        object? convertedInterval = typeConverter.ConvertFrom(intervalToConvert);
+        Assert.AreNotSame(intervalToConvert, convertedInterval);
+        Assert.AreEqual(intervalToConvert, convertedInterval);
+
+        convertedInterval = typeConverter.ConvertTo(intervalToConvert, intervalToConvert.GetType());
+        Assert.AreNotSame(intervalToConvert, convertedInterval);
+        Assert.AreEqual(intervalToConvert, convertedInterval);
+    }
+}
