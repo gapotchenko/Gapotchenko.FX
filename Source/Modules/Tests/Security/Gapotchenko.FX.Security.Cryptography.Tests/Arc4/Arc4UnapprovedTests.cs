edd3298a96daa3493118900e3b6f6e249a56c514
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2026-06-22 16:47:21 +0200

    Added tests for Arc4Unapproved algorithm.

--- Source/Modules/Tests/Security/Gapotchenko.FX.Security.Cryptography.Tests/Arc4/Arc4UnapprovedTests.cs
+++ Source/Modules/Tests/Security/Gapotchenko.FX.Security.Cryptography.Tests/Arc4/Arc4UnapprovedTests.cs
@@ -3,20 +3,17 @@
 // Copyright © Gapotchenko and Contributors
 //
 // File introduced by: Oleksiy Gapotchenko
-// Year of introduction: 2022
+// Year of introduction: 2026
 
 namespace Gapotchenko.FX.Security.Cryptography.Tests.Arc4;
 
 using Arc4 = Cryptography.Arc4;
 
 [TestClass]
-public sealed class Arc4Tests : Arc4TestsBase
+public sealed class Arc4UnapprovedTests : Arc4TestsBase
 {
     protected override Arc4 CreateAlgorithm()
     {
-        if (CryptographyPolicy.AllowOnlyFipsAlgorithms)
-            Assert.Inconclusive("ARC4 is not permitted under FIPS policy.");
-
-        return Arc4.Create();
+        return Arc4Unapproved.Create();
     }
 }
