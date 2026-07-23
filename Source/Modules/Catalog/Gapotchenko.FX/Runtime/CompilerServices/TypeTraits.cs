cf03204497e6901b759b4b991331c44409074ceb
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2026-07-24 01:24:08 +0200

    Added TypeTraits<T>.IsBitwiseEquatable property.

--- Source/Modules/Catalog/Gapotchenko.FX/Runtime/CompilerServices/TypeTraits.cs
+++ Source/Modules/Catalog/Gapotchenko.FX/Runtime/CompilerServices/TypeTraits.cs
@@ -12,11 +12,23 @@ public static class TypeTraits<T>
     {
         var type = typeof(T);
 
-        IsValueType = type.IsValueType;
+        bool isValueType = type.IsValueType;
+        IsValueType = isValueType;
+
+        IsBitwiseEquatable =
+            isValueType &&
+            (type.IsEnum ||
+            type.IsPrimitive && Type.GetTypeCode(type) is not (TypeCode.Single or TypeCode.Double or TypeCode.Decimal) ||
+            type == typeof(Guid));
     }
 
     /// <summary>
     /// Gets a value indicating whether <typeparamref name="T"/> type is a value type.
     /// </summary>
     public static readonly bool IsValueType;
+
+    /// <summary>
+    /// Gets a value indicating whether <typeparamref name="T"/> type is bitwise equatable.
+    /// </summary>
+    public static bool IsBitwiseEquatable { get; }
 }
