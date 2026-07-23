21d908ea884212b02982d7d67b0a7a1a75352dbf
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2026-07-24 00:24:51 +0200

    Added TypeTraits<T>.IsPrimitive property.

--- Source/Modules/Catalog/Gapotchenko.FX/Runtime/CompilerServices/TypeTraits.cs
+++ Source/Modules/Catalog/Gapotchenko.FX/Runtime/CompilerServices/TypeTraits.cs
@@ -1,4 +1,6 @@
-﻿namespace Gapotchenko.FX.Runtime.CompilerServices;
+﻿#pragma warning disable CA1000 // Do not declare static members on generic types
+
+namespace Gapotchenko.FX.Runtime.CompilerServices;
 
 /// <summary>
 /// Provides strongly typed access to reflection traits of <typeparamref name="T"/> type.
@@ -11,10 +13,16 @@ public static class TypeTraits<T>
         var type = typeof(T);
 
         IsValueType = type.IsValueType;
+        IsPrimitive = type.IsPrimitive;
     }
 
     /// <summary>
-    /// Gets a value indicating whether the type is a value type.
+    /// Gets a value indicating whether <typeparamref name="T"/> type is a value type.
     /// </summary>
     public static readonly bool IsValueType;
+
+    /// <summary>
+    /// Gets a value indicating whether <typeparamref name="T"/> type is primitive.
+    /// </summary>
+    public static bool IsPrimitive { get; }
 }
