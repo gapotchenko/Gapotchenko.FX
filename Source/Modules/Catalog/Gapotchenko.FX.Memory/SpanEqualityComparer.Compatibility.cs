7b238ae8625710dacb2424349f9ca681a3737dee
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2026-07-23 11:37:09 +0200

    Formalized shape of SpanEqualityComparer API.

---
+++ Source/Modules/Catalog/Gapotchenko.FX.Memory/SpanEqualityComparer.Compatibility.cs
@@ -0,0 +1,60 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Gapotchenko.FX.Memory;
+
+partial class SpanEqualityComparer
+{
+#if BINARY_COMPATIBILITY // 2026
+
+    /// <summary>
+    /// Determines whether specified spans are equal.
+    /// </summary>
+    /// <typeparam name="T">The span element type.</typeparam>
+    /// <param name="x">The first read-only span to compare.</param>
+    /// <param name="y">The second read-only span to compare.</param>
+    /// <returns><see langword="true"/> if the specified spans are equal; otherwise, <see langword="false"/>.</returns>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    [OverloadResolutionPriority(-1000)]
+    public static bool Equals<T>(Span<T> x, Span<T> y)
+    {
+        return
+            x == y ||
+            x != null && y != null &&
+            x.SequenceEqual(y);
+    }
+
+    /// <summary>
+    /// Returns a hash code for the specified span by combining the hash codes
+    /// returned by <see cref="IEqualityComparer{T}.GetHashCode(T)"/>
+    /// for its elements.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the span.</typeparam>
+    /// <param name="span">The span to compute a hash code for.</param>
+    /// <returns>
+    /// A hash code representing the content of the specified span.
+    /// </returns>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    [OverloadResolutionPriority(-1000)]
+    public static int GetHashCode<T>(ReadOnlySpan<T> span) => GetHashCode(span, null);
+
+    /// <summary>
+    /// Returns a hash code for the specified span.
+    /// </summary>
+    /// <typeparam name="T">The span element type.</typeparam>
+    /// <param name="span">The span.</param>
+    /// <returns>A hash code for the specified span.</returns>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    [OverloadResolutionPriority(-1000)]
+    public static int GetHashCode<T>(Span<T> span) => GetHashCode((ReadOnlySpan<T>)span);
+
+    /// <summary>
+    /// Returns a hash code for the specified span of bytes.
+    /// </summary>
+    /// <param name="span">The span of bytes.</param>
+    /// <returns>A hash code for the specified span of bytes.</returns>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    [OverloadResolutionPriority(-1000)]
+    public static int GetHashCode(Span<byte> span) => GetHashCode((ReadOnlySpan<byte>)span);
+
+#endif
+}
--- Source/Modules/Catalog/Gapotchenko.FX.Memory/SpanEqualityComparer.cs
+++ Source/Modules/Catalog/Gapotchenko.FX.Memory/SpanEqualityComparer.cs
@@ -1,37 +1,56 @@
-﻿using Gapotchenko.FX.Runtime.InteropServices;
-using System.Runtime.CompilerServices;
+﻿// Gapotchenko.FX
+//
+// Copyright © Gapotchenko and Contributors
+//
+// File introduced by: Oleksiy Gapotchenko
+// Year of introduction: 2023
+
+using Gapotchenko.FX.Runtime.InteropServices;
 
 namespace Gapotchenko.FX.Memory;
 
 /// <summary>
 /// Equality comparer for contiguous regions of memory represented by <see cref="ReadOnlySpan{T}"/> type.
 /// </summary>
-public static class SpanEqualityComparer
+public static partial class SpanEqualityComparer
 {
+    #region Equals
+
     /// <summary>
-    /// Determines whether specified read-only spans are equal.
+    /// Determines whether two spans are equal
+    /// by comparing the elements using <see cref="IEqualityComparer{T}"/>.
     /// </summary>
-    /// <typeparam name="T">The span element type.</typeparam>
-    /// <param name="x">The first read-only span to compare.</param>
-    /// <param name="y">The second read-only span to compare.</param>
-    /// <returns><see langword="true"/> if the specified read-only spans are equal; otherwise, <see langword="false"/>.</returns>
-    public static bool Equals<T>(ReadOnlySpan<T> x, ReadOnlySpan<T> y)
+    /// <typeparam name="T">The type of elements in the span.</typeparam>
+    /// <param name="x">The first span to compare.</param>
+    /// <param name="y">The second span to compare.</param>
+    /// <param name="comparer">
+    /// The <see cref="IEqualityComparer{T}"/> implementation to use when comparing elements,
+    /// or <see langword="null"/> to use the default <see cref="IEqualityComparer{T}"/> for the type of an element.
+    /// </param>
+    /// <returns>
+    /// <see langword="true"/> if the two spans are equal; 
+    /// otherwise, <see langword="false"/>.
+    /// </returns>
+    public static bool Equals<T>(ReadOnlySpan<T> x, ReadOnlySpan<T> y, IEqualityComparer<T>? comparer = null)
     {
         return
             x == y ||
             x != null && y != null &&
-            x.SequenceEqual(y);
+            x.SequenceEqual(y, comparer);
     }
 
     /// <summary>
-    /// Determines whether specified spans are equal.
+    /// Determines whether two spans are equal
+    /// by comparing the elements using <see cref="IEquatable{T}.Equals(T)"/>.
     /// </summary>
-    /// <typeparam name="T">The span element type.</typeparam>
-    /// <param name="x">The first read-only span to compare.</param>
-    /// <param name="y">The second read-only span to compare.</param>
-    /// <returns><see langword="true"/> if the specified spans are equal; otherwise, <see langword="false"/>.</returns>
-    [OverloadResolutionPriority(1)]
-    public static bool Equals<T>(Span<T> x, Span<T> y)
+    /// <typeparam name="T">The type of elements in the span.</typeparam>
+    /// <param name="x">The first span to compare.</param>
+    /// <param name="y">The second span to compare.</param>
+    /// <returns>
+    /// <see langword="true"/> if the two spans are equal; 
+    /// otherwise, <see langword="false"/>.
+    /// </returns>
+    public static bool Equals<T>(ReadOnlySpan<T> x, ReadOnlySpan<T> y) where T : IEquatable<T>
     {
         return
             x == y ||
@@ -44,56 +63,50 @@ public static class SpanEqualityComparer
     [Obsolete("SpanEqualityComparer.Equals(object, object) method cannot be used. Use SpanEqualityComparer.Equals<T>(ReadOnlySpan<T>, ReadOnlySpan<T>) method instead.", true)]
     public static new bool Equals(object? objA, object? objB) => throw new NotSupportedException();
 
-    /// <summary>
-    /// Returns a hash code for the specified span.
-    /// </summary>
-    /// <typeparam name="T">The span element type.</typeparam>
-    /// <param name="span">The span.</param>
-    /// <returns>A hash code for the specified span.</returns>
-    public static int GetHashCode<T>(Span<T> span) => GetHashCode((ReadOnlySpan<T>)span);
+    #endregion
+
+    #region GetHashCode
 
     /// <summary>
-    /// Returns a hash code for the specified read-only span.
+    /// Returns a hash code for the specified span by combining the hash codes
+    /// returned by <see cref="IEqualityComparer{T}.GetHashCode(T)"/>
+    /// for its elements.
     /// </summary>
-    /// <typeparam name="T">The span element type.</typeparam>
-    /// <param name="span">The read-only span.</param>
-    /// <returns>A hash code for the specified read-only span.</returns>
-    public static int GetHashCode<T>(ReadOnlySpan<T> span)
+    /// <typeparam name="T">The type of elements in the span.</typeparam>
+    /// <param name="span">The span to compute a hash code for.</param>
+    /// <param name="comparer">
+    /// The <see cref="IEqualityComparer{T}"/> implementation to use when computing hash codes for elements,
+    /// or <see langword="null"/> to use the default <see cref="IEqualityComparer{T}"/> for the type of an element.
+    /// </param>
+    /// <returns>A hash code representing the content of the specified span.</returns>
+    public static int GetHashCode<T>(ReadOnlySpan<T> span, IEqualityComparer<T>? comparer = null)
     {
-        if (span == null)
-        {
-            // Return zero for a span representing null to distinguish it from empty span.
-            return 0;
-        }
-        else
-        {
-            return HashOperations.GetHashCode(span);
-        }
+        return
+            GetDistinguishableHashCode(span) ??
+            HashOperations.GetHashCode(span, comparer);
     }
 
     /// <summary>
     /// Returns a hash code for the specified span of bytes.
     /// </summary>
-    /// <param name="span">The span of bytes.</param>
-    /// <returns>A hash code for the specified span of bytes.</returns>
-    public static int GetHashCode(Span<byte> span) =>
-        GetHashCode((ReadOnlySpan<byte>)span);
-
-    /// <summary>
-    /// Returns a hash code for the specified read-only span of bytes.
-    /// </summary>
-    /// <param name="span">The read-only span of bytes.</param>
-    /// <returns>A hash code for the specified read-only span of bytes.</returns>
+    /// <param name="span">The span of bytes to compute a hash code for.</param>
+    /// <returns>A hash code representing the content of the specified span of bytes.</returns>
     public static int GetHashCode(ReadOnlySpan<byte> span)
     {
-        if (span == null)
-        {
-            // Return zero for a span representing null to distinguish it from empty span.
-            return 0;
-        }
-        else
-        {
-            return HashOperations.GetHashCode(span);
-        }
+        return
+            GetDistinguishableHashCode(span) ??
+            HashOperations.GetHashCode(span);
     }
+
+    static int? GetDistinguishableHashCode<T>(ReadOnlySpan<T> span)
+    {
+        if (span == null)
+            return -1;
+        if (span.Length == 0)
+            return 0;
+        else
+            return null;
+    }
+
+    #endregion
 }
