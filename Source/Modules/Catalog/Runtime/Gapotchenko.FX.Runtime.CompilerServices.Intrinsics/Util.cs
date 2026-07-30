297f144e5213dab646c8cd2738d7e3794997436b
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2026-07-30 12:52:11 +0200

    Refactoring.

--- Source/Modules/Catalog/Runtime/Gapotchenko.FX.Runtime.CompilerServices.Intrinsics/Util.cs
+++ Source/Modules/Catalog/Runtime/Gapotchenko.FX.Runtime.CompilerServices.Intrinsics/Util.cs
@@ -9,13 +9,25 @@ namespace Gapotchenko.FX.Runtime.CompilerServices;
 
 static class Util
 {
-    public static bool HasPrologue(ReadOnlySpan<byte> data, byte[][] prologues)
+    public static bool HasPrologue<T>(ReadOnlySpan<T> data, T[][] prologues)
+        where T : IEquatable<T>?
     {
-        foreach (byte[] prologue in prologues)
+        foreach (var prologue in prologues)
         {
             if (data.StartsWith(prologue))
                 return true;
         }
         return false;
+    }
+
+    public static int GetGuaranteedInstructionCount<T>(ReadOnlySpan<T> data, (T[] Instructions, int Size)[] prologues)
+        where T : IEquatable<T>?
+    {
+        foreach (var prologue in prologues)
+        {
+            if (data.StartsWith(prologue.Instructions))
+                return prologue.Size;
+        }
+        return -1;
     }
 }
