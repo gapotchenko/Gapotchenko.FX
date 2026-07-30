406ce3b33091ea460670d223c0365ace79175681
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2026-07-30 14:25:46 +0200

    Intrinsic compiler: use a conservative minimum method body extent for x86.

--- Source/Modules/Catalog/Runtime/Gapotchenko.FX.Runtime.CompilerServices.Intrinsics/Pal/OS/Windows/AdapterWindowsX86.cs
+++ Source/Modules/Catalog/Runtime/Gapotchenko.FX.Runtime.CompilerServices.Intrinsics/Pal/OS/Windows/AdapterWindowsX86.cs
@@ -21,17 +21,24 @@ sealed class AdapterWindowsX86 : AdapterX86
 {
     public override PatchResult PatchMethod(MethodInfo method, ReadOnlySpan<byte> code)
     {
-        var methodInstructions = GetMethodInstructions(method);
-        if (!Util.HasPrologue(methodInstructions, m_SupportedPrologues))
+        var instructions = GetMethodInstructions(method);
+
+        int patchablePrologueSize = Util.GetPatchablePrologueSize(instructions, m_SupportedPrologues);
+        if (patchablePrologueSize < 0)
             return PatchResult.UnexpectedPrologue;
 
-        var patchInstructions = code;
+        instructions = instructions[..Math.Min(patchablePrologueSize, instructions.Length)];
 
-        int patchSize = patchInstructions.Length + 1 /* RET */;
-        if (patchSize > methodInstructions.Length)
+        int patchSize = code.Length + 1 /* RET */;
+        if (patchSize > instructions.Length)
+        {
+            // TODO: Use trampoline allocator to contain the code, patch method with JMP instruction to that code.
             return PatchResult.NoSpace;
-
-        methodInstructions = methodInstructions[..patchSize];
+        }
+        else
+        {
+            instructions = instructions[..patchSize];
+        }
 
 #if TFF_CER
         // Ensure that code changes are atomic by using the constrained execution region.
@@ -43,13 +50,13 @@ sealed class AdapterWindowsX86 : AdapterX86
 #endif
         {
             // Temporarily allow memory modification in order to apply the intrinsic code.
-            using var scope = VirtualProtectionScope.Create(methodInstructions, NativeMethods.PageProtect.ExecuteReadWrite);
+            using var scope = VirtualProtectionScope.Create(instructions, NativeMethods.PageProtect.ExecuteReadWrite);
 
             // Put the intrinsic code.
-            patchInstructions.CopyTo(methodInstructions);
+            code.CopyTo(instructions);
 
             // End the method with a RET instruction.
-            methodInstructions[code.Length] = RET;
+            instructions[code.Length] = RET;
 
             scope.FlushInstructions();
         }
@@ -57,16 +64,18 @@ sealed class AdapterWindowsX86 : AdapterX86
         return PatchResult.Success;
     }
 
-    static readonly byte[][] m_SupportedPrologues =
+    // The table defines a conservative minimum method body extent:
+    // complete prologue plus the shortest matching restoration/return sequence.
+    static readonly (byte[] Instructions, int Size)[] m_SupportedPrologues =
     [
-        [0x55, 0x8b, 0xec],             // PUSH EBP; MOV EBP,ESP
-        [0x8b, 0xff, 0x55, 0x8b, 0xec], // MOV EDI,EDI; PUSH EBP; MOV EBP,ESP
-        [0x53, 0x56, 0x57],             // PUSH EBX; PUSH ESI; PUSH EDI
-        [0x53, 0x83, 0xec],             // PUSH EBX; SUB ESP,imm8
-        [0x56, 0x83, 0xec],             // PUSH ESI; SUB ESP,imm8
-        [0x57, 0x83, 0xec],             // PUSH EDI; SUB ESP,imm8
-        [0x83, 0xec],                   // SUB ESP,imm8
-        [0x81, 0xec],                   // SUB ESP,imm32
+        ([0x55, 0x8b, 0xec], 3 + 2),             // PUSH EBP; MOV EBP,ESP
+        ([0x8b, 0xff, 0x55, 0x8b, 0xec], 5 + 2), // MOV EDI,EDI; PUSH EBP; MOV EBP,ESP
+        ([0x53, 0x56, 0x57], 3 + 4),             // PUSH EBX; PUSH ESI; PUSH EDI
+        ([0x53, 0x83, 0xec], 4 + 5),             // PUSH EBX; SUB ESP,imm8
+        ([0x56, 0x83, 0xec], 4 + 5),             // PUSH ESI; SUB ESP,imm8
+        ([0x57, 0x83, 0xec], 4 + 5),             // PUSH EDI; SUB ESP,imm8
+        ([0x83, 0xec], 3 + 4),                   // SUB ESP,imm8
+        ([0x81, 0xec], 6 + 7),                   // SUB ESP,imm32
     ];
 
     static unsafe Span<byte> GetMethodInstructions(MethodInfo method)
--- Source/Modules/Catalog/Runtime/Gapotchenko.FX.Runtime.CompilerServices.Intrinsics/Util.cs
+++ Source/Modules/Catalog/Runtime/Gapotchenko.FX.Runtime.CompilerServices.Intrinsics/Util.cs
@@ -20,7 +20,20 @@ static class Util
         return false;
     }
 
-    public static int GetGuaranteedInstructionCount<T>(ReadOnlySpan<T> data, (T[] Instructions, int Size)[] prologues)
+    /// <summary>
+    /// Gets the count of instructions guaranteed to be patchable
+    /// in a method with the specified prologues.
+    /// </summary>
+    /// <typeparam name="T">The type of instructions in the CPU instruction stream.</typeparam>
+    /// <param name="data">The method instructions.</param>
+    /// <param name="prologues">The set of prologues.</param>
+    /// <returns>
+    /// The count of instructions guaranteed to be patchable,
+    /// or <c>-1</c> when the method prologue is not recognized.
+    /// </returns>
+    public static int GetPatchablePrologueSize<T>(
+        ReadOnlySpan<T> data,
+        (T[] Instructions, int Size /* count of patchable instructions */)[] prologues)
         where T : IEquatable<T>?
     {
         foreach (var prologue in prologues)
