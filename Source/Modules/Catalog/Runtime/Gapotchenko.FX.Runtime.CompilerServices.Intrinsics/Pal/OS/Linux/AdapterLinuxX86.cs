1bc85e483a62545dec63e479d2bd9304931340fd
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2026-07-31 00:02:05 +0200

    Use instrinsic trampolines for Linux x86 when method body space is too tight.

--- Source/Modules/Catalog/Runtime/Gapotchenko.FX.Runtime.CompilerServices.Intrinsics/Pal/OS/Linux/AdapterLinuxX86.cs
+++ Source/Modules/Catalog/Runtime/Gapotchenko.FX.Runtime.CompilerServices.Intrinsics/Pal/OS/Linux/AdapterLinuxX86.cs
@@ -8,6 +8,9 @@
 using Gapotchenko.FX.Runtime.CompilerServices.Pal.Architectures;
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+#pragma warning disable CS9191 // The 'ref' modifier for an argument corresponding to 'in' parameter is equivalent to 'in'. Consider using 'in' instead.
 
 namespace Gapotchenko.FX.Runtime.CompilerServices.Pal.OS.Linux;
 
@@ -19,15 +22,13 @@ sealed class AdapterLinuxX86 : AdapterX86
     public override PatchResult PatchMethod(MethodInfo method, ReadOnlySpan<byte> code)
     {
         var instructions = GetMethodInstructions(method);
-        if (!Util.HasPrologue(instructions, m_SupportedPrologues))
+        int patchablePrologueSize = Util.GetPatchablePrologueSize(instructions, m_SupportedPrologues);
+        if (patchablePrologueSize < 0)
             return PatchResult.UnexpectedPrologue;
 
-        int patchSize = code.Length + 1 /* RET */;
-        if (patchSize > instructions.Length)
-            return PatchResult.NoSpace;
+        instructions = instructions[..Math.Min(patchablePrologueSize, instructions.Length)];
 
-        instructions = instructions[..patchSize];
-
+        PatchResult result;
 #if TFF_CER
         // Ensure that code changes are atomic by using the constrained execution region.
         RuntimeHelpers.PrepareConstrainedRegions();
@@ -37,29 +38,23 @@ sealed class AdapterLinuxX86 : AdapterX86
         finally
 #endif
         {
-            // Temporarily allow memory modification in order to apply the intrinsic code.
-            using var scope = MemoryProtectionScope.Create(instructions, NativeMethods.MemoryProtection.Read | NativeMethods.MemoryProtection.Write | NativeMethods.MemoryProtection.Execute);
-
-            // Put the intrinsic code.
-            code.CopyTo(instructions);
-
-            // End the method with a RET instruction.
-            instructions[code.Length] = Ret;
+            result = ApplyPatch(instructions, code);
         }
-
-        return PatchResult.Success;
+        return result;
     }
 
-    static readonly byte[][] m_SupportedPrologues =
+    // The table defines a conservative minimum method body extent:
+    // complete prologue plus the shortest matching restoration/return sequence.
+    static readonly (byte[] Instructions, int Size)[] m_SupportedPrologues =
     [
-        [0x55, 0x8b, 0xec],
-        [0x55, 0x89, 0xe5],
-        [0x53, 0x56, 0x57],
-        [0x53, 0x83, 0xec],
-        [0x56, 0x83, 0xec],
-        [0x57, 0x83, 0xec],
-        [0x83, 0xec],
-        [0x81, 0xec]
+        ([0x55, 0x8b, 0xec], 3 + 2), // PUSH EBP; MOV EBP,ESP
+        ([0x55, 0x89, 0xe5], 3 + 2), // PUSH EBP; MOV EBP,ESP
+        ([0x53, 0x56, 0x57], 3 + 4), // PUSH EBX; PUSH ESI; PUSH EDI
+        ([0x53, 0x83, 0xec], 4 + 5), // PUSH EBX; SUB ESP,imm8
+        ([0x56, 0x83, 0xec], 4 + 5), // PUSH ESI; SUB ESP,imm8
+        ([0x57, 0x83, 0xec], 4 + 5), // PUSH EDI; SUB ESP,imm8
+        ([0x83, 0xec], 3 + 4),       // SUB ESP,imm8
+        ([0x81, 0xec], 6 + 7),       // SUB ESP,imm32
     ];
 
     static unsafe Span<byte> GetMethodInstructions(MethodInfo method)
@@ -67,5 +62,56 @@ sealed class AdapterLinuxX86 : AdapterX86
         RuntimeHelpers.PrepareMethod(method.MethodHandle);
         byte* p = SkipBranches((byte*)method.MethodHandle.GetFunctionPointer());
         return Unwind.GetMethodInstructions(p);
+    }
+
+    static PatchResult ApplyPatch(Span<byte> instructions, ReadOnlySpan<byte> code)
+    {
+        int patchSize = code.Length + 1 /* RET */;
+
+        var trampoline = Span<byte>.Empty;
+        if (patchSize > instructions.Length)
+        {
+            if (instructions.Length < JmpRel32Size)
+                return PatchResult.NoSpace;
+
+            trampoline = TrampolineAllocator.Allocate(patchSize);
+
+            using (var trampolineScope = MemoryProtectionScope.Create(
+                trampoline,
+                NativeMethods.MemoryProtection.Read | NativeMethods.MemoryProtection.Write | NativeMethods.MemoryProtection.Execute))
+            {
+                code.CopyTo(trampoline);
+                trampoline[code.Length] = Ret;
+                trampolineScope.FlushInstructions();
+            }
+
+            instructions = instructions[..JmpRel32Size];
+        }
+        else
+        {
+            instructions = instructions[..patchSize];
+        }
+
+        using var scope = MemoryProtectionScope.Create(
+            instructions,
+            NativeMethods.MemoryProtection.Read | NativeMethods.MemoryProtection.Write | NativeMethods.MemoryProtection.Execute);
+
+        if (!trampoline.IsEmpty)
+        {
+            nint offset = Unsafe.ByteOffset(
+                ref MemoryMarshal.GetReference(instructions),
+                ref MemoryMarshal.GetReference(trampoline));
+            int displacement = (int)(offset - JmpRel32Size);
+            instructions[0] = JmpRel32;
+            MemoryMarshal.Write(instructions[1..], ref displacement);
+        }
+        else
+        {
+            code.CopyTo(instructions);
+            instructions[code.Length] = Ret;
+        }
+
+        scope.FlushInstructions();
+        return PatchResult.Success;
     }
 }
