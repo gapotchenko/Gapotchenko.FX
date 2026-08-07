c5e1ac42b08ec92f47988f78959eea0dd081895f
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2026-08-08 00:18:40 +0200

    Refactoring.

--- Source/Modules/Catalog/Security/Gapotchenko.FX.Security.Cryptography/Utils/FixedTimeLogic.cs
+++ Source/Modules/Catalog/Security/Gapotchenko.FX.Security.Cryptography/Utils/FixedTimeLogic.cs
@@ -5,12 +5,16 @@
 // File introduced by: Oleksiy Gapotchenko
 // Year of introduction: 2026
 
+using System.Runtime.CompilerServices;
+
 namespace Gapotchenko.FX.Security.Cryptography.Utils;
 
 static class FixedTimeLogic
 {
+    [MethodImpl(MethodImplOptions.NoInlining)]
     public static int IsZero(int value) => (value - 1 >> 31) & 1;
 
+    [MethodImpl(MethodImplOptions.NoInlining)]
     public static int Select(int condition, int whenTrue, int whenFalse)
     {
         int mask = -condition;
